12e94fc5dec425a897127483e179364d23446d3c
gojomo <gojomo@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2003-04-17 21:49:54 +0000

    placeholder for flexible config object

---
+++ trunk/heritrix/src/org/archive/crawler/framework/Config.java
@@ -0,0 +1,15 @@
+/* 
+ * Config.java
+ * Created on Apr 16, 2003
+ *
+ * $Header$
+ */
+package org.archive.crawler.framework;
+
+/**
+ * 
+ * @author Gordon Mohr
+ */
+public interface Config {
+
+}
