ca37e2894bc47ca1b45ac4d056269b3c1505801d
gojomo <gojomo@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2003-05-15 06:04:00 +0000

    *** empty log message ***

---
+++ heritrix/src/org/archive/crawler/core/CrawlController.java
@@ -0,0 +1,17 @@
+/* 
+ * CrawlController.java
+ * Created on May 14, 2003
+ *
+ * $Header$
+ */
+package org.archive.crawler.core;
+
+import java.util.Set;
+
+/**
+ * 
+ * @author Gordon Mohr
+ */
+public class CrawlController {
+	Set toes; /* of ToeThreads */
+}
