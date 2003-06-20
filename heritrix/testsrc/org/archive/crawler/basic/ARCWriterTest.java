07ceef84ceea742397fa180d068b269041865c8c
gojomo <gojomo@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2003-06-20 01:54:13 +0000

    moved junit tests to different source dir

---
+++ heritrix/testsrc/org/archive/crawler/basic/ARCWriterTest.java
@@ -0,0 +1,92 @@
+/*
+ * Created on Jun 12, 2003
+ *
+ */
+package org.archive.crawler.basic;
+
+import junit.framework.*;
+
+import org.archive.crawler.basic.ARCWriter; 
+import org.archive.crawler.basic.SimpleHTTPFetcher;
+import org.archive.crawler.datamodel.*;
+import org.archive.crawler.framework.*;
+import org.apache.commons.httpclient.Header;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
+
+import java.net.*;
+
+/**
+ * @author Parker Thompson
+ *  
+ */
+public class ARCWriterTest extends TestCase {
+	
+	ARCWriter writer;
+	SimpleHTTPFetcher fetcher;
+	CrawlURI curi;
+	CrawlController controller;
+	String orderFile;
+	CrawlOrder order;
+	CrawlHost crawlhost;
+	
+	public static void main(String[] args) {	
+		ARCWriterTest a = new ARCWriterTest("testHttpWrite");
+	}
+	
+	// initialize any variables used in the tests here
+	public void setUp(){
+		
+		writer 		= new ARCWriter();
+		curi 			= new CrawlURI("http://parkert.com");
+		crawlhost	= new CrawlHost("parkert.com");
+		
+		try {
+			crawlhost.setIP( InetAddress.getByName("parkert.com") );
+			curi.setHost(crawlhost);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		curi.getAList().putString("content-type", "text/html");
+	}
+	
+	// clean up after the test here if necessary
+	public void tearDown(){
+		
+	}
+	
+	public ARCWriterTest(String s){
+		super(s);
+	}
+	
+	public void testHttpWrite(){
+		
+		HttpClient http = new HttpClient();
+		
+		GetMethod get = new GetMethod(curi.getUURI().getUri().toASCIIString());
+		get.setFollowRedirects(false);
+		get.setRequestHeader("User-Agent","ARCWriterTest");
+		
+		try {
+			http.executeMethod(get);
+			Header contentLength = get.getResponseHeader("Content-Length");
+			
+			curi.getAList().putObject("http-transaction",get);
+
+			//fetcher.process(curi);
+			writer.setOutputDir(new String("/home/parkert/"));
+			writer.process(curi);
+	
+			System.out.println("test ran, check " + writer.getOutputDir() + " for resulting arc(s).");
+						
+		} catch (Exception e) {	
+			System.out.println("error fetching page");
+			e.printStackTrace();
+		}
+		
+
+	}
+	
+}
