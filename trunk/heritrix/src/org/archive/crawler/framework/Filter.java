ef42d054f25ab474ca3fc31f97b8d7016ef8e349
gojomo <gojomo@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2003-07-01 01:08:21 +0000

    basic filter superclass

--- trunk/heritrix/src/org/archive/crawler/framework/Filter.java
+++ trunk/heritrix/src/org/archive/crawler/framework/Filter.java
@@ -12,6 +12,7 @@ package org.archive.crawler.framework;
  */
 public abstract class Filter extends XMLConfig {
 	String name;
+	boolean inverter = false;
 	
 	public  void setName(String n) {
 		name = n;
@@ -20,5 +21,20 @@ public abstract class Filter extends XMLConfig {
 		return name;
 	}
 	
-	public abstract boolean accepts(Object o); 
+	public boolean accepts(Object o) {
+		return inverter ^ innerAccepts(o);
+	}
+	
+	/**
+	 * @param o
+	 * @return
+	 */
+	protected abstract boolean innerAccepts(Object o);
+	
+	public void initialize() {
+		setName(getStringAt("@name"));
+		if("not".equals(getStringAt("@modifier"))) {
+			inverter = true;
+		}
+	}
 }
