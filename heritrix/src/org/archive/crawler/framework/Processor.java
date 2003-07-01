44a216594489d73ff64a7c864616fe5d9ba7d184
gojomo <gojomo@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2003-07-01 01:07:56 +0000

    added filter support; refactored process(); added local name

--- heritrix/src/org/archive/crawler/framework/Processor.java
+++ heritrix/src/org/archive/crawler/framework/Processor.java
@@ -6,6 +6,10 @@
  */
 package org.archive.crawler.framework;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.logging.Logger;
+
 import org.archive.crawler.datamodel.CrawlURI;
 import org.w3c.dom.Node;
 
@@ -14,14 +18,61 @@ import org.w3c.dom.Node;
  * @author Gordon Mohr
  */
 public class Processor extends XMLConfig {
+	/**
+	 * XPath to any specified filters
+	 */
+	private static String XP_FILTERS = "filter";
+	
+	private static Logger logger = Logger.getLogger("org.archive.crawler.framework.Processor");
+
 	protected CrawlController controller;
 	Processor defaultNext;
+	String name;
+	ArrayList filters = new ArrayList();
 	
-	public void process(CrawlURI curi) {
+	public final void process(CrawlURI curi) {
 		// by default, simply forward curi along
 		curi.setNextProcessor(getDefaultNext());
+		if(filtersAccept(curi)) {
+			innerProcess(curi);
+		} // TODO: else perhaps send to different next? 
 	}
 	
+	/**
+	 * @param curi
+	 */
+	protected void innerProcess(CrawlURI curi) {
+		// by default do nothing
+	}
+
+	/**
+	 * Do all specified filters (if any) accept this CrawlURI? 
+	 *  
+	 * @param curi
+	 * @return
+	 */
+	protected boolean filtersAccept(CrawlURI curi) {
+		if (filters.isEmpty()) {
+			return true;
+		}
+		Iterator iter = filters.iterator();
+		while(iter.hasNext()) {
+			Filter f = (Filter)iter.next();
+			if( !f.accepts(curi) ) {
+				logger.info("Filter "+f+" rejected "+curi+" in Processor "+getName());
+				return false; 
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * @return
+	 */
+	private String getName() {
+		return name;
+	}
+
 	/**
 	 * 
 	 */
@@ -31,13 +82,28 @@ public class Processor extends XMLConfig {
 	}
 	
 	public void initialize(CrawlController c) {
+		setName(getStringAt("@name"));
 		controller = c;
 		Node n;
 		if ((n=xNode.getAttributes().getNamedItem("next"))!=null) {
 			defaultNext = (Processor)c.getProcessors().get(n.getNodeValue());
 		}
+		instantiateAllInto(XP_FILTERS,filters);
+		Iterator iter = filters.iterator();
+		while(iter.hasNext()) {
+			Object o = iter.next();
+			Filter f = (Filter)o;
+			f.initialize();
+		}
 	}
 	
+	/**
+	 * @param string
+	 */
+	private void setName(String string) {
+		name=string;
+	}
+
 	public Processor spawn() {
 		Processor newInstance = null;
 		try {
