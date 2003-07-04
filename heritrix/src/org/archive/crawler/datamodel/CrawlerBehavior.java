083d1d0efd459ccbf818e63dd00dbe20664c8ed4
gojomo <gojomo@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2003-07-04 00:59:04 +0000

    crawlscope seemed superfluous

--- heritrix/src/org/archive/crawler/datamodel/CrawlerBehavior.java
+++ heritrix/src/org/archive/crawler/datamodel/CrawlerBehavior.java
@@ -6,7 +6,12 @@
  */
 package org.archive.crawler.datamodel;
 
-import org.archive.crawler.framework.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.archive.crawler.framework.XMLConfig;
 import org.w3c.dom.Node;
 
 /**
@@ -14,6 +19,8 @@ import org.w3c.dom.Node;
  *
  */
 public class CrawlerBehavior extends XMLConfig {
+	List seeds = null;
+
 
 	/**
 	 * @param node
@@ -42,5 +49,36 @@ public class CrawlerBehavior extends XMLConfig {
 	public String getFrom() {
 		return getStringAt("//http-headers/From");
 	}
+
+	public List getSeeds() {
+		if (seeds != null) {
+			return seeds;
+		}
+		seeds = new ArrayList();
+		BufferedReader reader = nodeValueOrSrcReader("//seeds");
+		String read;
+		try {
+			while (reader != null) {
+				do {read = reader.readLine();}
+				while (
+				  (read != null) 
+				  && ( (read=read.trim()).startsWith("#") 
+					   || read.length() == 0) );
+			
+				if (read == null) {
+					reader.close();
+					reader = null;
+				} else {
+					seeds.add( UURI.createUURI(read) );
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return seeds;
+   	
+	}
+
 
 }
