a4ca696459d34757f81b8a655b3a1fdde3f0259f
gojomo <gojomo@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2003-07-09 01:16:44 +0000

    new log formats

---
+++ trunk/heritrix/src/org/archive/crawler/io/UriErrorFormatter.java
@@ -0,0 +1,41 @@
+/*
+ * UriErrorFormatter.java
+ * Created on Jul 7, 2003
+ *
+ * $Header$
+ */
+package org.archive.crawler.io;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+
+import org.archive.crawler.datamodel.CoreAttributeConstants;
+import org.archive.crawler.datamodel.CrawlURI;
+import org.archive.util.ArchiveUtils;
+
+/**
+ * @author gojomo
+ *
+ */
+public class UriErrorFormatter extends Formatter implements CoreAttributeConstants {
+	
+	/* (non-Javadoc)
+	 * @see java.util.logging.Formatter#format(java.util.logging.LogRecord)
+	 */
+	public String format(LogRecord lr) {
+		CrawlURI curi = (CrawlURI) lr.getParameters()[0];
+		String problem = (String) lr.getParameters()[1];
+				
+		return ArchiveUtils.get17DigitDate()
+		+ " "
+		+ curi
+		+ " "
+		+ lr.getMessage()
+		+ " "
+		+ problem
+		+ "\n";
+	}
+}
+
