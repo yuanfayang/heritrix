b17549948c5cd4f3a8c526220c786c1ac0ce53e1
gojomo <gojomo@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2003-07-17 16:05:25 +0000

    refactoring for clarity/divisibility

--- heritrix/src/org/archive/crawler/basic/SimplePreconditionEnforcer.java
+++ heritrix/src/org/archive/crawler/basic/SimplePreconditionEnforcer.java
@@ -34,63 +34,46 @@ public class SimplePreconditionEnforcer extends Processor implements FetchStatus
 	 */
 	protected void innerProcess(CrawlURI curi) {
 		
-		// if we haven't done a dns lookup  and this isn't a dns uri 
-		// shoot that off and defer further processing
-		if( !curi.getHost().hasBeenLookedUp() && 
-			!curi.getUURI().getUri().getScheme().equals("dns")
-		){
-			logger.info("deferring processing of " + curi.toString() + " for dns lookup." );
-
-			String hostname = curi.getHost().getHostname();
-			
-			// if they passed us an ip don't choke on it
-			if(hostname==null){
-				hostname = curi.getUURI().getUri().getAuthority();
-			}
-			curi.setPrerequisiteUri("dns:" + hostname);
-			curi.incrementDeferrals();
-			curi.cancelFurtherProcessing();			
-			return;
-		}
-		
-		// if we've done a dns lookup and it didn't resolve a host
-		// cancel all processing of this URI
-		if(curi.getHost().hasBeenLookedUp() && curi.getHost().getIP() == null){
-			logger.info("no dns for " + curi.getHost().toString() + " cancelling processing for " + curi.toString() );
-
-			//TODO currently we're using FetchAttempts to denote both fetch attempts and
-			// the choice to not attempt (here).  Eventually these will probably have to be treated seperately
-			// to allow us to treat dns failures and connections failures (downed hosts, route failures, etc) seperately.
-			curi.setFetchStatus(S_DOMAIN_UNRESOLVABLE);
-			curi.incrementFetchAttempts();
-			curi.cancelFurtherProcessing();
-			// should there be a return here? -gm
-			// yes, I think so: -gm
+		if (considerDnsPreconditions(curi)) {
 			return;
 		}
 		
 		// make sure we only process schemes we understand (i.e. not dns)
 		if(!curi.getUURI().getUri().getScheme().equals("http")){
-			// logger.info("PolitenessEnforcer doesn't understand uri's of type "+curi.getUURI().getUri().getScheme() + " (ignoring)");
+			logger.fine("PolitenessEnforcer doesn't understand uri's of type "+curi.getUURI().getUri().getScheme() + " (ignoring)");
 			return;
 		}
 		
-		// for all curis, set appropriate delays
+		if (considerRobotsPreconditions(curi)) {
+			return;
+		}
+
+		// OK, it's allowed
+		
+		// for all curis that will in fact be fetched, set appropriate delays
 		// TODOSOMEDAY: allow per-host, per-protocol, etc. factors
 		curi.setDelayFactor(getDelayFactorFor(curi));
 		curi.setMinimumDelay(getMinimumDelayFor(curi));
+
+		return;
+	}
+
+	/**
+	 * @return
+	 */
+	private boolean considerRobotsPreconditions(CrawlURI curi) {
 		// treat /robots.txt fetches specially
 		if (curi.getUURI().getUri().getPath().equals("/robots.txt")) {
 			// allow processing to continue
-			return; 
+			return false; 
 		}
 		// require /robots.txt if not present
 		if (curi.getHost().getRobotsExpires()<System.currentTimeMillis()) {
-			logger.info("No valid robots for "+curi.getHost()+"; deferring "+curi);
+			logger.fine("No valid robots for "+curi.getHost()+"; deferring "+curi);
 			curi.setPrerequisiteUri("/robots.txt");
 			curi.incrementDeferrals();
 			curi.cancelFurtherProcessing();
-			return;
+			return true;
 		}
 		// test against robots.txt if available
 		String ua = controller.getOrder().getBehavior().getUserAgent();
@@ -99,11 +82,45 @@ public class SimplePreconditionEnforcer extends Processor implements FetchStatus
 			curi.cancelFurtherProcessing();  // turn off later stages
 			curi.setFetchStatus(S_ROBOTS_PRECLUDED);
 			curi.getAList().putString("error","robots.txt exclusion");
-			logger.info("robots.txt precluded "+curi);
-			return;
+			logger.fine("robots.txt precluded "+curi);
+			return true;
 		}
-		// OK, it's allowed
-		return;
+		return false;
+	}
+
+	/**
+	 * @param curi
+	 * @return true if no further processing in this module shoudl occur
+	 */
+	private boolean considerDnsPreconditions(CrawlURI curi) {
+		// if we haven't done a dns lookup  and this isn't a dns uri 
+		// shoot that off and defer further processing
+		if( !curi.getHost().hasBeenLookedUp() && 
+			!curi.getUURI().getUri().getScheme().equals("dns")
+		){
+			logger.fine("deferring processing of " + curi.toString() + " for dns lookup." );
+
+			String hostname = curi.getHost().getHostname();
+			curi.setPrerequisiteUri("dns:" + hostname);
+			curi.incrementDeferrals();
+			curi.cancelFurtherProcessing();			
+			return true;
+		}
+		
+		// if we've done a dns lookup and it didn't resolve a host
+		// cancel all processing of this URI
+		if(curi.getHost().hasBeenLookedUp() && curi.getHost().getIP() == null){
+			logger.fine("no dns for " + curi.getHost().toString() + " cancelling processing for " + curi.toString() );
+
+			//TODO currently we're using FetchAttempts to denote both fetch attempts and
+			// the choice to not attempt (here).  Eventually these will probably have to be treated seperately
+			// to allow us to treat dns failures and connections failures (downed hosts, route failures, etc) seperately.
+			curi.setFetchStatus(S_DOMAIN_UNRESOLVABLE);
+			curi.incrementFetchAttempts();
+			curi.cancelFurtherProcessing();
+			return true;
+		}
+		return false;
 	}
 
 	/**
