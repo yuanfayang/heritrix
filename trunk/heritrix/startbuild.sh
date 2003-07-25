88bfa9329959415e286d20c1c19d638c1c1f6d5f
parkerthompson <parkerthompson@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2003-07-25 23:40:52 +0000

    Improving build scripts mucho.

--- trunk/heritrix/startbuild.sh
+++ trunk/heritrix/startbuild.sh
@@ -8,34 +8,60 @@
 ##							#
 ######################################################### 
 
-## path to ant's root directory
-export ANT_HOME=<PUT ANT HOME HERE>
+# make sure they specified a build file
+if [ ! $1 ]; then
+	echo ""
+	echo "  usage: $0 <build file> [target]"
+	echo ""
+	exit
+fi
 
-## path to java home (not java executible)
-export JAVA_HOME=<PUT JAVA HOME HERE>
+# make sure CRAWL_HOME is set
+if [ ! $CRAWL_HOME ]; then
+        echo ""
+        echo "  Before running this script you must set"
+        echo "  the environemnt variable CRAWL_HOME"
+        echo ""
+        exit
+fi
+                                                                                
+## the following block reads common (shared) environment
+## variables from the configuration file
+                                                                                
+# set field seperator to split on newlines
+IFS='
+'
+for line in `cat $CRAWL_HOME/environment.txt`
+do
+        notcomment=${line##\#*}
+        if [ $notcomment ]; then
+                export $line
+        fi
+done
 
-## method to authenticate
-## set to 'ssh' if you're using a secure non-anon connection
-## otherwise leaving this commented out is ok
-#export CVS_RSH="ssh"
 
-
-## don't worry about stuff below this line (unless you want to)
+## make sure we got what we needed from the 
+## env var file
+if [ ! $JAVA_HOME ] || [ ! ANT_HOME ]; then
+	echo ""
+	echo "  You have failed to set (manually or via the 'environment.txt'"
+	echo "  file either JAVA_HOME, ANT_HOME, or both.  Please make sure "
+	echo "  these are set before running this script.  See the docs for "
+	echo "  more information."
+	echo ""
+	exit
+fi
 
 export PATH=$PATH:${ANT_HOME}/bin
 export CVSREAD=1
 export CVSROOT=/home/cvs
 
-echo "If you experience errors note that this"
-echo "script needs to be run from the directory"
-echo "in which it lives."
-
 ## run ant with the first target passed (e.g. clean, all, nightly)
 ## or nightly if no arg is given
 if [ $1 ]
 then
-	ant -f build.xml -logger org.apache.tools.ant.listener.MailLogger $1
+	ant -f $1 -logger org.apache.tools.ant.listener.MailLogger $2
 else
 	echo "executing default build (localbuild)"
-	ant -f build.xml -logger org.apache.tools.ant.listener.MailLogger localbuild
+	ant -f $1 -logger org.apache.tools.ant.listener.MailLogger localbuild
 fi
