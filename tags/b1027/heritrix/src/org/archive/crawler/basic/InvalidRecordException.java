3e71c7a1df581f2cd16717d1815bf341252c9bc8
nobody <nobody@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2003-08-07 22:03:16 +0000

    This commit was manufactured by cvs2svn to create tag 'b1027'.

---
+++ tags/b1027/heritrix/src/org/archive/crawler/basic/InvalidRecordException.java
@@ -0,0 +1,23 @@
+/*
+ * Created on Jun 19, 2003
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package org.archive.crawler.basic;
+
+/**
+ * @author Parker Thompson
+ *
+ */
+public class InvalidRecordException extends Exception {
+
+	public InvalidRecordException(){
+		super();
+	}
+	
+	public InvalidRecordException(String s){
+		super(s);
+	}
+
+}
