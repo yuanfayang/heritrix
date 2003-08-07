b891615095d9df5fbc52f3f82e678f8e45c8ca4a
nobody <nobody@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2003-08-07 22:03:16 +0000

    This commit was manufactured by cvs2svn to create tag 'b1023'.

---
+++ tags/b1023/heritrix/src/org/archive/crawler/datamodel/CrawlServer.java
@@ -0,0 +1,142 @@
+/*
+ * CrawlServer.java
+ * Created on Apr 17, 2003
+ *
+ * $Header$
+ */
+package org.archive.crawler.datamodel;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.zip.Checksum;
+
+import org.apache.commons.httpclient.methods.GetMethod;
+
+/**
+ * Represents a single remote "host". 
+ * 
+ * @author gojomo
+ *
+ */
+public class CrawlServer {
+	public static long DEFAULT_ROBOTS_VALIDITY_DURATION = 1000*60*60*24; // one day 
+	String server; // actually, host+port in the http case
+	CrawlHost host;
+	RobotsExclusionPolicy robots;
+	long robotsExpires = -1;
+	Checksum robotstxtChecksum;
+	
+	/**
+	 * @param h
+	 */
+	public CrawlServer(String h) {
+		// TODO: possibly check for illegal host string
+		server = h;
+	}
+	
+	/**
+	 * @return
+	 */
+	public long getRobotsExpires() {
+		return robotsExpires;
+	}
+
+	/**
+	 * @param l
+	 */
+	public void setRobotsExpires(long l) {
+		robotsExpires = l;
+	}
+
+	/**
+	 * @return
+	 */
+	public RobotsExclusionPolicy getRobots() {
+		return robots;
+	}
+
+	/**
+	 * @param policy
+	 */
+	public void setRobots(RobotsExclusionPolicy policy) {
+		robots = policy;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	public String toString() {
+		return "CrawlServer("+server+")";
+	}
+
+	/**
+	 * @param get
+	 */
+	public void updateRobots(GetMethod get) {
+		robotsExpires = System.currentTimeMillis()+DEFAULT_ROBOTS_VALIDITY_DURATION;
+		if (get.getStatusCode()==404) {
+			// not found == all ok
+			robots = RobotsExclusionPolicy.ALLOWALL;
+			return;
+		}
+//	PREVAILING PRACTICE PER GOOGLE: treat these errors as all-allowed, 
+//  since they're usually indicative of a mistake
+//      if ((get.getStatusCode() >= 401) && (get.getStatusCode() <= 403)) {
+//			// authorization/allowed errors = all deny
+//			robots = RobotsExclusionPolicy.DENYALL;
+//			return;
+//		}
+		// TODO: handle other errors, perhaps redirects
+		// note that akamai will return 400 for some "not founds"
+		try {
+			BufferedReader reader = new BufferedReader(
+					new InputStreamReader(
+						get.getResponseBodyAsStream()));
+			robots = RobotsExclusionPolicy.policyFor(reader);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			robots = RobotsExclusionPolicy.ALLOWALL;
+		}
+		return;
+	}
+	
+	 
+	public boolean isRobotsExpired() {
+		if (robotsExpires >= 0 && robotsExpires < System.currentTimeMillis()) {
+			return true;
+		}
+		return false;
+	}
+	 
+	public String getServer(){
+	   return server;
+	}
+	
+	/**
+	 * @return
+	 */
+	public CrawlHost getHost() {
+		return host;
+	}
+
+	/**
+	 * @param host
+	 */
+	public void setHost(CrawlHost host) {
+		this.host = host;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getHostname() {
+		int colonIndex = server.indexOf(":");
+		if(colonIndex < 0) {
+			return server;
+		}
+		return server.substring(0,colonIndex);
+	}
+
+}
