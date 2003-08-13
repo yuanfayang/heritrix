a0628c130abda4b0cb9d95abc0a9fd24f993a783
nobody <nobody@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2003-08-13 19:17:29 +0000

    This commit was manufactured by cvs2svn to create tag 'b1037'.

---
+++ tags/b1037/heritrix/src/org/archive/crawler/framework/Filter.java
@@ -0,0 +1,47 @@
+/* 
+ * Filter.java
+ * Created on Apr 16, 2003
+ *
+ * $Header$
+ */
+package org.archive.crawler.framework;
+
+/**
+ * 
+ * @author Gordon Mohr
+ */
+public abstract class Filter extends XMLConfig {
+	String name;
+	boolean inverter = false;
+	
+	public  void setName(String n) {
+		name = n;
+	}
+	public String getName() {
+		return name;
+	}
+	
+	public boolean accepts(Object o) {
+		return inverter ^ innerAccepts(o);
+	}
+	
+	/**
+	 * @param o
+	 * @return
+	 */
+	protected abstract boolean innerAccepts(Object o);
+	
+	public void initialize() {
+		setName(getStringAt("@name"));
+		if("not".equals(getStringAt("@modifier"))) {
+			inverter = true;
+		}
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	public String toString() {
+		return "Filter<"+name+">";
+	}
+
+}
