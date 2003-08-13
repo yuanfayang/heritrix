5c18b1ee832d13f51460e60e27e92ec929798167
nobody <nobody@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2003-08-13 19:17:29 +0000

    This commit was manufactured by cvs2svn to create tag 'b1043'.

---
+++ tags/b1043/heritrix/src/org/archive/crawler/Heritrix.java
@@ -0,0 +1,92 @@
+/*
+ * Main.java
+ * Created on May 15, 2003
+ *
+ * $Header$
+ */
+package org.archive.crawler;
+
+import java.util.logging.Logger;
+
+import org.archive.crawler.admin.SimpleHttpServer;
+import org.archive.crawler.datamodel.CrawlOrder;
+import org.archive.crawler.datamodel.InitializationException;
+import org.archive.crawler.framework.CrawlController;
+
+/**
+ * Main class for Heritrix crawler.
+ * 
+ * Currently takes a single command-line argument, which
+ * should be an XML crawl-order file describing the crawl to 
+ * undertake, and begins that crawl.
+ * 
+ * (Eventually, will start web UI and await further
+ * instructions.)
+ * 
+ * @author gojomo
+ *
+ */
+public class Heritrix {
+	private static Logger logger =
+		Logger.getLogger("org.archive.crawler.Heritrix");
+
+	public static void main(String[] args) {
+		(new Heritrix()).instanceMain(args);
+	}
+
+	public void instanceMain(String[] args) {
+		// Default crawlOrder
+		String crawlOrderFile = "test-config/order.xml";
+		CrawlOrder order;
+		CrawlController controller = new CrawlController();
+
+		try {
+			switch (args.length) {
+				case 1 :
+					crawlOrderFile = args[0];
+				case 0 :
+					order = CrawlOrder.readFromFile(crawlOrderFile);
+					controller.setOrder(order);
+					startServer(controller);
+					break;
+				case 2 :
+					if (args[0].equals("-no-wui")) {
+						crawlOrderFile = args[1];
+						order = CrawlOrder.readFromFile(crawlOrderFile);
+						controller.initialize(order);
+						controller.startCrawl();
+						break;
+					}
+				default :
+					usage();
+					return;
+			}
+//		} catch (IOException e) {
+//			System.out.println("Unable to read order file: " + e);
+//			return;
+
+		// catch all configuration exceptions, which at this level are fatal
+		}catch(InitializationException e){
+			System.out.println("Fatal configuration exception: " + e.toString());
+		}
+		
+		logger.info("exitting main thread");
+	}
+	
+	private void usage() {
+		System.out.println(
+			"USAGE: java Heritrix [-no-wui] order.xml");
+		System.out.println(
+				"\t-no-wui start crawler without Web User Interface");
+	}
+	private void startServer(CrawlController c) {
+		SimpleHttpServer server = new SimpleHttpServer();
+		server.setControler(c);
+		try {
+			server.startServer();
+		} catch (Exception e) {
+			e.printStackTrace(System.out);
+		}
+
+	}
+}
