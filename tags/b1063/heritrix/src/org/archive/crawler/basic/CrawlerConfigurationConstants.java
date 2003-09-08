52f1731a93e6150695b992d8c2156d7d9ef302fc
nobody <nobody@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2003-09-08 23:35:00 +0000

    This commit was manufactured by cvs2svn to create tag 'b1063'.

---
+++ tags/b1063/heritrix/src/org/archive/crawler/basic/CrawlerConfigurationConstants.java
@@ -0,0 +1,16 @@
+/*
+ * Created on Jul 29, 2003
+ *
+ */
+package org.archive.crawler.basic;
+
+/**
+ * @author Parker Thompson
+ *
+ */
+public interface CrawlerConfigurationConstants {
+	
+	public static final String DEFAULT_CRAWLER_NAME = "HerItrix";
+	public static final String DEFAULT_USER_AGENT = "Heritrix pre-Alpha (contact gojomo@archive.org)";
+	public static final int DEFAULT_STATISTICS_REPORT_INTERVAL = 60;
+}
