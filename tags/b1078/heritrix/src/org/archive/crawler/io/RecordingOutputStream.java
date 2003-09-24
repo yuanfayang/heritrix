aaa0d014cd9785033580fba72b486823c88fbdaa
nobody <nobody@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2003-09-24 01:46:37 +0000

    This commit was manufactured by cvs2svn to create tag 'b1078'.

---
+++ tags/b1078/heritrix/src/org/archive/crawler/io/RecordingOutputStream.java
@@ -0,0 +1,26 @@
+/*
+ * ReplayableOutputStream.java
+ * Created on Sep 23, 2003
+ *
+ * $Header$
+ */
+package org.archive.crawler.io;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * @author gojomo
+ *
+ */
+public class RecordingOutputStream extends OutputStream {
+
+	/* (non-Javadoc)
+	 * @see java.io.OutputStream#write(int)
+	 */
+	public void write(int b) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
