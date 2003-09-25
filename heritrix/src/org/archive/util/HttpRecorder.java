6f5aa037b03e1485c6dbecbfb2e0b6353dd59fb7
gojomo <gojomo@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2003-09-25 00:14:03 +0000

    http byte-level recording (in progress)

--- heritrix/src/org/archive/util/HttpRecorder.java
+++ heritrix/src/org/archive/util/HttpRecorder.java
@@ -6,8 +6,12 @@
  */
 package org.archive.util;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+
+import org.archive.crawler.io.RecordingInputStream;
+import org.archive.crawler.io.RecordingOutputStream;
 
 /**
  * Initially only supports HTTP/1.0 (one request, one response per stream)
@@ -16,21 +20,35 @@ import java.io.OutputStream;
  *
  */
 public class HttpRecorder {
+	String backingFilenamePrefix;
+	RecordingInputStream ris;
+	RecordingOutputStream ros;
+	
+	/**
+	 * 
+	 */
+	public HttpRecorder(String backingFilenamePrefix) {
+		super();
+		ris = new RecordingInputStream(32768,backingFilenamePrefix+".ris",2^20);
+		ros = new RecordingOutputStream(2048,backingFilenamePrefix+".ros",2^12);
+	}
 
 	/**
 	 * @param is
 	 * @return
 	 */
-	public InputStream inputWrap(InputStream is) {
-		return is;
+	public InputStream inputWrap(InputStream is) throws IOException {
+		ris.open(is);
+		return ris;
 	}
 
 	/**
 	 * @param outputStream
 	 * @return
 	 */
-	public OutputStream outputWrap(OutputStream outputStream) {
-		return outputStream;
+	public OutputStream outputWrap(OutputStream os) throws IOException {
+		ros.open(os);
+		return ros;
 	}
 	
 }
