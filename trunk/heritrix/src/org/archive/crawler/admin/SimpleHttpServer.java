194bb49ddb6339a36aba3848806287d31688df24
ia_igor <ia_igor@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2003-10-05 22:17:06 +0000

    *** empty log message ***

--- trunk/heritrix/src/org/archive/crawler/admin/SimpleHttpServer.java
+++ trunk/heritrix/src/org/archive/crawler/admin/SimpleHttpServer.java
@@ -7,7 +7,7 @@
 package org.archive.crawler.admin;
 
 import org.mortbay.http.*;
-import org.mortbay.http.handler.*;
+import org.mortbay.jetty.Server;
 import org.archive.crawler.datamodel.CrawlOrder;
 import org.archive.crawler.framework.CrawlController;
 
@@ -20,77 +20,54 @@ import org.archive.crawler.framework.CrawlController;
 public class SimpleHttpServer {
 
 	private int _port;
-	private HttpServer _server = null;
+	private Server _server = null;
 	private static int DEFAULT_PORT = 8080;
 	private static String _contextPath = "/";
 	private static String _resourceBase = "./WebUIResources/";
 	private HttpContext _context = new HttpContext();
 	private CrawlController _controller = new CrawlController();
 	private CrawlOrder _order;
-	public SimpleHttpServer() {
+	public SimpleHttpServer() throws Exception {
 		initialize(DEFAULT_PORT);
 	}
-	public SimpleHttpServer(int port) {
+	public SimpleHttpServer(int port) throws Exception {
 		initialize(port);
 	}
 
-	private void initialize(int port) {
+	private void initialize(int port) throws Exception {
 		if (_server == null) {
-			// Start the crawl
-	
-			_server = new HttpServer();
+			_server = new Server();
 			SocketListener listener = new SocketListener();
 			listener.setPort(port);
 			_server.addListener(listener);
-
-			_context.setContextPath(_contextPath);
-			_context.setResourceBase(_resourceBase);
-			_context.addHandler(new ResourceHandler());
-			_server.addContext(_context);
-
+			_server.addWebApplication("admin", "WebUIResources/webapps/admin/");
 		}
 	}
 
-	public void startServer() throws Exception{
-		// Add Crawler Handler
-		CrawlerHandler handler = new CrawlerHandler(_controller);
-		_context.addHandler(handler);
+	public void startServer() throws Exception {
 		_server.start();
 	}
 	public void stopServer() throws Exception {
 		_server.stop();
 	}
-	/**
-	 * @return
-	 */
+
 	public int getPort() {
 		return _port;
 	}
 
-	/**
-	 * @return
-	 */
 	public HttpServer getServer() {
 		return _server;
 	}
 
-	/**
-	 * @return
-	 */
 	public CrawlController getController() {
 		return _controller;
 	}
-	/**
-	 * 
-	 */
-	public void setControler(CrawlController c){
+
+	public void setControler(CrawlController c) {
 		_controller = c;
 	}
-	/**
-	 * 
-	 */
+
 	public void setOrder(CrawlOrder o) {
 		_order = o;
 	}
-
 }
