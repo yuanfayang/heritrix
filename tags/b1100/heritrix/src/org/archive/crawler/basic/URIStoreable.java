77526beced2b442adf7baa30d299f3a927878b7b
nobody <nobody@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2003-10-10 03:14:16 +0000

    This commit was manufactured by cvs2svn to create tag 'b1100'.

---
+++ tags/b1100/heritrix/src/org/archive/crawler/basic/URIStoreable.java
@@ -0,0 +1,40 @@
+/*
+ * KeyedItem.java
+ * Created on Jun 3, 2003
+ *
+ * $Header$
+ */
+package org.archive.crawler.basic;
+
+/**
+ * @author gojomo
+ *
+ */
+public interface URIStoreable {
+
+	public static final Object FORGOTTEN = "FORGOTTEN".intern();
+	public static final Object FINISHED = "FINISHED".intern();;
+	public static final Object HELD = "HELD".intern();
+	public static final Object IN_PROCESS = "IN_PROCESS".intern();
+	public static final Object PENDING = "PENDING".intern();
+	public static final Object READY = "READY".intern();
+	public static final Object SNOOZED = "SNOOZED".intern();
+
+	/**
+	 * @return
+	 */
+	Object getClassKey();
+
+	Object getStoreState();
+	
+	void setStoreState(Object s);
+	
+	long getWakeTime();
+	
+	void setWakeTime(long w);
+
+	/**
+	 * a fallback string to use when wake times are equal
+	 */
+	String getSortFallback();
+}
