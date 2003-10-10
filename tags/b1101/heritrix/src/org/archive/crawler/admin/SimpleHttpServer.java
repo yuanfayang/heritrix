5d7282240e79803d93248b4a1df0d7ff3e0342b8
nobody <nobody@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2003-10-10 03:14:16 +0000

    This commit was manufactured by cvs2svn to create tag 'b1101'.

---
+++ tags/b1101/heritrix/src/org/archive/crawler/admin/SimpleHttpServer.java
@@ -0,0 +1,73 @@
+/*
+ * Created on Jul 11, 2003
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package org.archive.crawler.admin;
+
+import org.mortbay.http.*;
+import org.mortbay.jetty.Server;
+import org.archive.crawler.datamodel.CrawlOrder;
+import org.archive.crawler.framework.CrawlController;
+
+/**
+ * @author Administrator
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class SimpleHttpServer {
+
+	private int _port;
+	private Server _server = null;
+	private static int DEFAULT_PORT = 8080;
+	private static String _contextPath = "/";
+	private static String _resourceBase = "./WebUIResources/";
+	private HttpContext _context = new HttpContext();
+	private CrawlController _controller = new CrawlController();
+	private CrawlOrder _order;
+	public SimpleHttpServer() throws Exception {
+		initialize(DEFAULT_PORT);
+	}
+	public SimpleHttpServer(int port) throws Exception {
+		initialize(port);
+	}
+
+	private void initialize(int port) throws Exception {
+		if (_server == null) {
+			_server = new Server();
+			SocketListener listener = new SocketListener();
+			listener.setPort(port);
+			_server.addListener(listener);
+			_server.addWebApplication("admin", "WebUIResources/webapps/admin/");
+		}
+	}
+
+	public void startServer() throws Exception {
+		_server.start();
+	}
+	public void stopServer() throws Exception {
+		_server.stop();
+	}
+
+	public int getPort() {
+		return _port;
+	}
+
+	public HttpServer getServer() {
+		return _server;
+	}
+
+	public CrawlController getController() {
+		return _controller;
+	}
+
+	public void setControler(CrawlController c) {
+		_controller = c;
+	}
+
+	public void setOrder(CrawlOrder o) {
+		_order = o;
+	}
+}
