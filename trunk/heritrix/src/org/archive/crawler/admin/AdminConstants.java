8da86b94e553fed8af7c189d4c380e41498badc9
kristinn_sig <kristinn_sig@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2003-11-07 00:20:44 +0000

    new order-related constants

--- trunk/heritrix/src/org/archive/crawler/admin/AdminConstants.java
+++ trunk/heritrix/src/org/archive/crawler/admin/AdminConstants.java
@@ -33,11 +33,45 @@ public interface AdminConstants {
 	public static final String XSL_STYLE = "order-conf.xsl";
 	public static final String MAINMENU_JSP = "mainmenu.jsp";
 	public static final String CRAWL_ORDER_JSP = "crawlorder.jsp";
-	public static final String ORDER_HTML_FORM_PAGE = "orderform.html";
+	public static final String ORDER_HTML_FORM_PAGE = "orderform.html"; // TODO: Remove all depracted constants.
 	public static final String STATS_JSP = "stats.jsp";
 	public static final String DEFAULT_ORDER_FILE = "order.xml";
 	public static final String CRAWLING_IN_PROGRESS = "Crawling In Progress";
 	public static final String NO_CRAWLING = "No Crawling in Progress";
 	public static final String NO_ACTIVE_THREADS = "All threads are idle";
 	public static final String CRAWL_ORDER_UPDATED = "Crawl Order Updated";
+	
+	// XPaths
+	// From CrawlOrder
+	public static final String XP_CRAWL_ORDER_NAME = "//crawl-order/@name";
+	public static final String XP_HTTP_USER_AGENT = "//http-headers/@User-Agent";
+	public static final String XP_HTTP_FROM = "//http-headers/@From";
+	public static final String XP_MAX_TOE_THREADS = "//behavior/@max-toe-threads";
+	public static final String XP_ROBOTS_HONORING_POLICY_NAME = "//behavior/robots-honoring-policy/@name";
+	public static final String XP_ROBOTS_HONORING_POLICY_MASQUERADE = "//behavior/robots-honoring-policy/@masquerade";
+	public static final String XP_ROBOTS_HONORING_POLICY_CUSTOM_ROBOTS = "//behavior/robots-honoring-policy/custom-robots";
+	public static final String XP_ROBOTS_HONORING_POLICY_USER_AGENTS = "//behavior/robots-honoring-policy/user-agents/agent";
+	// From CrawlController
+	public static final String XP_STATS_LEVEL = "//loggers/crawl-statistics/@level";
+	public static final String XP_STATS_INTERVAL = "//loggers/crawl-statistics/@interval";
+	public static final String XP_DISK_PATH = "//behavior/@disk-path";
+	public static final String XP_PROCESSORS = "//behavior/processors/processor";
+	public static final String XP_FRONTIER = "//behavior/frontier";
+	public static final String XP_CRAWL_SCOPE = "//scope";
+	// From Frontier
+	public static String XP_DELAY_FACTOR = "@delay-factor";
+	public static String XP_MIN_DELAY = "@min-delay-ms";
+	public static String XP_MAX_DELAY = "@max-delay-ms";
+	
+	// Custom	
+	public static String XP_MAX_TRANS_HOPS = "//scope/@max-trans-hops";
+	public static String XP_MAX_LINK_HOPS = "//scope/@max-link-hops";
+	public static String XP_CRAWL_MODE = "//scope/@mode";
+	
+	public static String XP_ARC_PREFIX = "//@prefix";
+	public static String XP_ARC_COMPRESSION_IN_USE = "//@compress";
+	public static String XP_MAX_ARC_SIZE = "//@max-arc-size";
+	
+	public static final String XP_CRAWL_COMMENT = "//crawl-order/@comment";
+	
 }
