3bb640de48da58da5df9000a3261fea8583fb521
kristinn_sig <kristinn_sig@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2003-11-11 00:05:01 +0000

    Modified the implementation of jobs to make them more modularized.

---
+++ trunk/heritrix/src/org/archive/crawler/admin/SimpleCrawlJob.java
@@ -0,0 +1,204 @@
+package org.archive.crawler.admin;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.archive.crawler.datamodel.CrawlOrder;
+import org.archive.crawler.framework.CrawlJob;
+import org.archive.crawler.framework.exceptions.InitializationException;
+import org.archive.util.ArchiveUtils;
+
+import org.w3c.dom.Node;
+
+/**
+ * @author Kristinn Sigurdsson
+ *
+ * This class encapsulates a crawl job.  This includes, a crawl order filename,
+ * a name for the crawl, it's status etc.
+ */
+
+public class SimpleCrawlJob implements CrawlJob
+{
+	protected String sName;
+	protected String sFilename; //An XML file describing a crawl order (including path)
+	protected String sStatus;
+	protected StatisticsTracker stats;
+	protected boolean isReadOnly = false;
+	protected int priority;
+	
+	protected OrderTransformation orderTransform;
+	protected Node orderNode;
+
+
+	
+	public SimpleCrawlJob(String name, String crawlorderfile, int priority)
+	{
+		this.priority = priority;
+		sName = name;
+		sFilename = crawlorderfile;
+		sStatus = STATUS_CREATED;
+
+	}
+	
+	/**
+	 * @return
+	 */
+	public String getJobName() {
+		return sName;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getStatus() {
+		return sStatus;
+	}
+
+	/**
+	 * @param string
+	 */
+	public void setStatus(String string) {
+		sStatus = string;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.archive.crawler.framework.CrawlJob#setJobName(java.lang.String)
+	 */
+	public void setJobName(String jobname) {
+		sName = jobname;		
+	}
+
+	/* (non-Javadoc)
+	 * @see org.archive.crawler.framework.CrawlJob#getCrawlOrder()
+	 */
+	public CrawlOrder getCrawlOrder() {
+		CrawlOrder co = null;
+		try {
+			co = CrawlOrder.readFromFile(getCrawlOrderFile());
+		} 
+		catch (InitializationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+
+		return co;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.archive.crawler.framework.CrawlJob#setCrawlOrder(java.lang.String)
+	 */
+	public boolean setCrawlOrder(String crawlOrderFile) {
+		if(isReadOnly())
+		{
+			// Can't modify this attribute if read-only
+			return false;
+		}
+		else
+		{
+			sFilename = crawlOrderFile;
+			return true;
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see org.archive.crawler.framework.CrawlJob#getCrawlOrderFile()
+	 */
+	public String getCrawlOrderFile() {
+		// TODO Auto-generated method stub
+		return sFilename;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.archive.crawler.framework.CrawlJob#setJobPriority(int)
+	 */
+	public void setJobPriority(int priority) {
+		this.priority = priority;		
+	}
+
+	/* (non-Javadoc)
+	 * @see org.archive.crawler.framework.CrawlJob#getJobPriority()
+	 */
+	public int getJobPriority() {
+		return priority;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.archive.crawler.framework.CrawlJob#setReadOnly()
+	 */
+	public void setReadOnly() {
+		isReadOnly = true;		
+	}
+
+	/* (non-Javadoc)
+	 * @see org.archive.crawler.framework.CrawlJob#isReadOnly()
+	 */
+	public boolean isReadOnly() {
+		return isReadOnly;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.archive.crawler.framework.CrawlJob#setStatisticsTracker(org.archive.crawler.admin.StatisticsTracker)
+	 */
+	public void setStatisticsTracker(StatisticsTracker tracker) {
+		stats = tracker;	
+	}
+
+	/* (non-Javadoc)
+	 * @see org.archive.crawler.framework.CrawlJob#getStatisticsTracker()
+	 */
+	public StatisticsTracker getStatisticsTracker() {
+		return stats;
+	}
+
+	public void createCrawlOrderFile(HttpServletRequest req, String filename)
+	{
+		Enumeration it = req.getParameterNames();
+		String name;
+		String seedsFileName = null;
+		String seeds = null;
+	
+		while (it.hasMoreElements()) 
+		{
+			name = it.nextElement().toString();
+			String value = req.getParameter(name);
+			if (name.equals("//seeds")) 
+			{
+				seeds = value;
+			} 
+			else 
+			{
+				if (name != null && value != null) 
+				{
+					if (name.equals("//seeds/@src")) 
+					{
+						seedsFileName = value;
+					}
+					orderTransform.setNodeValue(name, value);
+				}
+			}
+		}
+		if (seeds != null && seedsFileName != null) 
+		{
+			try 
+			{
+				BufferedWriter writer =
+					new BufferedWriter(
+						new FileWriter(ArchiveUtils.getFilePath(filename) + seedsFileName));
+				if (writer != null) 
+				{
+					writer.write(seeds);
+					writer.close();
+				}
+			}
+			catch (Exception e) 
+			{
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+		}
+		orderTransform.serializeToXMLFile(AdminConstants.WEB_APP_PATH + filename);
+	}	
+}
--- trunk/heritrix/src/org/archive/crawler/framework/CrawlJobHandler.java
+++ trunk/heritrix/src/org/archive/crawler/framework/CrawlJobHandler.java
@@ -1,23 +1,26 @@
 /**
+ * @author Kristinn Sigurdsson
+ * 
  * Defines a framework for a handler that accepts crawl jobs and excecutes them
  * according to it's programming (sequential as in a queue, paralel on multiple crawlers etc.)
  */
 
 package org.archive.crawler.framework;
 
-import java.util.ArrayList;
+import java.util.Vector;
+
+import org.archive.crawler.datamodel.CrawlOrder;
 
 public interface CrawlJobHandler
 {
-	//	TODO: Define Jobs. Jobs are refered to as 'Object' for now. Related to that, should jobs have uids?
-	
+
 	/**
 	 * Submit a job to the handler
 	 * 
 	 * @param job A new job for the handler
 	 */
 	
-	public void addJob(Object job); 
+	public void addJob(CrawlJob job); 
 	
 	/**
 	 * A list of all pending jobs
@@ -25,7 +28,7 @@ public interface CrawlJobHandler
 	 * @return A list of all pending jobs in an ArrayList.  
 	 * No promises are made about the order of the list
 	 */
-	public ArrayList getPendingJobs();
+	public Vector getPendingJobs();
 	
 	/**
 	 * A list of jobs currently being crawled
@@ -33,23 +36,29 @@ public interface CrawlJobHandler
 	 * @return A list of all jobs currently being crawled (and having gone through the CJH
 	 * framework) as an ArrayList.  
 	 */
-	public ArrayList getCurrentJobs();
+	public Vector getCurrentJobs();
 	
 	/**
 	 * A list of all finished jobs
 	 * 
-	 * @param limit Return only the 'limit' last. Any negative value or 0 will cause the 
-	 * entire list to be returned. 
-	 * 
-	 * @return A list of all finished jobs.  The list is in the order of completion (latest 
-	 * completed first).  No promises are made about how long finished jobs are remembered.
+	 * @return A list of all finished jobs as a Vector.
 	 */
-	public ArrayList getFinishedJobs(int limit);
+	public Vector getCompletedJobs();
 	
 	/**
-	 * Cancel a specific job.  
+	 * Remove a specific job that is pending.
 	 * 
-	 * @param job The job that is to be cancelled.
+	 * @param job The job that is to be removed.
+	 * 
+	 * @return false if the job is no longer in the queue - has begun running or was removed 
+	 * by another thread. The job is removed and true is returned otherwise.     
 	 */
-	public void cancelJob(Object job);
+	public boolean removeJob(CrawlJob job);
+	
+	/**
+	 * Returns the default crawl order - all new crawl orders are based on this
+	 * 
+	 * @return the default crawl order
+	 */
+	public CrawlOrder getDefaultCrawlOrder();
 }
