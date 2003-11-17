7e0fc29d8943ecf649fc0bef0914edec88466c82
johnerik <johnerik@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2003-11-17 20:46:25 +0000

    Added to new methods to make it possible for code that need to interact with a Matcher object to reuse those objects as well.

--- trunk/heritrix/src/org/archive/util/TextUtils.java
+++ trunk/heritrix/src/org/archive/util/TextUtils.java
@@ -106,6 +106,47 @@ public class TextUtils {
 	final static Hashtable patternMatchers = new Hashtable(50); // Resuable match objects
 	
 	/**
+	 * Get a matcher object for a precompiled regex pattern.
+	 * This method tries to reuse Matcher objects for efficiency.
+	 * 
+	 * @param p the precompiled Pattern
+	 * @param input the character sequence the matcher should be using
+	 * @return a matcher object loaded with the submitted character sequence
+	 */
+	public static Matcher getMatcher(Pattern p, CharSequence input) {
+		Stack matchers;
+		if((matchers = (Stack) patternMatchers.get(p)) == null) {
+			matchers = new Stack();
+			patternMatchers.put(p, matchers);
+		}
+		Matcher matcher;
+		try {
+			matcher = (Matcher) matchers.pop();
+			matcher.reset(input);
+		} catch(EmptyStackException e) {
+			matcher = p.matcher(input);
+		}
+		return matcher;
+	}
+
+	/**
+	 * Use this method to indicate that you are finnished with a Matcher object so
+	 * that it can be recycled. It is up to the user to make sure that this object really isn't
+	 * used anymore. If used after it is marked as freed behaviour is unknown because
+	 * the Matcher object is not thread safe.
+	 * 
+	 * @param m the Matcher object that is no longer needed.
+	 */
+	public static void freeMatcher(Matcher m) {
+		Stack matchers;
+		if((matchers = (Stack) patternMatchers.get(m.pattern())) == null) {
+			// This matcher wasn't created by any pattern in the map, throw it away
+			return;
+		}
+		matchers.push(m);
+	}
+
+	/**
 	 * Utility method using a precompiled pattern instead of using the replaceAll method of
 	 * the String class. This method will also be reusing Matcher objects.
 	 * 
