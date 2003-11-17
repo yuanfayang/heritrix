e570e5423cb30e7d1f7859eaaf5d79af62ec4b26
gojomo <gojomo@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2003-11-17 14:54:21 +0000

    thread-info convenience method

--- heritrix/src/org/archive/util/DevUtils.java
+++ heritrix/src/org/archive/util/DevUtils.java
@@ -10,18 +10,28 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.logging.Logger;
 
+import org.archive.crawler.framework.ToeThread;
+
 /**
  * @author gojomo
  *
  */
 public class DevUtils {
-	private static Logger logger = Logger.getLogger("org.archive.util.DevUtils");
+	public static Logger logger = Logger.getLogger("org.archive.util.DevUtils");
 
-	public static void warnHandle(Exception ex, String note) {
+	public static void warnHandle(Throwable ex, String note) {
 		StringWriter sw = new StringWriter();
 		sw.write(note);
 		sw.write("\n");
 		ex.printStackTrace(new PrintWriter(sw));
 		logger.warning(sw.toString());
+	}
+	
+	public static String extraInfo() {
+		Thread current = Thread.currentThread();
+		if (current instanceof ToeThread) {
+			return ((ToeThread)current).report();
+		}
+		return "";
 	}
 }
