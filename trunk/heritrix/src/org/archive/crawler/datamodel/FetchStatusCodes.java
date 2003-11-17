7ee193fead0d363e791131094ce01b21493e28ce
gojomo <gojomo@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2003-11-17 14:48:20 +0000

    explicit deferred state

--- trunk/heritrix/src/org/archive/crawler/datamodel/FetchStatusCodes.java
+++ trunk/heritrix/src/org/archive/crawler/datamodel/FetchStatusCodes.java
@@ -29,6 +29,8 @@ public interface FetchStatusCodes {
 	public static int S_UNFETCHABLE_URI = -7;
 	public static int S_TOO_MANY_RETRIES = -8;
 
+	public static int S_DEFERRED = -50;
+
 	public static int S_ROBOTS_PRECLUDED = -9998;
 	public static int S_DEEMED_CHAFF = -4000;
 	public static int S_TOO_MANY_LINK_HOPS = -4001;
