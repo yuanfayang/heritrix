bd3ddc40f57594f95ac3b0a3dc6220b982e4d778
johnerik <johnerik@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2003-11-20 22:56:11 +0000

    Added method for listening to pauses

--- trunk/heritrix/src/org/archive/crawler/framework/CrawlListener.java
+++ trunk/heritrix/src/org/archive/crawler/framework/CrawlListener.java
@@ -16,4 +16,16 @@ public interface CrawlListener
 	 * @param sExitMessage - Type of exit (human readable)
 	 */
 	public void crawlEnding(String sExitMessage);	
+
+	/**
+	 * Called when a CrawlController is going to be paused. It is called again when
+	 * alle threads are idle and the CrawlController actually has paused. It will also
+	 * get called when the CrawlController is resuming from a pause.
+	 * 
+	 * @param statusMessage is @link{CrawlJob#STATUS_WAITING_FOR_PAUSE}
+	 * when CrawlController is asked to pause, and is @link{CrawlJob#STATUS_PAUSED}
+	 * when the CrawlController is paused. When the CrawlController is resuming from a
+	 * pause the statusMessage will be @link{CrawlJob#STATUS_RESUMED}
+	 */
+	public void crawlPausing(String statusMessage);
 }
