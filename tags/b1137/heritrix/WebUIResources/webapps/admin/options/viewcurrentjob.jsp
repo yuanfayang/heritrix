b34e74d3f3a87713f605d53dfcc9ffc481a72a4b
nobody <nobody@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2003-11-22 01:24:13 +0000

    This commit was manufactured by cvs2svn to create tag 'b1137'.

---
+++ tags/b1137/heritrix/WebUIResources/webapps/admin/options/viewcurrentjob.jsp
@@ -0,0 +1,24 @@
+<%@include file="/include/handler.jsp"%>
+<%@include file="/include/secure.jsp"%>
+
+
+	<html>
+		<head>
+			<title>Crawl in progress</title>
+		</head>
+		<body>
+
+		<fieldset style="width: 600px" align="top">
+			<legend>Crawler status</legend>
+			<iframe name="frmStatus" src="/admin/status.jsp?time=10" width="730" height="170" frameborder="0" ></iframe>
+		</fieldset><a href="/admin/main.jsp">Main page</a>
+		<fieldset style="width: 600px" align="top">
+			<legend>Frontier data</legend>
+			<iframe name="frmFrontier" src="/admin/options/viewfrontier.jsp" width="730" height="300" frameborder="0" ></iframe>
+		</fieldset><a href="viewfrontier.jsp">View frontier report in fullscreen</a>
+		<fieldset style="width: 600px" align="top">
+			<legend>Thread data</legend>
+			<iframe name="frmThreads" src="/admin/options/viewthreads.jsp" width="730" height="300" frameborder="0" ></iframe>
+		</fieldset><a href="viewthreads.jsp">View threads report in fullscreen</a>
+		</body>
+	</html>
---
+++ tags/b1137/heritrix/src/org/archive/crawler/admin/AdminConstants.java
@@ -0,0 +1,76 @@
+/*
+ * Created on Oct 1, 2003
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package org.archive.crawler.admin;
+
+/**
+ * @author Administrator
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public interface AdminConstants {
+	public static final String CRAWLER_STARTED = "Crawler Started!";
+	public static final String CRAWLER_STOPPED = "Crawler Stopped!";
+	public static final String CRAWLER_RUNNING_ERR =
+		"ERROR: Couldn't start a new crawl (crawler is already running)";
+	public static final String CRAWLER_NOT_RUNNING_ERR =
+	"ERROR: Couldn't stop crawling (no running crawler was found)";
+	public static final String CRAWLER_TERMINATED =
+		"===>Crawling Stopped!<===<br>\nShutting Down the Server!";
+	public static final String WEB_APP_PATH = "WebUIResources/webapps/admin/";
+	public static final String DEFAULT_ORDER_FILE = "order.xml";
+	public static final String CRAWLING_IN_PROGRESS = "Crawling In Progress";
+	public static final String NO_CRAWLING = "No Crawling in Progress";
+	public static final String NO_ACTIVE_THREADS = "All threads are idle";
+	public static final String CRAWL_ORDER_UPDATED = "Crawl Order Updated";
+	
+	// XPaths
+	// From CrawlOrder
+	public static final String XP_CRAWL_ORDER_NAME = "//crawl-order/@name";
+	public static final String XP_HTTP_USER_AGENT = "//http-headers/@User-Agent";
+	public static final String XP_HTTP_FROM = "//http-headers/@From";
+	public static final String XP_MAX_TOE_THREADS = "//behavior/@max-toe-threads";
+	public static final String XP_ROBOTS_HONORING_POLICY_NAME = "//behavior/robots-honoring-policy/@name";
+	public static final String XP_ROBOTS_HONORING_POLICY_MASQUERADE = "//behavior/robots-honoring-policy/@masquerade";
+	public static final String XP_ROBOTS_HONORING_POLICY_CUSTOM_ROBOTS = "//behavior/robots-honoring-policy/custom-robots";
+	public static final String XP_ROBOTS_HONORING_POLICY_USER_AGENTS = "//behavior/robots-honoring-policy/user-agents/agent";
+	// From CrawlController
+	public static final String XP_STATS_LEVEL = "//loggers/crawl-statistics/@level";
+	public static final String XP_STATS_INTERVAL = "//loggers/crawl-statistics/@interval-seconds";
+	public static final String XP_DISK_PATH = "//behavior/@disk-path";
+	public static final String XP_PROCESSORS = "//behavior/processors/processor";
+	public static final String XP_FRONTIER = "//behavior/frontier";
+	public static final String XP_CRAWL_SCOPE = "//scope";
+	// From Frontier
+	public static final String XP_DELAY_FACTOR = "@delay-factor";
+	public static final String XP_MIN_DELAY = "@min-delay-ms";
+	public static final String XP_MAX_DELAY = "@max-delay-ms";
+	
+	// Custom	
+	public static final String XP_MAX_TRANS_HOPS = "//scope/@max-trans-hops";
+	public static final String XP_MAX_LINK_HOPS = "//scope/@max-link-hops";
+	public static final String XP_CRAWL_MODE = "//scope/@mode";
+	
+	public static final String XP_ARC_PREFIX = "//@prefix";
+	public static final String XP_ARC_COMPRESSION_IN_USE = "//@compress";
+	public static final String XP_MAX_ARC_SIZE = "//@max-arc-size";
+	public static final String XP_ARC_DUMP_PATH = "//processor/@path";
+	
+	public static final String XP_CRAWL_COMMENT = "//crawl-order/@comment";
+	public static final String XP_SEEDS = "//seeds";
+	public static final String XP_SEEDS_FILE = "//seeds/@src";
+	
+	public static final String XP_POLITENESS_DELAY_FACTOR = "//frontier/@delay-factor";
+	public static final String XP_POLITENESS_MIN_DELAY = "//frontier/@min-delay-ms";
+	public static final String XP_POLITENESS_MAX_DELAY = "//frontier/@max-delay-ms";
+	public static final String XP_POLITENESS_MIN_INTERVAL = "//frontier/@min-interval-ms";
+
+	public static final String XP_HTTPFETCH_MAX_FETCH_ATTEMPTS = "//processor/@max-fetch-attempts";
+	public static final String XP_HTTPFETCH_MAX_LENGTH_BYTES = "//processor/@max-length-bytes";
+	public static final String XP_HTTPFETCH_SOTIMEOUT = "//processor/@sotimeout-ms";
+	public static final String XP_HTTPFETCH_TIMEOUT = "//processor/@timeout-seconds";
+}
