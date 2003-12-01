fcbc27511cbc0a80f181cc8f508fc7c75a746d72
johnerik <johnerik@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2003-12-01 21:41:47 +0000

    Fixing bug #848661. Added the possibility to force a prerequisite URI to be fetched even though it already has been fetched and is in the alreadyIncluded map.

--- heritrix/src/org/archive/crawler/datamodel/CoreAttributeConstants.java
+++ heritrix/src/org/archive/crawler/datamodel/CoreAttributeConstants.java
@@ -46,6 +46,7 @@ public interface CoreAttributeConstants {
 	public static String A_LOCALIZED_ERRORS = "localized-errors";
 	
 	public static String A_PREREQUISITE_URI = "prerequisite-uri";
+	public static String A_FORCED_PREREQUISITE_URI = "forced-prerequisite-uri";
 	public static String A_HTML_EMBEDS = "html-embeds";
 	public static String A_HTML_SPECULATIVE_EMBEDS = "html-speculativeembeds";
 	public static String A_DISTANCE_FROM_SEED = "distance-from-seed";
