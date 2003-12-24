c00697068eec0af046bb0a63a326306b1f8d79b4
nobody <nobody@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2003-12-24 20:47:19 +0000

    This commit was manufactured by cvs2svn to create tag 'b1172'.

---
+++ tags/b1172/heritrix/src/java/org/archive/crawler/fetcher/FetchDNS.java
@@ -0,0 +1,216 @@
+/* Copyright (C) 2003 Internet Archive.
+ *
+ * This file is part of the Heritrix web crawler (crawler.archive.org).
+ *
+ * Heritrix is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * any later version.
+ *
+ * Heritrix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with Heritrix; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * SimpleDNSFetcher.java
+ * Created on Jun 5, 2003
+ *
+ * $Header$
+ */
+package org.archive.crawler.fetcher;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.logging.Logger;
+
+import org.archive.crawler.datamodel.CoreAttributeConstants;
+import org.archive.crawler.datamodel.CrawlServer;
+import org.archive.crawler.datamodel.CrawlURI;
+import org.archive.crawler.datamodel.FetchStatusCodes;
+import org.archive.crawler.framework.CrawlController;
+import org.archive.crawler.framework.Processor;
+import org.xbill.DNS.ARecord;
+import org.xbill.DNS.DClass;
+import org.xbill.DNS.Record;
+import org.xbill.DNS.Type;
+import org.xbill.DNS.dns;
+
+
+/**
+ * @author gojomo
+ *
+ */
+public class FetchDNS extends Processor implements CoreAttributeConstants, FetchStatusCodes {
+	private static Logger logger = Logger.getLogger("org.archive.crawler.basic.FetcherDNS");
+	
+ 	// set to false for performance, true if your URIs will contain useful type/class info (usually they won't)
+	public static final boolean DO_CLASS_TYPE_CHECKING = true;
+
+	// defaults
+ 	private short ClassType = DClass.IN;
+ 	private short TypeType = Type.A;
+ 	
+ 	protected InetAddress serverInetAddr = null;
+ 	// protected CrawlServer dnsServer = null;
+
+  	public void initialize(CrawlController c){
+  		super.initialize(c);
+  		
+  		// lookup nameserver
+//		String nameServer = FindServer.server();
+//		
+//		try {
+//			// if we're local get something more useful than the loopback
+//			if (nameServer.equals("127.0.0.1")) {
+//				serverInetAddr = InetAddress.getLocalHost();
+//			} else {
+//				serverInetAddr = InetAddress.getByName(nameServer);
+//			}
+//			
+//			// create a dns host to attach to dns records
+//			dnsServer = new CrawlServer(nameServer);
+//			dnsServer.getHost().setIP(serverInetAddr);
+//			
+//		} catch (UnknownHostException e) {
+//			e.printStackTrace();
+//		}
+  	}
+
+	/* (non-Javadoc)
+	 * @see org.archive.crawler.framework.Processor#process(org.archive.crawler.datamodel.CrawlURI)
+	 */
+	protected void innerProcess(CrawlURI curi) {
+		
+		Record[] rrecordSet = null; 		// store retrieved dns records
+		long now; 									// the time this operation happened
+		CrawlServer targetServer = null;
+		String dnsName = parseTargetDomain(curi);	
+			
+		if (!curi.getUURI().getScheme().equals("dns")) {
+			// only handles dns
+			return;
+		}
+		
+		// curi.setServer(dnsServer);
+		
+		// make sure we're in "normal operating mode", e.g. a cache + controller exist to assist us
+		if (controller != null && controller.getServerCache() != null) {
+			targetServer = controller.getServerCache().getServerFor(dnsName);
+		} else {
+			// standalone operation (mostly for test cases/potential other uses)
+			targetServer = new CrawlServer(dnsName);
+		}
+		
+		// if it's an ip no need to do a lookup
+		if (dnsName.matches("[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}")) {
+			// ideally this branch would never be reached: no CrawlURI
+			// would be created for numerical IPs
+			logger.warning("unnecessary DNS CrawlURI created: "+curi);
+			
+			try {
+				String[] octets = dnsName.split("\\.");
+
+				targetServer.getHost().setIP(
+					InetAddress.getByAddress(
+						dnsName,
+						new byte[] {
+							(byte) (new Integer(octets[0])).intValue(),
+							(byte) (new Integer(octets[1])).intValue(),
+							(byte) (new Integer(octets[2])).intValue(),
+							(byte) (new Integer(octets[3])).intValue()})
+				);
+				
+//				if(targetHost.getIP() == null){
+//					System.out.println("crapped out creating ip address for " + dnsName);
+//				}
+
+			} catch (UnknownHostException e) {
+				// this should never happen as a dns lookup is not made
+			
+				e.printStackTrace();
+			}
+			
+			// don't expire numeric IPs
+			targetServer.getHost().setIpExpires(Long.MAX_VALUE);
+			curi.setFetchStatus(S_DNS_SUCCESS);
+			
+			// no further lookup necessary
+			return;
+		}
+				
+//		if(curi.getFetchAttempts() >= MAX_DNS_FETCH_ATTEMPTS){
+//			curi.setFetchStatus(S_DOMAIN_UNRESOLVABLE);
+//			return;
+//		}
+					
+		// give it a go    
+		//curi.incrementFetchAttempts();
+
+		//TODO add support for type and class specifications in query string, for now always use defaults
+		/* if(SimpleDNSFetcher.DO_CLASS_TYPE_CHECKING){
+		} */
+		
+		now = System.currentTimeMillis();
+		curi.getAList().putLong(A_FETCH_BEGAN_TIME, now);
+			
+		// try to get the records for this host (assume domain name)
+		rrecordSet = dns.getRecords(dnsName, TypeType, ClassType);
+		
+		targetServer.getHost().setHasBeenLookedUp();
+		
+		if (rrecordSet != null) {
+			curi.setFetchStatus(S_DNS_SUCCESS);
+			curi.getAList().putString(A_CONTENT_TYPE, "text/dns");
+			curi.getAList().putObject(A_RRECORD_SET_LABEL, rrecordSet);
+
+			// get TTL and IP info from the first A record (there may be multiple, e.g. www.washington.edu)
+			// then update the CrawlServer
+			for (int i = 0; i < rrecordSet.length; i++) {
+
+				if (rrecordSet[i].getType() != Type.A) {
+					continue;
+				}
+
+				ARecord AsA = (ARecord) rrecordSet[i];
+				targetServer.getHost().setIP(AsA.getAddress());
+				targetServer.getHost().setIpExpires(1000 * (long) AsA.getTTL() + now);
+				break; // only need to process one record
+			}
+		} else {
+			curi.setFetchStatus(S_DOMAIN_UNRESOLVABLE);
+		}
+		
+		curi.getAList().putLong(A_FETCH_COMPLETED_TIME, System.currentTimeMillis());
+	}
+	
+	// TODO should throw some sort of exception if it's passed
+	// a non-dns uri.  currently assumes the caller knows what he/she is doing
+	public static String parseTargetDomain(CrawlURI curi){
+
+		// should look like "dns:" [ "//" hostport "/" ] dnsname [ "?" dnsquery ]
+		String uri = curi.getURIString();
+
+		// if it's not a dns uri
+		if(!uri.startsWith("dns:")){
+			return null;
+		}
+		
+		uri = uri.substring(4);						// drop "dns:" prefix
+			
+		if(uri.startsWith("//")){						// drop hostport
+			uri = uri.replaceFirst("//.+/", "");
+		}
+		
+		// drop query string
+		if(uri.indexOf("?") > -1){				
+			uri = uri.substring(0, uri.indexOf("?"));
+		}
+		
+		return uri;
+	}
+	
+}
---
+++ tags/b1172/heritrix/src/java/org/archive/util/DiskQueue.java
@@ -0,0 +1,152 @@
+/* Copyright (C) 2003 Internet Archive.
+ *
+ * This file is part of the Heritrix web crawler (crawler.archive.org).
+ *
+ * Heritrix is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * any later version.
+ *
+ * Heritrix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with Heritrix; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * DiskQueue.java
+ * Created on Oct 16, 2003
+ *
+ * $Header$
+ */
+package org.archive.util;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.NoSuchElementException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.archive.io.DiskBackedByteQueue;
+import org.archive.io.NullOutputStream;
+
+/**
+ * Queue which stores all its objects to disk using object
+ * serialization, on top of a DiskBackedByteQueue. 
+ *
+ * The serialization state is reset after each enqueue(). 
+ * Care should be taken not to enqueue() items which will
+ * pull out excessive referenced objects, or objects which
+ * will be redundantly reinstantiated upon dequeue() from 
+ * disk. 
+ * 
+ * @author Gordon Mohr
+ */
+public class DiskQueue implements Queue {
+	private static Logger logger = Logger.getLogger("org.archive.util.DiskQueue");
+
+	private File scratchDir;
+	String name;
+	long length;
+	DiskBackedByteQueue bytes;
+	ObjectOutputStream testStream; // to verify that object is serializable
+	ObjectOutputStream tailStream;
+	ObjectInputStream headStream;
+	
+	/**
+	 * 
+	 */
+	public DiskQueue(File dir, String name) {
+		length = 0;
+		this.name = name;
+		this.scratchDir = dir;
+		// TODO someday: enable queue to already be filled
+	}
+	
+	private void lateInitialize() throws FileNotFoundException, IOException {
+		bytes = new DiskBackedByteQueue(scratchDir,this.name);
+		testStream = new ObjectOutputStream(new NullOutputStream());
+		tailStream = new ObjectOutputStream(bytes.getTailStream());
+		headStream = new ObjectInputStream(bytes.getHeadStream());
+	}
+
+	/* (non-Javadoc)
+	 * @see org.archive.util.Queue#enqueue(java.lang.Object)
+	 */
+	public void enqueue(Object o){
+		//logger.finest(name+"("+length+"): "+o);
+		try {
+			if(bytes==null) {
+				lateInitialize();
+			}
+			// TODO: optimize this, for example by serializing to buffer, then writing to disk on success
+			testStream.writeObject(o);
+			testStream.reset();
+			tailStream.writeObject(o);
+			tailStream.reset(); // forget state with each enqueue
+			length++;
+		} catch (IOException e) {
+			// TODO convert to runtime exception?
+			DevUtils.logger.log(Level.SEVERE,"enqueue("+o+")"+DevUtils.extraInfo(),e);
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see org.archive.util.Queue#isEmpty()
+	 */
+	public boolean isEmpty() {
+		return length==0;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.archive.util.Queue#dequeue()
+	 */
+	public Object dequeue() {
+		if (isEmpty()) {
+			throw new NoSuchElementException();
+		}
+		Object o;
+		try {
+			o = headStream.readObject();
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new NoSuchElementException();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			throw new NoSuchElementException();
+		}
+		// logger.finest(name+"("+length+"): "+o);
+		length--;
+		return o;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.archive.util.Queue#length()
+	 */
+	public long length() {
+		return length;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.archive.util.Queue#release()
+	 */
+	public void release() {
+		if (bytes != null) {
+			try {
+				headStream.close();
+				tailStream.close();
+				bytes.discard();
+
+			} catch (IOException e) {
+				// TODO: convert to runtime? 
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
