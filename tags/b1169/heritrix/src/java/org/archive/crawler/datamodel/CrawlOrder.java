a84401cfed23b797a4db641a97d93ea9e9f75ae6
nobody <nobody@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2003-12-24 20:47:19 +0000

    This commit was manufactured by cvs2svn to create tag 'b1169'.

---
+++ tags/b1169/heritrix/src/java/org/archive/crawler/datamodel/CrawlOrder.java
@@ -0,0 +1,220 @@
+/* Copyright (C) 2003 Internet Archive.
+ *
+ * This file is part of the Heritrix web crawler (crawler.archive.org).
+ *
+ * Heritrix is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * any later version.
+ *
+ * Heritrix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with Heritrix; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * CrawlOrder.java
+ * Created on May 15, 2003
+ *
+ * $Header$
+ */
+package org.archive.crawler.datamodel;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.archive.crawler.framework.XMLConfig;
+import org.archive.crawler.framework.exceptions.*;
+import org.w3c.dom.Document;
+
+/** Read and manipulate configuration (order) file.
+ */
+public class CrawlOrder extends XMLConfig {
+	private static final String XP_CRAWL_ORDER_NAME = "//crawl-order/@name";
+	private static final String XP_HTTP_USER_AGENT = "//http-headers/@User-Agent";
+	private static final String XP_HTTP_FROM = "//http-headers/@From";
+	private static final String XP_MAX_TOE_THREADS = "//behavior/@max-toe-threads";
+	private static final String XP_ROBOTS_HONORING_POLICY_NAME = "//behavior/robots-honoring-policy/@name";
+	private static final String XP_ROBOTS_HONORING_POLICY_MASQUERADE = "//behavior/robots-honoring-policy/@masquerade";
+	private static final String XP_ROBOTS_HONORING_POLICY_CUSTOM_ROBOTS = "//behavior/robots-honoring-policy/custom-robots";
+	private static final String XP_ROBOTS_HONORING_POLICY_USER_AGENTS = "//behavior/robots-honoring-policy/user-agents/agent";
+	
+	String caseFlattenedUserAgent;
+	String name;
+    String outputLocation;
+	String crawlOrderFilename;
+	RobotsHonoringPolicy robotsHonoringPolicy = null;
+	
+	/**
+	 * Create a CrawlOrder instance from the given file.
+	 * 
+	 * @param crawlOrderFile
+	 * @return the created CrawlOrder
+	 */
+	public static CrawlOrder readFromFile(String crawlOrderFile) throws InitializationException {
+		
+		String pathToDoc = null;
+		Document doc = null;
+		
+		// try to extract a default path for later file reading
+		int pathEnd = crawlOrderFile.lastIndexOf(File.separatorChar);
+		if(pathEnd >=0){
+			pathToDoc = crawlOrderFile.substring(0,pathEnd);
+		}
+		
+		try{
+			doc = readDocumentFromFile(crawlOrderFile);
+		}catch(IOException e){
+			throw new InitializationException("Can't read from '" + crawlOrderFile + "': " + e.toString(), e);
+		}
+		
+		
+		CrawlOrder co = new CrawlOrder(doc, pathToDoc);				
+//		XMLConfig behave = co.getBehavior();
+//		if(behave != null){
+//			behave.setDefaultFileLocation(pathToDoc);
+//		}
+		
+		return co;
+	}
+	
+	/** Construct a CrawlOrder instance given a Document */
+	public CrawlOrder(Document doc) throws InitializationException {
+		this(doc, ".");
+	}
+
+	/** Construct a CrawlOrder instance given a Document and a path to that
+	 *  document.  The path is necessary in the event that the config file inherits
+	 *  from another config file (so we can locate that on the file sytem).
+	 * @param doc
+	 * @param pathToDoc
+	 * @throws InitializationException
+	 */
+	public CrawlOrder(Document doc, String pathToDoc)
+		throws InitializationException {
+		xNode = doc;
+
+		if (pathToDoc != null) {
+			defaultFilePath = pathToDoc;
+		} else {
+			defaultFilePath = ".";
+		}
+		
+		//loadParents(pathToDoc);
+		initialize();
+
+	}
+	
+	/** Load common configuration variables from config file.  Should only 
+	 *  be called on the primary (non-inherited from) crawl order.
+	 */
+	public void initialize(){
+		name = getStringAt(XP_CRAWL_ORDER_NAME);
+	}
+	
+	protected void loadParents(String pathToDoc) throws InitializationException {
+		
+		// try to read any configuration files which this extends
+		// if this fails let the Initialization exception bubble up
+		String parentFileName = getStringAt("//crawl-order/@extends");
+		//TODO check for infinite loop (parent == self)
+		if (parentFileName != null && parentFileName.length() != 0) {
+			try {
+				if(isAbsoluteFilePath(parentFileName)){
+					parentConfigurationFile = 
+						readFromFile(parentFileName);
+				}else{
+					parentConfigurationFile =
+						readFromFile(pathToDoc + File.separator + parentFileName);
+				}
+				// can't read parent, proceed as best we can.
+			} catch (InitializationException e) {
+				System.out.println(
+					"Unable to read config file '"
+						+ pathToDoc
+						+ File.separator
+						+ parentFileName
+						+ "' (non fatal), continuing...");
+			}
+		}
+	}
+
+	/**
+	 * @return
+	 */
+	public String getName() {
+		// if this node doesn't have it but we have a parent conf file check that
+		if(name == null && parentConfigurationFile != null){
+			return ((CrawlOrder)parentConfigurationFile).getName();
+		}
+		return name;
+	}
+
+	public String getOutputLocation(){
+		if(outputLocation == null && parentConfigurationFile != null){
+			return ((CrawlOrder)parentConfigurationFile).getOutputLocation();
+		}
+		return outputLocation;
+	}
+	
+	/**
+	 * @return
+	 */
+	public String getUserAgent() {
+		if (caseFlattenedUserAgent==null) {
+			caseFlattenedUserAgent =  getStringAt(XP_HTTP_USER_AGENT).toLowerCase();
+		}
+		return caseFlattenedUserAgent;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getFrom() {
+		return getStringAt(XP_HTTP_FROM);
+	}
+	
+	/**
+		 * @return
+	*/
+	public int getMaxToes() {
+		return getIntAt(XP_MAX_TOE_THREADS);
+	}
+
+	/**
+	 * 
+	 */
+	public String getCrawlOrderFilename() {
+		return crawlOrderFilename;
+	}
+
+	/**
+	 * This method constructs a new RobotsHonoringPolicy object from the orders file.
+	 * 
+	 * If this method is called repeatedly it will return the same instance each time.
+	 * 
+	 * @return the new RobotsHonoringPolicy
+	 */
+	public RobotsHonoringPolicy getRobotsHonoringPolicy() {
+		if (robotsHonoringPolicy==null) {
+			robotsHonoringPolicy = new RobotsHonoringPolicy(getStringAt(XP_ROBOTS_HONORING_POLICY_NAME));
+			robotsHonoringPolicy.setMasquerade(getStringAt(XP_ROBOTS_HONORING_POLICY_MASQUERADE));
+			
+			// if the policy type is custom, we should look up the admins robots.txt file
+			if(robotsHonoringPolicy.isType(RobotsHonoringPolicy.CUSTOM)) {
+				robotsHonoringPolicy.setCustomRobots(getStringAt(XP_ROBOTS_HONORING_POLICY_CUSTOM_ROBOTS));
+			}
+			if (robotsHonoringPolicy.isType(RobotsHonoringPolicy.MOST_FAVORED_SET)) {
+				Iterator iter = getTextNodesAt(xNode, XP_ROBOTS_HONORING_POLICY_USER_AGENTS).iterator();
+				while (iter.hasNext()) {
+					robotsHonoringPolicy.addUserAgent((String) iter.next());
+				}
+			}
+		}
+		return robotsHonoringPolicy;
+	}
+}
---
+++ tags/b1169/heritrix/src/java/org/archive/crawler/framework/CrawlScope.java
@@ -0,0 +1,136 @@
+/* Copyright (C) 2003 Internet Archive.
+ *
+ * This file is part of the Heritrix web crawler (crawler.archive.org).
+ *
+ * Heritrix is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * any later version.
+ *
+ * Heritrix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with Heritrix; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * CrawlScope.java
+ * Created on Oct 1, 2003
+ *
+ * $Header$
+ */
+package org.archive.crawler.framework;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.archive.crawler.datamodel.CandidateURI;
+import org.archive.crawler.util.SeedsInputIterator;
+import org.archive.util.DevUtils;
+
+/**
+ * A CrawlScope instance defines which URIs are "in"
+ * a particular crawl. 
+ * 
+ * It is essentially a Filter which determines, looking at 
+ * the totality of information available about a 
+ * CandidateURI/CrawlURI instamce, if that URI should be 
+ * scheduled for crawling.
+ * 
+ * Dynamic information inherent in the discovery of the
+ * URI -- such as the path by which it was discovered --
+ * may be considered.
+ * 
+ * Dynamic information which requires the consultation 
+ * of external and potentially volatile information --
+ * such as current robots.txt requests and the history
+ * of attempts to crawl the same URI -- should NOT be
+ * considered. Those potentially high-latency decisions
+ * should be made at another step. . 
+ * 
+ * @author gojomo
+ *
+ */
+public abstract class CrawlScope extends Filter {
+	// the xpath to extract seeds from the scope configuration element
+	public static final String XP_SEEDS = "seeds";
+	// a monotonically increasing version number, for scopes that may change
+	int version = 0;
+	// associated CrawlController
+	CrawlController controller;
+	
+	public void initialize(CrawlController controller) {
+		super.initialize(controller);
+		this.controller = controller;
+		// TODO let configuration info specify seedExtractor
+	}
+
+	/**
+	 * Return the scope version. Increments if the scope changes,
+	 * for example by operator edits during a crawl. A CandidateURI
+	 * remembers any scope version it was previously accepted by,
+	 * which helps avoid redundant scope checks. 
+	 * 
+	 * @return
+	 */
+	public int getVersion() {
+		return version;
+	}
+
+	public String toString() {
+		return "CrawlScope<"+name+">";
+	}	
+	
+	
+	/**
+	 * Return an iterator of the seeds in this scope. The seed
+	 * input is taken from either the configuration file, or the 
+	 * external seed file it specifies.
+	 * 
+	 * @return
+	 */
+	public Iterator getSeedsIterator() {
+		try {
+			return new SeedsInputIterator(nodeValueOrSrcReader(XP_SEEDS),controller);
+		} catch (IOException e) {
+			DevUtils.warnHandle(e,"problem reading seeds");
+			return null;
+		}
+	}
+
+//	/**
+//	 * Adds a seed to this scope -- and the associated crawl/frontier.
+//	 * 
+//	 * TODO determine if this is appropriate place for this
+//	 * @param u
+//	 */
+//	public void addSeed(UURI u){
+//		seeds.add(u);
+//		CandidateURI caUri = new CandidateURI(u);
+//		caUri.setIsSeed(true);
+//		controller.getFrontier().schedule(caUri);
+//	}
+	
+	/** 
+	 * Returns whether the given object (typically a CandidateURI) falls
+	 * within this scope. If so, stamps the object with the current scope
+	 * version, so that subsequent checks are expedited. IMPORTANT NOTE: 
+	 * assumes the same CandidateURI object will not be tested  against 
+	 * different CrawlScope objects. 
+	 */
+	public boolean accepts(Object o) {
+		// expedited check
+		if (o instanceof CandidateURI
+			&& ((CandidateURI) o).getScopeVersion() == version) {
+			return true;
+		}
+		boolean result = super.accepts(o);
+		// stamp with version for expedited check
+		if (result == true && o instanceof CandidateURI) {
+			((CandidateURI) o).setScopeVersion(version);
+		}
+		return result;
+	}
+}
