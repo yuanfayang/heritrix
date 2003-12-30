e49d2709ccd74c94e4a0e597af83e745852b69cc
stack-sf <stack-sf@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2003-12-30 23:02:47 +0000

    Added optional system property so its possible to specify an alternate webapp path (e.g. If you want to run the webapp in eclipse, you'd set the a webapp path of 'src/webapp' -- i.e. -Dheritrix.webapp.path=src/webapps -- in place of the default used at runtime of 'webapp').
    * src/java/org/archive/crawler/admin/AdminConstants.java:
        Formatting removing tabs and making lines lengths no longer than 80 chars.
        Added defines to support specification of alternat webapp location.
        (WEBAPP_BASE_DIR, WEBAPP_PATH): Removed.
        (DEFAULT_WEBAPP_PATH, WEBAPP_PATH_NAME, ADMIN_WEBAPP_NAME,
            DEFAULT_ORDER_FILE_NAME): Added.
    * src/java/org/archive/crawler/admin/SimpleHandler.java
        Formatting removing tabs and making lines lengths no longer than 80 chars.
        Added to constructor code which looks for alternate webapp location and
        specification of web IU default order.xml (Removed the code that assumed
        that if passed order.xml is relative path, that its relative to user.dir).
        Added javadoc.
    * src/java/org/archive/crawler/admin/SimpleHttpServer.java
        Formatting removing tabs and making lines lengths no longer than 80 chars.
        (_contextPath, _resourceBase, WEBAPP_BASE_DIR): Removed unused data members.    Added javadoc.
    * src/java/org/archive/crawler/Heritrix.java
        Formatting removing tabs and making lines lengths no longer than 80 chars.
        Formatted usage message so it fits on a 80-character wide terminal.
        Shortened names variables.
    * xdocs/user.xml:
        (heritrix.webapp.path, heritrix.default.orderfile): Added documentation.
        Formatting.

--- trunk/heritrix/src/java/org/archive/crawler/admin/AdminConstants.java
+++ trunk/heritrix/src/java/org/archive/crawler/admin/AdminConstants.java
@@ -1,4 +1,9 @@
-/* Copyright (C) 2003 Internet Archive.
+/* 
+ * AdminConstants
+ * 
+ * $Id$
+ * 
+ * Copyright (C) 2003 Internet Archive.
  *
  * This file is part of the Heritrix web crawler (crawler.archive.org).
  *
@@ -19,73 +24,120 @@
 package org.archive.crawler.admin;
 
 /**
- * An aggregate of various constants.
+ * An aggregate of constants related to crawler admin.
  * <p>
- * The concept behind this interface is that implmenting classes have quick access to these
- * constants.
+ * The concept behind this interface is that implmenting classes have quick
+ * access to these constants.
  * <p>
- * Most of the constants are XPaths in the configuration DOM. Used by the web interface.
+ * Most of the constants are XPaths in the configuration DOM.  Used at least by
+ * the crawler web interface.
  */
 public interface AdminConstants {
-	public static final String CRAWLER_STARTED = "Crawler Started!";
-	public static final String CRAWLER_STOPPED = "Crawler Stopped!";
-	public static final String CRAWLER_RUNNING_ERR =
-		"ERROR: Couldn't start a new crawl (crawler is already running)";
-	public static final String CRAWLER_NOT_RUNNING_ERR =
-	"ERROR: Couldn't stop crawling (no running crawler was found)";
-	public static final String CRAWLER_TERMINATED =
-		"===>Crawling Stopped!<===<br>\nShutting Down the Server!";
-    public static final String WEBAPP_BASE_DIR = "./";
-	public static final String WEBAPP_PATH = "src/webapps/admin/";
-	public static final String DEFAULT_ORDER_FILE = "order.xml";
-	public static final String CRAWLING_IN_PROGRESS = "Crawling In Progress";
-	public static final String NO_CRAWLING = "No Crawling in Progress";
-	public static final String NO_ACTIVE_THREADS = "All threads are idle";
-	public static final String CRAWL_ORDER_UPDATED = "Crawl Order Updated";
-	
-	// XPaths
-	// From CrawlOrder
-	public static final String XP_CRAWL_ORDER_NAME = "//crawl-order/@name";
-	public static final String XP_HTTP_USER_AGENT = "//http-headers/@User-Agent";
-	public static final String XP_HTTP_FROM = "//http-headers/@From";
-	public static final String XP_MAX_TOE_THREADS = "//behavior/@max-toe-threads";
-	public static final String XP_ROBOTS_HONORING_POLICY_NAME = "//behavior/robots-honoring-policy/@name";
-	public static final String XP_ROBOTS_HONORING_POLICY_MASQUERADE = "//behavior/robots-honoring-policy/@masquerade";
-	public static final String XP_ROBOTS_HONORING_POLICY_CUSTOM_ROBOTS = "//behavior/robots-honoring-policy/custom-robots";
-	public static final String XP_ROBOTS_HONORING_POLICY_USER_AGENTS = "//behavior/robots-honoring-policy/user-agents/agent";
-	// From CrawlController
-	public static final String XP_STATS_LEVEL = "//loggers/crawl-statistics/@level";
-	public static final String XP_STATS_INTERVAL = "//loggers/crawl-statistics/@interval-seconds";
-	public static final String XP_DISK_PATH = "//behavior/@disk-path";
-	public static final String XP_PROCESSORS = "//behavior/processors/processor";
-	public static final String XP_FRONTIER = "//behavior/frontier";
-	public static final String XP_CRAWL_SCOPE = "//scope";
-	// From Frontier
-	public static final String XP_DELAY_FACTOR = "@delay-factor";
-	public static final String XP_MIN_DELAY = "@min-delay-ms";
-	public static final String XP_MAX_DELAY = "@max-delay-ms";
-	
-	// Custom	
-	public static final String XP_MAX_TRANS_HOPS = "//scope/@max-trans-hops";
-	public static final String XP_MAX_LINK_HOPS = "//scope/@max-link-hops";
-	public static final String XP_CRAWL_MODE = "//scope/@mode";
-	
-	public static final String XP_ARC_PREFIX = "//@prefix";
-	public static final String XP_ARC_COMPRESSION_IN_USE = "//@compress";
-	public static final String XP_MAX_ARC_SIZE = "//@max-arc-size";
-	public static final String XP_ARC_DUMP_PATH = "//processor/@path";
-	
-	public static final String XP_CRAWL_COMMENT = "//crawl-order/@comment";
-	public static final String XP_SEEDS = "//seeds";
-	public static final String XP_SEEDS_FILE = "//seeds/@src";
-	
-	public static final String XP_POLITENESS_DELAY_FACTOR = "//frontier/@delay-factor";
-	public static final String XP_POLITENESS_MIN_DELAY = "//frontier/@min-delay-ms";
-	public static final String XP_POLITENESS_MAX_DELAY = "//frontier/@max-delay-ms";
-	public static final String XP_POLITENESS_MIN_INTERVAL = "//frontier/@min-interval-ms";
+    public static final String CRAWLER_STARTED = "Crawler Started!";
+    public static final String CRAWLER_STOPPED = "Crawler Stopped!";
+    public static final String CRAWLER_RUNNING_ERR =
+        "ERROR: Couldn't start a new crawl (crawler is already running)";
+    public static final String CRAWLER_NOT_RUNNING_ERR =
+    "ERROR: Couldn't stop crawling (no running crawler was found)";
+    public static final String CRAWLER_TERMINATED =
+        "===>Crawling Stopped!<===<br>\nShutting Down the Server!";
+    public static final String CRAWLING_IN_PROGRESS = "Crawling In Progress";
+    public static final String NO_CRAWLING = "No Crawling in Progress";
+    public static final String NO_ACTIVE_THREADS = "All threads are idle";
+    public static final String CRAWL_ORDER_UPDATED = "Crawl Order Updated";
+  
+    /**
+     *  Default webapp path.
+     */
+    public static final String DEFAULT_WEBAPP_PATH = "webapps";
+    
+    /**
+     * Name of system property whose specification overrides
+     * DEFAULT_WEBAPP_PATH.
+     */
+    public static final String WEBAPP_PATH_NAME = "heritrix.webapp.path";
 
-	public static final String XP_HTTPFETCH_MAX_FETCH_ATTEMPTS = "//processor/@max-fetch-attempts";
-	public static final String XP_HTTPFETCH_MAX_LENGTH_BYTES = "//processor/@max-length-bytes";
-	public static final String XP_HTTPFETCH_SOTIMEOUT = "//processor/@sotimeout-ms";
-	public static final String XP_HTTPFETCH_TIMEOUT = "//processor/@timeout-seconds";
+    /**
+     * Default name of admin webapp.
+     */
+    public static final String ADMIN_WEBAPP_NAME = "admin";
+    
+    /**
+     * Name of system property whose specification overrides default order file
+     * used.
+     * 
+     * Default is WEBAPP_PATH + ADMIN_WEBAPP_NAME 
+     * + DEFAULT_ORDER_FILE.  Pass an absolute or relative path.
+     */
+    public static final String DEFAULT_ORDER_FILE_NAME 
+        = "heritrix.default.orderfile";
+    
+    /**
+     * Default order file name.
+     */
+    public static final String DEFAULT_ORDER_FILE = "order.xml";
+    
+    // XPaths
+    // From CrawlOrder
+    public static final String XP_CRAWL_ORDER_NAME = "//crawl-order/@name";
+    public static final String XP_HTTP_USER_AGENT 
+        = "//http-headers/@User-Agent";
+    public static final String XP_HTTP_FROM = "//http-headers/@From";
+    public static final String XP_MAX_TOE_THREADS 
+        = "//behavior/@max-toe-threads";
+    public static final String XP_ROBOTS_HONORING_POLICY_NAME 
+        = "//behavior/robots-honoring-policy/@name";
+    public static final String XP_ROBOTS_HONORING_POLICY_MASQUERADE 
+        = "//behavior/robots-honoring-policy/@masquerade";
+    public static final String 
+        XP_ROBOTS_HONORING_POLICY_CUSTOM_ROBOTS 
+            = "//behavior/robots-honoring-policy/custom-robots";
+    public static final String XP_ROBOTS_HONORING_POLICY_USER_AGENTS 
+        = "//behavior/robots-honoring-policy/user-agents/agent";
+    // From CrawlController
+    public static final String XP_STATS_LEVEL 
+        = "//loggers/crawl-statistics/@level";
+    public static final String XP_STATS_INTERVAL 
+        = "//loggers/crawl-statistics/@interval-seconds";
+    public static final String XP_DISK_PATH = "//behavior/@disk-path";
+    public static final String XP_PROCESSORS 
+        = "//behavior/processors/processor";
+    public static final String XP_FRONTIER = "//behavior/frontier";
+    public static final String XP_CRAWL_SCOPE = "//scope";
+    // From Frontier
+    public static final String XP_DELAY_FACTOR = "@delay-factor";
+    public static final String XP_MIN_DELAY = "@min-delay-ms";
+    public static final String XP_MAX_DELAY = "@max-delay-ms";
+    
+    // Custom    
+    public static final String XP_MAX_TRANS_HOPS = "//scope/@max-trans-hops";
+    public static final String XP_MAX_LINK_HOPS = "//scope/@max-link-hops";
+    public static final String XP_CRAWL_MODE = "//scope/@mode";
+    
+    public static final String XP_ARC_PREFIX = "//@prefix";
+    public static final String XP_ARC_COMPRESSION_IN_USE = "//@compress";
+    public static final String XP_MAX_ARC_SIZE = "//@max-arc-size";
+    public static final String XP_ARC_DUMP_PATH = "//processor/@path";
+    
+    public static final String XP_CRAWL_COMMENT = "//crawl-order/@comment";
+    public static final String XP_SEEDS = "//seeds";
+    public static final String XP_SEEDS_FILE = "//seeds/@src";
+    
+    public static final String XP_POLITENESS_DELAY_FACTOR 
+        = "//frontier/@delay-factor";
+    public static final String XP_POLITENESS_MIN_DELAY 
+        = "//frontier/@min-delay-ms";
+    public static final String XP_POLITENESS_MAX_DELAY 
+        = "//frontier/@max-delay-ms";
+    public static final String XP_POLITENESS_MIN_INTERVAL 
+        = "//frontier/@min-interval-ms";
+
+    public static final String XP_HTTPFETCH_MAX_FETCH_ATTEMPTS 
+        = "//processor/@max-fetch-attempts";
+    public static final String XP_HTTPFETCH_MAX_LENGTH_BYTES 
+        = "//processor/@max-length-bytes";
+    public static final String XP_HTTPFETCH_SOTIMEOUT 
+        = "//processor/@sotimeout-ms";
+    public static final String XP_HTTPFETCH_TIMEOUT 
+        = "//processor/@timeout-seconds";
 }
--- trunk/heritrix/src/java/org/archive/crawler/admin/SimpleHttpServer.java
+++ trunk/heritrix/src/java/org/archive/crawler/admin/SimpleHttpServer.java
@@ -1,4 +1,11 @@
-/* Copyright (C) 2003 Internet Archive.
+/* 
+ * SimpleHttpServer
+ * 
+ * $Id$
+ *
+ * Created on Jul 11, 2003
+ *
+ * Copyright (C) 2003 Internet Archive.
  *
  * This file is part of the Heritrix web crawler (crawler.archive.org).
  *
@@ -15,63 +22,95 @@
  * You should have received a copy of the GNU Lesser Public License
  * along with Heritrix; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- * Created on Jul 11, 2003
- *
- * To change the template for this generated file go to
- * Window>Preferences>Java>Code Generation>Code and Comments
  */
 package org.archive.crawler.admin;
 
+import java.io.File;
 import java.io.IOException;
 
 import org.mortbay.http.*;
 import org.mortbay.jetty.Server;
 
+
 /**
- * @author Administrator
- *
- * To change the template for this generated type comment go to
- * Window>Preferences>Java>Code Generation>Code and Comments
+ * Wrapper for embedded Jetty server.
  */
-public class SimpleHttpServer implements AdminConstants {
+public class SimpleHttpServer 
+    implements AdminConstants 
+{
+    private int _port;
+    private Server _server = null;
+    public static final int DEFAULT_PORT = 8080;
 
-	private int _port;
-	private Server _server = null;
-	public static final int DEFAULT_PORT = 8080;
-	private static String _contextPath = "/";
-	private static String _resourceBase = WEBAPP_BASE_DIR;
-	private HttpContext _context = new HttpContext();
-	public SimpleHttpServer() throws Exception {
-		initialize(DEFAULT_PORT);
-	}
-	public SimpleHttpServer(int port) throws Exception {
-		initialize(port);
-	}
 
-	private void initialize(int port) throws IOException {
-		if (_server == null) {
-			_server = new Server();
-			SocketListener listener = new SocketListener();
-			listener.setPort(port);
-			_server.addListener(listener);
-			_server.addWebApplication("admin", WEBAPP_PATH);
-		}
-	}
+    public SimpleHttpServer() throws Exception {
+        initialize(DEFAULT_PORT);
+    }
 
-	public void startServer() throws Exception {
-		_server.start();
-	}
-	public void stopServer() throws Exception {
-		_server.stop();
-	}
+    public SimpleHttpServer(int port) throws Exception {
+        initialize(port);
+    }
 
-	public int getPort() {
-		return _port;
-	}
+    private void initialize(int port) throws IOException {
+        if (_server == null) {
+            _server = new Server();
+            SocketListener listener = new SocketListener();
+            listener.setPort(port);
+            _server.addListener(listener);
+            _server.addWebApplication(ADMIN_WEBAPP_NAME, 
+                    getAdminWebappPath());
+        }
+    }
 
-	public HttpServer getServer() {
-		return _server;
-	}
+    /**
+     * Return the admin webapp path.
+     * 
+     * Looks at system properties to see if default has been overridden.  This
+     * method is used by SimpleHandler also.
+     * 
+     * @ return Return admin webapp path (Path returned has a trailing '/').
+     */
+    protected static String getAdminWebappPath()
+    {
+        String webappPath = System.getProperty(WEBAPP_PATH_NAME);
+        if (webappPath == null) {
+            webappPath = DEFAULT_WEBAPP_PATH;
+        }
+        if (!webappPath.endsWith(File.separator)) {
+            webappPath = webappPath + File.separator;
+        }
+        return webappPath + ADMIN_WEBAPP_NAME + File.separator;
+    }
+    
+    /**
+     * Start the server.
+     * 
+     * @throws Exception
+     */
+    public void startServer() throws Exception {
+        _server.start();
+    }
+    
+    /**
+     * Stop the running server.
+     * 
+     * @throws Exception
+     */
+    public void stopServer() throws Exception {
+        _server.stop();
+    }
 
+    /**
+     * @return Port server is running on.
+     */
+    public int getPort() {
+        return _port;
+    }
+
+    /**
+     * @return Server reference.
+     */
+    public HttpServer getServer() {
+        return _server;
+    }
 }
