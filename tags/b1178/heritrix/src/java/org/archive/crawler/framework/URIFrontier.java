48ee17998671ca9155eaa51495c0ba4648ab9ec5
nobody <nobody@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2004-01-03 01:16:46 +0000

    This commit was manufactured by cvs2svn to create tag 'b1178'.

---
+++ tags/b1178/heritrix/src/java/org/archive/crawler/framework/URIFrontier.java
@@ -0,0 +1,82 @@
+/* Copyright (C) 2003 Internet Archive.
+ *
+ * This file is part of the Heritrix web crawler (crawler.archive.org).
+ *
+ * Heritrix is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * any later version.
+ *
+ * Heritrix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with Heritrix; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * URIFrontier.java
+ * Created on Oct 1, 2003
+ *
+ * $Header$
+ */
+package org.archive.crawler.framework;
+
+import java.io.IOException;
+
+import org.archive.crawler.datamodel.CandidateURI;
+import org.archive.crawler.datamodel.CrawlURI;
+import org.archive.crawler.framework.exceptions.FatalConfigurationException;
+
+/**
+ * @author gojomo
+ *
+ */
+public interface URIFrontier {
+	void initialize(CrawlController c) throws FatalConfigurationException, IOException;
+
+	void schedule(CandidateURI caUri);
+	CrawlURI next(int timeout);
+	void finished(CrawlURI curi);
+	
+	boolean isEmpty();
+
+	/**
+	 * Schedule at top priority (for example, before any
+	 * subsequently finished() items that must be retried) 
+	 * 
+	 * @param caUri
+	 */
+	void scheduleHigh(CandidateURI caUri);
+
+	long successfullyFetchedCount();
+
+	long discoveredUriCount();
+
+	/**
+	 * @return Estimated number of URIs scheduled for prcoessing..
+	 */
+	long pendingUriCount();
+
+	long failedFetchCount();
+
+	/**
+	 * Put caUri into a queue of items to be scheduled 
+	 * later (that is, avoid synchronization overhead)
+	 * @param caUri
+	 */
+	void batchSchedule(CandidateURI caUri);
+
+	/**
+	 * @param caUri
+	 */
+	void batchScheduleHigh(CandidateURI caUri);
+	
+	/**
+	 * Force all batch-scheduled candidates to be 
+	 * actually scheduled. 
+	 * 
+	 */
+	void batchFlush();
+}
---
+++ tags/b1178/heritrix/src/java/org/archive/util/FixedSizeList.java
@@ -0,0 +1,62 @@
+/* Copyright (C) 2003 Internet Archive.
+ *
+ * This file is part of the Heritrix web crawler (crawler.archive.org).
+ *
+ * Heritrix is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * any later version.
+ *
+ * Heritrix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with Heritrix; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Created on Jul 17, 2003
+ *
+ */
+package org.archive.util;
+
+import java.util.LinkedList;
+
+/** Provide a simple fixed-size queue that handles ordering and
+ *  makes sure it's a first-in first-out kind of thing.
+ * 
+ * @author Parker Thompson
+ */
+public class FixedSizeList extends LinkedList {
+
+	public FixedSizeList() {
+	}
+
+	protected int maxSize = 10;
+		
+	public FixedSizeList(int size){
+		maxSize = size;
+	}
+	
+	public boolean add(Object item){
+		makeSpace();
+		return super.add(item);
+	}
+	
+	public void add(int index, Object element) {
+		makeSpace();
+		super.add(index, element);
+	}
+
+	public void addLast(Object o) {
+		makeSpace();
+		super.addLast(o);
+	}
+	
+	private void makeSpace() {
+		if(size() >= maxSize){
+			removeFirst();
+		}
+	}
+}
