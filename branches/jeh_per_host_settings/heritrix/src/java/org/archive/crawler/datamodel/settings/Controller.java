311813d84f3255fde95868f91119719481fceb35
johnerik <johnerik@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2004-01-08 01:31:01 +0000

    A do-nothing crawl controller for testing.

---
+++ branches/jeh_per_host_settings/heritrix/src/java/org/archive/crawler/datamodel/settings/Controller.java
@@ -0,0 +1,56 @@
+/* Controller
+ * 
+ * $Id$
+ * 
+ * Created on Dec 19, 2003
+ *
+ * Copyright (C) 2004 Internet Archive.
+ *
+ * This file is part of the Heritrix web crawler (crawler.archive.org).
+ *
+ * Heritrix is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * any later version.
+ *
+ * Heritrix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with Heritrix; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.archive.crawler.datamodel.settings;
+
+/**
+ * 
+ * @author John Erik Halse
+ *
+ */
+public class Controller extends CrawlerModule {
+
+	/**
+	 * @param parent
+	 * @param name
+	 * @param description
+	 */
+	public Controller() {
+		super("controller", "Heritrix crawl controller");
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.archive.crawler.datamodel.settings.ComplexType#setDefaults()
+	 */
+	protected void setDefaults() {
+		addElementType("settings-directory", "Directory where per host settings are kept", "settings");
+        addElementType("foo", "Just for testing", "bar");
+        addElementType("list", "List Test", new IntegerList(new int[] {10, 20}));
+		ComplexType c = addComplexType(new ComplexType("Processors", "URI processors"));
+		c.addComplexType(new CrawlerModule("Processors2", "URI processors"));
+		addComplexType(new CrawlerModule("TestModule", "TESTMODULE"));
+		addComplexType(new CrawlerModule("TestModule2", "TESTMODULE"));
+		addComplexType(new CrawlerModule("TestModule3", "TESTMODULE3"));
+	}
+}
