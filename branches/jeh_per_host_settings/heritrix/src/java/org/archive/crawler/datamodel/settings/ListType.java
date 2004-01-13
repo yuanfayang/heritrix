3115f393226768a62eb3dece29f7dfce60de8da2
johnerik <johnerik@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2004-01-13 01:08:17 +0000

    Added javadoc

--- branches/jeh_per_host_settings/heritrix/src/java/org/archive/crawler/datamodel/settings/ListType.java
+++ branches/jeh_per_host_settings/heritrix/src/java/org/archive/crawler/datamodel/settings/ListType.java
@@ -28,57 +28,116 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-/**
+/** Super type for all lists.
  * 
  * @author John Erik Halse
- *
  */
 public abstract class ListType implements Type {
     private final List listData = new ArrayList();
     private final String name;
     private final String description;
 
-    /**
+    /** Constructs a new ListType.
      * 
+     * @param name the name of the list
+     * @param description the description of the list
      */
     public ListType(String name, String description) {
         this.name = name;
         this.description = description;
     }
 
+    /** Appends the specified element to the end of this list.
+     * 
+     * @param element element to be appended to this list.
+     * @throws ClassCastException is thrown if the element was of wrong type
+     *         and could not be converted.
+     */
     public void add(Object element) {
         element = checkType(element);
         listData.add(element);
     }
-    
+
+    /** Inserts the specified element at the specified position in this list.
+     *  
+     * Shifts the element currently at that position (if any) and any
+     * subsequent elements to the right (adds one to their indices).
+     * 
+     * @param index index at which the specified element is to be inserted.
+     * @param element element to be inserted.
+     * @throws ClassCastException is thrown if the element was of wrong type
+     *         and could not be converted.
+     */
     public void add(int index, Object element) {
         element = checkType(element);
         listData.add(index, element);
     }
-    
+
+    /** Appends all of the elements in the specified list to the end of this
+     * list, in the order that they are returned by the specified lists's
+     * iterator.
+     * 
+     * The behavior of this operation is unspecified if the specified
+     * collection is modified while the operation is in progress.
+     *
+     * This method is a helper method for subclasses. 
+     *
+     * @param l list whose elements are to be added to this list.
+     */
     protected void addAll(ListType l) {
         listData.addAll(l.listData);
     }
-    
+
+    /** Replaces the element at the specified position in this list with the
+     *  specified element.
+     * 
+     * @param index index at which the specified element is to be inserted.
+     * @param element element to be inserted.
+     * @return the element previously at the specified position.
+     * @throws ClassCastException is thrown if the element was of wrong type
+     *         and could not be converted.
+     */
     public Object set(int index, Object element) {
         element = checkType(element);
         return listData.set(index, element);
     }
-    
+
+    /** Returns an iterator over the elements in this list in proper sequence.
+     * 
+     * @return an iterator over the elements in this list.
+     */
     public Iterator iterator() {
         return listData.iterator();
     }
 
+    /** Get the number of elements in this list.
+     * 
+     * @return number of elements.
+     */
     public int size() {
-        return listData.size();    
+        return listData.size();
     }
-    
+
+    /** Returns true if this list contains no elements.
+     * 
+     * @return true if this list contains no elements.
+     */
     public boolean isEmpty() {
         return listData.isEmpty();
     }
-    
+
+    /** Check if element is of right type for this list.
+     * 
+     * If this method gets a String, it should try to convert it to
+     * the right element type before throwing an exception.
+     * 
+     * @param element element to check.
+     * @return element of the right type.
+     * @throws ClassCastException is thrown if the element was of wrong type
+     *         and could not be converted.
+     */
     public abstract Object checkType(Object element) throws ClassCastException;
-    
+
     /* (non-Javadoc)
      * @see org.archive.crawler.datamodel.settings.Type#getDefaultValue()
      */
@@ -99,7 +158,7 @@ public abstract class ListType implements Type {
     public String getName() {
         return name;
     }
-    
+
     /** Removes all elements from this list.
      */
     public void clear() {
