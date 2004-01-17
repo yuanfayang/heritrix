2dac41fb7fe68167af17ee5d7dc6ee6a7be72ec9
johnerik <johnerik@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2004-01-17 00:13:45 +0000

    I think Scope is converted into new settings framework, but testing remains.

--- heritrix/src/java/org/archive/crawler/basic/Scope.java
+++ heritrix/src/java/org/archive/crawler/basic/Scope.java
@@ -28,7 +28,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Logger;
 
-import javax.management.Attribute;
 import javax.management.AttributeNotFoundException;
 import javax.management.InvalidAttributeValueException;
 
@@ -38,9 +37,9 @@ import org.archive.crawler.datamodel.settings.CrawlerModule;
 import org.archive.crawler.datamodel.settings.CrawlerSettings;
 import org.archive.crawler.datamodel.settings.SimpleType;
 import org.archive.crawler.filter.HopsFilter;
+import org.archive.crawler.filter.OrFilter;
 import org.archive.crawler.filter.SeedExtensionFilter;
 import org.archive.crawler.filter.TransclusionFilter;
-import org.archive.crawler.framework.CrawlController;
 import org.archive.crawler.framework.CrawlScope;
 import org.archive.crawler.framework.Filter;
 
@@ -94,200 +93,209 @@ public class Scope extends CrawlScope {
     public static final String MODE_HOST = "host";
     public static final String MODE_USER_DEFINED = "userdefined";
 
-    static final String[] allowedModes = new String[] {MODE_BROAD, MODE_DOMAIN, MODE_HOST, MODE_PATH, MODE_USER_DEFINED};
+    static final String[] allowedModes =
+        new String[] {
+            MODE_BROAD,
+            MODE_DOMAIN,
+            MODE_HOST,
+            MODE_PATH,
+            MODE_USER_DEFINED };
     static final String defaultMode = MODE_DOMAIN;
 
-	Filter focusFilter; 
-	Filter transitiveFilter; 
-	Filter excludeFilter;
-	List seeds;
+    Filter focusFilter;
+    Filter transitiveFilter;
+    OrFilter excludeFilter;
+    List seeds;
     String mode = null;
-	
+
     public Scope(String name) {
         super(name);
 
-        addElementToDefinition(new SimpleType(ATTR_MAX_LINK_HOPS, "Max link hops", new Integer(25)));
-        addElementToDefinition(new SimpleType(ATTR_MAX_TRANS_HOPS, "Max trans hops", new Integer(5)));
-        addElementToDefinition(new SimpleType(ATTR_MODE, "Mode", defaultMode, allowedModes));
-        
-        ComplexType filter = (ComplexType) addElementToDefinition(new CrawlerModule(ATTR_FOCUS_FILTER));
+        addElementToDefinition(
+            new SimpleType(
+                ATTR_MAX_LINK_HOPS,
+                "Max link hops",
+                new Integer(25)));
+        addElementToDefinition(
+            new SimpleType(
+                ATTR_MAX_TRANS_HOPS,
+                "Max trans hops",
+                new Integer(5)));
+        addElementToDefinition(
+            new SimpleType(ATTR_MODE, "Mode", defaultMode, allowedModes));
+
+        ComplexType filter =
+            (ComplexType) addElementToDefinition(
+                new CrawlerModule(ATTR_FOCUS_FILTER));
         filter.setTransient(true);
-        filter = (ComplexType) addElementToDefinition(new CrawlerModule(ATTR_TRANSITIVE_FILTER));
+        filter =
+            (ComplexType) addElementToDefinition(
+                new CrawlerModule(ATTR_TRANSITIVE_FILTER));
         filter.setTransient(true);
-        filter = (ComplexType) addElementToDefinition(new CrawlerModule(ATTR_EXCLUDE_FILTER));
-        filter.setTransient(true);
+        excludeFilter =
+            (OrFilter) addElementToDefinition(
+                new OrFilter(ATTR_EXCLUDE_FILTER));
     }
-    
+
     public Scope() {
         this(ATTR_NAME);
     }
-    
-	/* (non-Javadoc)
-	 * @see org.archive.crawler.framework.Filter#initialize(org.archive.crawler.framework.CrawlController)
-	 */
-	public void initialize(CrawlerSettings settings) {
-		super.initialize(settings);
-		// setup focusFilter
+
+    /* (non-Javadoc)
+     * @see org.archive.crawler.framework.Filter#initialize(org.archive.crawler.framework.CrawlController)
+     */
+    public void initialize(CrawlerSettings settings) {
+        super.initialize(settings);
+        // setup focusFilter
         try {
             mode = (String) getAttribute(settings, ATTR_MODE);
 
-		if(mode == null || mode.equals(MODE_USER_DEFINED)) {
-            Object filter = getAttribute(settings, ATTR_FOCUS_FILTER);
-            if (filter instanceof Filter) {
-                focusFilter = (Filter) filter;
-                focusFilter.setTransient(false);
-            }
-		} else if (mode.equals(MODE_BROAD)){
-			focusFilter = null;
-		} else {
-			// SeedExtensionFilter implied
-			focusFilter = new SeedExtensionFilter(ATTR_FOCUS_FILTER);
-            focusFilter.setTransient(true);
-            try {
+            if (mode == null || mode.equals(MODE_USER_DEFINED)) {
+                Object filter = getAttribute(settings, ATTR_FOCUS_FILTER);
+                if (filter instanceof Filter) {
+                    focusFilter = (Filter) filter;
+                    focusFilter.setTransient(false);
+                }
+            } else if (mode.equals(MODE_BROAD)) {
+                focusFilter = null;
+            } else {
+                // SeedExtensionFilter implied
+                focusFilter = new SeedExtensionFilter(ATTR_FOCUS_FILTER);
+                focusFilter.setTransient(true);
                 setAttribute(settings, focusFilter);
-            } catch (InvalidAttributeValueException e1) {
-                // TODO Auto-generated catch block
-                e1.printStackTrace();
-            } catch (AttributeNotFoundException e1) {
-                // TODO Auto-generated catch block
-                e1.printStackTrace();
             }
-		}
-		if(focusFilter != null) {
-			focusFilter.initialize(settings);
-			// only set up transitiveFilter if focusFilter set
-            Object filter = getAttribute(settings, ATTR_TRANSITIVE_FILTER);
-            if (filter instanceof Filter) {
-                transitiveFilter = (Filter) filter;
-                transitiveFilter.setTransient(false);
+            if (focusFilter != null) {
+                focusFilter.initialize(settings);
+                // only set up transitiveFilter if focusFilter set
+                Object filter = getAttribute(settings, ATTR_TRANSITIVE_FILTER);
+                if (filter instanceof Filter) {
+                    transitiveFilter = (Filter) filter;
+                    transitiveFilter.setTransient(false);
+                }
+                if (transitiveFilter == null) {
+                    transitiveFilter =
+                        new TransclusionFilter(ATTR_TRANSITIVE_FILTER);
+                    transitiveFilter.setTransient(true);
+                    setAttribute(settings, transitiveFilter);
+                }
+                transitiveFilter.initialize(settings);
             }
-			if(transitiveFilter == null) {
-				transitiveFilter = new TransclusionFilter(ATTR_TRANSITIVE_FILTER);
-                transitiveFilter.setTransient(true);
-                setAttribute(settings, transitiveFilter);
-			}
-			transitiveFilter.initialize(settings);
-		}
-		// setup exclude filter
-        if (((Integer) getAttribute(settings, ATTR_MAX_LINK_HOPS)).intValue() != 0) {
-        }
-		// TODO: if(getNodeAt("@max-link-hops")!=null) {
-			// HopsFilter implied
-		//	excludeFilter = new HopsFilter();
-		//	excludeFilter.setNode(xNode);
-		//} 
-		// TODO: Filter optionalExclude = (Filter) instantiate("exclude");
-		//if (optionalExclude != null) {
-		//	if (excludeFilter == null) {
-		//		excludeFilter = optionalExclude;
-		//	} else {
-		//		excludeFilter = excludeFilter.orWith(optionalExclude);
-		//	}
-		//}
-		if (excludeFilter != null) {
-			excludeFilter.initialize(settings);
-		} 
+
+            // setup exclude filter
+            HopsFilter hopsFilter = null;
+            if (((Integer) getAttribute(settings, ATTR_MAX_LINK_HOPS))
+                .intValue()
+                != 0) {
+                // hopsFilter implied
+                hopsFilter = new HopsFilter("hopsFilter");
+                hopsFilter.setTransient(true);
+                excludeFilter.addFilter(settings, hopsFilter);
+            }
+            if (excludeFilter.isEmpty(settings)) {
+                excludeFilter = null;
+            } else {
+                excludeFilter.initialize(settings);
+            }
 
         } catch (InvalidAttributeValueException e) {
             logger.severe(e.getMessage());
         } catch (AttributeNotFoundException e) {
             logger.severe(e.getMessage());
+            e.printStackTrace();
         }
-	}
+    }
 
-	/**
-	 * 
-	 */
-	private void cacheSeeds() {
-		seeds = new ArrayList();
-		Iterator iter = super.getSeedsIterator();
-		while(iter.hasNext()) {
-			seeds.add(iter.next());
-		}
-	}
+    /**
+     * 
+     */
+    private void cacheSeeds() {
+        seeds = new ArrayList();
+        Iterator iter = super.getSeedsIterator();
+        while (iter.hasNext()) {
+            seeds.add(iter.next());
+        }
+    }
 
-	/** 
-	 * @see org.archive.crawler.framework.Filter#innerAccepts(java.lang.Object)
-	 */
-	protected boolean innerAccepts(Object o) {
-		return ((isSeed(o)||
-				focusAccepts(o))||
-				transitiveAccepts(o))
-				&&!excludeAccepts(o);
-	}
-	
-//	/**
-//	 * @param o
-//	 * @return
-//	 */
-//	private boolean alwaysAccepts(Object o) {
-//		// TODO Auto-generated method stub
-//		return false;
-//	}
+    /** 
+     * @see org.archive.crawler.framework.Filter#innerAccepts(java.lang.Object)
+     */
+    protected boolean innerAccepts(Object o) {
+        return ((isSeed(o) || focusAccepts(o)) || transitiveAccepts(o))
+            && !excludeAccepts(o);
+    }
 
-	/**
-	 * @param o
-	 * @return True if exclude filter accepts passed object.
-	 */
-	private boolean excludeAccepts(Object o) {
-		if (excludeFilter == null) {
-			return false;
-		}
-		return excludeFilter.accepts(o);
-	}
+    //	/**
+    //	 * @param o
+    //	 * @return
+    //	 */
+    //	private boolean alwaysAccepts(Object o) {
+    //		// TODO Auto-generated method stub
+    //		return false;
+    //	}
 
-	/**
-	 * @param o
-	 * @return True if transitive filter accepts passed object.
-	 */
-	private boolean transitiveAccepts(Object o) {
-		if (transitiveFilter == null) {
-			return true;
-		}
-		return transitiveFilter.accepts(o);
-	}
+    /**
+     * @param o
+     * @return True if exclude filter accepts passed object.
+     */
+    private boolean excludeAccepts(Object o) {
+        if (excludeFilter == null) {
+            return false;
+        }
+        return excludeFilter.accepts(o);
+    }
 
-	/**
-	 * @param o
-	 * @return True if focus filter accepts passed object.
-	 */
-	private boolean focusAccepts(Object o) {
-		if (focusFilter == null) {
-			return true;
-		}
-		return focusFilter.accepts(o);
-	}
+    /**
+     * @param o
+     * @return True if transitive filter accepts passed object.
+     */
+    private boolean transitiveAccepts(Object o) {
+        if (transitiveFilter == null) {
+            return true;
+        }
+        return transitiveFilter.accepts(o);
+    }
 
-	private boolean isSeed(Object o) {
-		return o instanceof CandidateURI && ((CandidateURI)o).getIsSeed();
-	}
+    /**
+     * @param o
+     * @return True if focus filter accepts passed object.
+     */
+    private boolean focusAccepts(Object o) {
+        if (focusFilter == null) {
+            return true;
+        }
+        return focusFilter.accepts(o);
+    }
 
-	public Filter getExcludeFilter() {
-		return excludeFilter;
-	}
+    private boolean isSeed(Object o) {
+        return o instanceof CandidateURI && ((CandidateURI) o).getIsSeed();
+    }
 
-	public Filter getFocusFilter() {
-		return focusFilter;
-	}
+    public Filter getExcludeFilter() {
+        return excludeFilter;
+    }
 
-	public Filter getTransitiveFilter() {
-		return transitiveFilter;
-	}
+    public Filter getFocusFilter() {
+        return focusFilter;
+    }
 
-	/* (non-Javadoc)
-	 * @see org.archive.crawler.framework.CrawlScope#getSeedsIterator()
-	 */
-	public Iterator getSeedsIterator() {
-		if (focusFilter == null) {
-			// a cached seeds list isn't necessary for scope tests
-			return super.getSeedsIterator();
-		} 
-		// seeds should be in memory for scope tests
-		if (seeds==null) {
-			cacheSeeds();
-		}
-		return seeds.iterator();
-	}
+    public Filter getTransitiveFilter() {
+        return transitiveFilter;
+    }
+
+    /* (non-Javadoc)
+     * @see org.archive.crawler.framework.CrawlScope#getSeedsIterator()
+     */
+    public Iterator getSeedsIterator() {
+        if (focusFilter == null) {
+            // a cached seeds list isn't necessary for scope tests
+            return super.getSeedsIterator();
+        }
+        // seeds should be in memory for scope tests
+        if (seeds == null) {
+            cacheSeeds();
+        }
+        return seeds.iterator();
+    }
 
     /**
      * @return
@@ -296,4 +304,4 @@ public class Scope extends CrawlScope {
         return mode;
     }
 
-} 
+}
