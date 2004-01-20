039e20c28009230208ffdb5062c7cbf8986ae45e
stack-sf <stack-sf@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2004-01-20 21:21:03 +0000

    * src/cc/start.sh     Test for log dir before creating.  Also, no need catching stdout and stderr     for second time; this stuff is already being logged to cruisecontrol.log

--- heritrix/src/cc/startup.sh
+++ heritrix/src/cc/startup.sh
@@ -6,8 +6,12 @@ export MAVEN_HOME=$HOME/bin/maven-1.0-rc1
 export CCDIR=$HOME/bin/cruisecontrol-2.1.4/main
 export JETTY_HOME=$HOME/bin/Jetty-4.2.15rc0/
 export CCBUILDDIR=$HOME/cc
-mkdir ${CCBUILDDIR}/logs
-nohup $CCDIR/bin/cruisecontrol.sh -port 8081 \
-    &> ${CCBUILDDIR}/logs/stdout_err.log &
+if test ! -d ${CCBUILDDIR}/logs
+then
+    mkdir ${CCBUILDDIR}/logs
+fi
+# CC output goes into cruisecontrol.log.  No need of catching it for second
+# time into a stdout/stderr file.
+nohup $CCDIR/bin/cruisecontrol.sh -port 8081 &> /dev/null &
 cd $JETTY_HOME
 nohup $JAVA_HOME/bin/java -jar start.jar &> ${CCBUILDDIR}/logs/jetty.log &
