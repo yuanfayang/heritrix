092544ba8f5d3bf4af7c23dbbdc8dd73322baa76
johnerik <johnerik@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2004-01-28 19:12:25 +0000

    merged from HEAD

--- branches/jeh_per_host_settings/heritrix/src/java/org/archive/crawler/datamodel/CrawlOrder.java
+++ branches/jeh_per_host_settings/heritrix/src/java/org/archive/crawler/datamodel/CrawlOrder.java
@@ -23,10 +23,6 @@
  * along with Heritrix; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * CrawlOrder.java
- * Created on May 15, 2003
- *
- * $Header$
  */
 
 package org.archive.crawler.datamodel;
