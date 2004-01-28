e7f1948a1367b04e4e9159d66e2e6dbb0ab5942a
johnerik <johnerik@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2004-01-28 20:37:01 +0000

    merged from HEAD

--- heritrix/src/java/org/archive/util/AbstractLongFPSet.java
+++ heritrix/src/java/org/archive/util/AbstractLongFPSet.java
@@ -40,23 +40,23 @@ import java.util.logging.Logger;
  *
  */
 public abstract class AbstractLongFPSet implements LongFPSet {
-	private static Logger logger =
-	    Logger.getLogger("org.archive.util.AbstractLongFPSet");
+    private static Logger logger =
+        Logger.getLogger("org.archive.util.AbstractLongFPSet");
 
     /** A constant used to indicate that a slot in the set storage is empty.
      * A zero or positive value means slot is filled
      */
-	protected static byte EMPTY = -1;
+    protected static byte EMPTY = -1;
 
     /** the capacity of this set, specified as the exponent of a power of 2 */
-	protected int capacityPowerOfTwo;
+    protected int capacityPowerOfTwo;
 
     /** the load factor, as a fraction.  This gives the amount of free space
      * to keep in the Set. */
-	protected float loadFactor;
+    protected float loadFactor;
 
     /** The current number of elements in the set */
-	protected long count;
+    protected long count;
 
     /** Create a new AbstractLongFPSet with a given capacity and load Factor
      *
@@ -71,198 +71,197 @@ public abstract class AbstractLongFPSet implements LongFPSet {
         this.count = 0;
     }
 
-	/** Does this set contain the given value?
+    /** Does this set contain the given value?
      * 
      * @see org.archive.util.LongFPSet#contains(long)
-	 */
-	public boolean contains(long val) {
-		long i = indexFor(val);
-		if(slotHasData(i)) {
-			noteAccess(i);
-			return true;
-		}
-		return false;
-	}
+     */
+    public boolean contains(long val) {
+        long i = indexFor(val);
+        if (slotHasData(i)) {
+            noteAccess(i);
+            return true;
+        }
+        return false;
+    }
 
-	/** Check the state of a slot in the storage.
-	 * 
-	 * @param i the index of the slot to check
-	 * @return -1 if slot is filled; nonegative if full. 
-	 */
-	protected abstract int getSlotState(long i);
-
-	/** Note access (hook for subclass cache-replacement strategies)
+    /** Check the state of a slot in the storage.
      * 
-	 * @param index
-	 */
-	private void noteAccess(long index) {
-		// by default do nothing
-		// cache subclasses may use to update access counts, etc.	
-	}
+     * @param i the index of the slot to check
+     * @return -1 if slot is filled; nonegative if full. 
+     */
+    protected abstract int getSlotState(long i);
 
-	/** Return the number of entries in this set.
+    /** Note access (hook for subclass cache-replacement strategies)
+     * 
+     * @param index
+     */
+    private void noteAccess(long index) {
+        // by default do nothing
+        // cache subclasses may use to update access counts, etc.	
+    }
+
+    /** Return the number of entries in this set.
      * 
      * @see org.archive.util.LongFPSet#count()
-	 */
-	public long count() {
-		return count;
-	}
+     */
+    public long count() {
+        return count;
+    }
 
-	/** Add the given value to this set
+    /** Add the given value to this set
      * 
      * @see org.archive.util.LongFPSet#add(long)
-	 */
-	public boolean add(long val) {
-		logger.finest("Adding "+val);
-		long i = indexFor(val);
-		if (slotHasData(i)) {
-			// positive index indicates already in set
-			return false;
-		}
+     */
+    public boolean add(long val) {
+        logger.finest("Adding " + val);
+        long i = indexFor(val);
+        if (slotHasData(i)) {
+            // positive index indicates already in set
+            return false;
+        }
         // we have a possible slot now, which is encoded as a negative number
 
         // check for space, and grow if needed
-        if((count+1)>(loadFactor*(1<<capacityPowerOfTwo))) {
-			makeSpace();
-			// find new i
-			i = indexFor(val);
-			assert i < 0 : "slot should be empty";
-		}
+        if ((count + 1) > (loadFactor * (1 << capacityPowerOfTwo))) {
+            makeSpace();
+            // find new i
+            i = indexFor(val);
+            assert i < 0 : "slot should be empty";
+        }
 
         i = asDataSlot(i); // convert to positive index
         setAt(i, val);
         count++;
         noteAccess(i);
         return true;
-	}
+    }
 
     /**
-	 * Make additional space to keep the load under the target
-	 * loadFactor level. Subclasses may grow or discard entries
-	 * to satisfy. 
-	 * 
-	 */
-	protected abstract void makeSpace();
-
-	/**
-	 * Set the stored value at the given slot. 
-	 * 
-	 * @param i the slot index
-	 * @param l the value to set
-	 */
-	protected abstract void setAt(long i, long l);
-
-	/** Get the stored value at the given slot. 
+     * Make additional space to keep the load under the target
+     * loadFactor level. Subclasses may grow or discard entries
+     * to satisfy. 
      * 
-	 * @param i the slot index
-	 * @return The stored value at the given slot. 
-	 */
-	protected abstract long getAt(long i);
+     */
+    protected abstract void makeSpace();
 
-	/** Given a value, check the store for its existence. If it exists, it
+    /**
+     * Set the stored value at the given slot. 
+     * 
+     * @param i the slot index
+     * @param l the value to set
+     */
+    protected abstract void setAt(long i, long l);
+
+    /** Get the stored value at the given slot. 
+     * 
+     * @param i the slot index
+     * @return The stored value at the given slot. 
+     */
+    protected abstract long getAt(long i);
+
+    /** Given a value, check the store for its existence. If it exists, it
      * will return the index where the value resides.  Otherwise it return
      * an encoded index, which is a possible storage location for the value.
      *
      * Note, if we have a loading factor less than 1.0, there should always 
      * be an empty location where we can store the value
      *
-	 * @param val the fingerprint value to check for
-	 * @return The (positive) index where the value already resides, 
-	 * or an empty index where it could be inserted (encoded as a
-	 * negative number). 
-	 */
-	private long indexFor(long val) {
-		long candidateIndex = startIndexFor(val);
-		while (true) {
-			if (getSlotState(candidateIndex) < 0) {
-				// slot empty; return negative number encoding index
-				return asEmptySlot(candidateIndex);
-			}
-			if (getAt(candidateIndex) == val) {
-				// already present; return positive index
-				return candidateIndex;
-			}
-			candidateIndex++;
-			if (candidateIndex==1<<capacityPowerOfTwo) {
-				candidateIndex = 0; // wraparound
-			}
-		}
-	}
+     * @param val the fingerprint value to check for
+     * @return The (positive) index where the value already resides, 
+     * or an empty index where it could be inserted (encoded as a
+     * negative number). 
+     */
+    private long indexFor(long val) {
+        long candidateIndex = startIndexFor(val);
+        while (true) {
+            if (getSlotState(candidateIndex) < 0) {
+                // slot empty; return negative number encoding index
+                return asEmptySlot(candidateIndex);
+            }
+            if (getAt(candidateIndex) == val) {
+                // already present; return positive index
+                return candidateIndex;
+            }
+            candidateIndex++;
+            if (candidateIndex == 1 << capacityPowerOfTwo) {
+                candidateIndex = 0; // wraparound
+            }
+        }
+    }
 
     /**
-	 * Return the recommended storage index for the given value. 
-	 * Assumes values are already well-distributed; merely uses
-	 * high-order bits. 
-	 * 
-	 * @param val
-	 * @return The recommended storage index for the given value. 
-	 */
-	private long startIndexFor(long val) {
-		return (val >>> (64 - capacityPowerOfTwo));
-	}
+     * Return the recommended storage index for the given value. 
+     * Assumes values are already well-distributed; merely uses
+     * high-order bits. 
+     * 
+     * @param val
+     * @return The recommended storage index for the given value. 
+     */
+    private long startIndexFor(long val) {
+        return (val >>> (64 - capacityPowerOfTwo));
+    }
 
-	/* @(non-Javadoc)
+    /* @(non-Javadoc)
      * @see org.archive.util.LongFPSet#remove(long)
-	 */
-	public boolean remove(long l) {
-		long i = indexFor(l);
-		if (!slotHasData(i)) {
-			// not present, not changed
-			return false;
-		}
-		removeAt(i);
-		return true;
-	}
-
-	/**
-	 * Remove the value at the given index, relocating its
-	 * successors as necessary. 
-	 * 
-	 *  @param index
-	 */
-	protected void removeAt(long index) {
-		count--;
-		clearAt(index);
-		long probeIndex = index+1;
-		while(true) {
-			if (probeIndex==1<<capacityPowerOfTwo) {
-				probeIndex=0; //wraparound
-			}
-			if(getSlotState(probeIndex)<0) {
-				// vacant 
-				break;
-			}
-			long val = getAt(probeIndex);
-			long newIndex = indexFor(val);
-			if(newIndex!=probeIndex) {
-				// value must shift down
-				newIndex = asDataSlot(newIndex); // positivize
-				relocate(val, probeIndex, newIndex);
-			}
-			probeIndex++;
-		}
-	}
-
+     */
+    public boolean remove(long l) {
+        long i = indexFor(l);
+        if (!slotHasData(i)) {
+            // not present, not changed
+            return false;
+        }
+        removeAt(i);
+        return true;
+    }
 
     /**
-	 * @param index
-	 */
-	protected abstract void clearAt(long index);
+     * Remove the value at the given index, relocating its
+     * successors as necessary. 
+     * 
+     *  @param index
+     */
+    protected void removeAt(long index) {
+        count--;
+        clearAt(index);
+        long probeIndex = index + 1;
+        while (true) {
+            if (probeIndex == 1 << capacityPowerOfTwo) {
+                probeIndex = 0; //wraparound
+            }
+            if (getSlotState(probeIndex) < 0) {
+                // vacant 
+                break;
+            }
+            long val = getAt(probeIndex);
+            long newIndex = indexFor(val);
+            if (newIndex != probeIndex) {
+                // value must shift down
+                newIndex = asDataSlot(newIndex); // positivize
+                relocate(val, probeIndex, newIndex);
+            }
+            probeIndex++;
+        }
+    }
 
-	/**
-	 * 
-	 */
-	protected abstract void relocate(long value, long fromIndex, long toIndex);
+    /**
+     * @param index
+     */
+    protected abstract void clearAt(long index);
 
-	/** 
-	 * Low-cost, non-definitive (except when true) contains
-	 * test. Default answer of false is acceptable.
-	 * 
-	 * @see org.archive.util.LongFPSet#quickContains(long)
-	 */
-	public boolean quickContains(long fp) {
-		return false;
-	}
+    /**
+     * 
+     */
+    protected abstract void relocate(long value, long fromIndex, long toIndex);
+
+    /** 
+     * Low-cost, non-definitive (except when true) contains
+     * test. Default answer of false is acceptable.
+     * 
+     * @see org.archive.util.LongFPSet#quickContains(long)
+     */
+    public boolean quickContains(long fp) {
+        return false;
+    }
 
     /**
      * given a slot index, which could or could not be empty, return it as
@@ -272,10 +271,10 @@ public abstract class AbstractLongFPSet implements LongFPSet {
      * @return the index, converted to represent an slot with data
      */
     private long asDataSlot(final long index) {
-        if(slotHasData(index)) { // slot already has data
+        if (slotHasData(index)) { // slot already has data
             return index;
         }
-        return -(index + 1);
+        return - (index + 1);
     }
 
     /** given a slot index, which could or could not be empty, return it as
@@ -284,7 +283,7 @@ public abstract class AbstractLongFPSet implements LongFPSet {
      * @return the index, converted to represent an empty slot
      */
     private long asEmptySlot(final long index) {
-        if(!slotHasData(index)) { // already empty slot
+        if (!slotHasData(index)) { // already empty slot
             return index;
         }
         return -index - 1;
@@ -296,7 +295,6 @@ public abstract class AbstractLongFPSet implements LongFPSet {
      * @return <code>true</code> if the slot has data
      */
     private boolean slotHasData(final long index) {
-
         return index >= 0;
     }
 }
--- heritrix/src/java/org/archive/util/HttpRecorder.java
+++ heritrix/src/java/org/archive/util/HttpRecorder.java
@@ -1,4 +1,10 @@
-/* Copyright (C) 2003 Internet Archive.
+/* HTTPRecorder
+ *
+ * $Id$
+ * 
+ * Created on Sep 22, 2003
+ * 
+ * Copyright (C) 2003 Internet Archive.
  *
  * This file is part of the Heritrix web crawler (crawler.archive.org).
  *
@@ -15,11 +21,6 @@
  * You should have received a copy of the GNU Lesser Public License
  * along with Heritrix; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- * HTTPRecorder.java
- * Created on Sep 22, 2003
- *
- * $Header$
  */
 package org.archive.util;
 
@@ -32,6 +33,7 @@ import java.util.logging.Level;
 import org.archive.io.RecordingInputStream;
 import org.archive.io.RecordingOutputStream;
 
+
 /**
  * Pairs together a RecordingInputStream and RecordingOutputStream
  * to capture exactly a single HTTP transaction.
@@ -39,32 +41,62 @@ import org.archive.io.RecordingOutputStream;
  * Initially only supports HTTP/1.0 (one request, one response per stream)
  * 
  * @author gojomo
- *
  */
-public class HttpRecorder {
+public class HttpRecorder
+{
 	private static final int DEFAULT_OUTPUT_BUFFER_SIZE = 4096;
 	private static final int DEFAULT_INPUT_BUFFER_SIZE = 65536;
-	protected RecordingInputStream ris;
-	protected RecordingOutputStream ros;
+    
+	private RecordingInputStream ris = null;
+    private RecordingOutputStream ros = null;
+    
+    /**
+     * Backing file basename.
+     * 
+     * Keep it around so can clean up backing files left on disk.
+     */
+    private String backingFileBasename = null;
+    
+    /**
+     * Backing file output stream suffix.
+     */
+    private static final String RECORDING_OUTPUT_STREAM_SUFFIX = ".ros";
+    
+   /**
+    * Backing file input stream suffix.
+    */
+    private static final String RECORDING_INPUT_STREAM_SUFFIX = ".ris";
 	
+    
 	/**
-	 * @param tempDir
-	 * @param backingFilenamePrefix
-	 * 
+     * Create an HttpRecorder.
+     * 
+	 * @param tempDir Directory into which we drop backing files for 
+     * recorded input and output.
+	 * @param backingFilenameBase Backing filename base to which we'll append
+     * suffices <code>ris</code> for recorded input stream and
+     * <code>ros</code> for recorded output stream.
 	 */
-	public HttpRecorder(File tempDir, String backingFilenamePrefix) {
-//		super();
+	public HttpRecorder(File tempDir, String backingFilenameBase) {
 		tempDir.mkdirs();
-		String tempDirPath = tempDir.getPath()+File.separatorChar;
-		ris = new RecordingInputStream(DEFAULT_INPUT_BUFFER_SIZE,tempDirPath+backingFilenamePrefix+".ris",2^20);
-		ros = new RecordingOutputStream(DEFAULT_OUTPUT_BUFFER_SIZE,tempDirPath+backingFilenamePrefix+".ros",2^12);
+		String tempDirPath = tempDir.getPath() + File.separatorChar;
+        this.backingFileBasename = 
+            (new File(tempDir.getPath(), backingFilenameBase))
+                .getAbsolutePath();
+		this.ris = new RecordingInputStream(DEFAULT_INPUT_BUFFER_SIZE, 
+            this.backingFileBasename + RECORDING_INPUT_STREAM_SUFFIX);
+		this.ros = new RecordingOutputStream(DEFAULT_OUTPUT_BUFFER_SIZE,
+            this.backingFileBasename + RECORDING_OUTPUT_STREAM_SUFFIX);
 	}
 
 	/**
 	 * Wrap the provided stream with the internal RecordingInputStream
 	 * 
-	 * @param is
-	 * @return An inputstream.
+	 * @param is InputStream to wrap.
+     * 
+	 * @return The input stream wrapper which itself is an input stream.
+     * Pass this in place of the passed stream so input can be recorded.
+     * 
 	 * @throws IOException
 	 */
 	public InputStream inputWrap(InputStream is) throws IOException {
@@ -74,8 +106,12 @@ public class HttpRecorder {
 
 	/**
 	 * Wrap the provided stream with the internal RecordingOutputStream
-	 * @param os
-	 * @return An output stream.
+     * 
+	 * @param os The output stream to wrap.
+     * 
+	 * @return The output stream wrapper which is itself an output stream.  
+     * Pass this in place of the passed stream so output can be recorded.
+     * 
 	 * @throws IOException
 	 */
 	public OutputStream outputWrap(OutputStream os) throws IOException {
@@ -90,33 +126,40 @@ public class HttpRecorder {
 		try {
 			ris.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			DevUtils.logger.log(Level.SEVERE,"close() ris"+DevUtils.extraInfo(),e);
-
+            // TODO: Can we not let the exception out of here and report it
+            // higher up in the caller?
+			DevUtils.logger.log(Level.SEVERE, "close() ris" + 
+                DevUtils.extraInfo(), e);
 		}
 		try {
 			ros.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			DevUtils.logger.log(Level.SEVERE,"close() ros"+DevUtils.extraInfo(),e);
+			DevUtils.logger.log(Level.SEVERE, "close() ros" +
+                DevUtils.extraInfo(), e);
 		}
 	}
 
 	/**
 	 * Return the internal RecordingInputStream
+     * 
 	 * @return A RIS.
 	 */
 	public RecordingInputStream getRecordedInput() {
-		return ris;
+		return this.ris;
 	}
+    
+    /**
+     * @return The RecordingOutputStream.
+     */
+    public RecordingOutputStream getRecordedOutput() {
+        return this.ros;
+    }
 
 	/**
-	 * Mark the point where the HTTP headers end. 
-	 * 
+	 * Mark current position as the point where the HTTP headers end.
 	 */
 	public void markContentBegin() {
 		ris.markContentBegin();
-        // ris.startDigest();
 	}
 
 	public long getResponseContentLength() {
@@ -124,15 +167,67 @@ public class HttpRecorder {
 	}
 
 	/**
-	 * 
+	 * Close both input and output recorders.
+     * 
+     * Recorders are the output streams to which we are recording.
+     * {@link #close} closes the stream that is being recorded and the 
+     * recorder. This method explicitly closes the recorder only.
 	 */
 	public void closeRecorders() {
 		try {
 			ris.closeRecorder();
 			ros.closeRecorder();
 		} catch (IOException e) {
-			DevUtils.warnHandle(e,"convert to runtime exception?");
+			DevUtils.warnHandle(e, "Convert to runtime exception?");
 		}
 	}
-	
+    
+    /**
+     * Clean up backing files.
+     * 
+     * @see java.lang.Object#finalize()
+     */
+    protected void finalize()
+        throws Throwable
+    {
+        try
+        {
+            this.cleanup();
+        }
+        
+        catch(Exception e)
+        {
+            // We're in finalize.  Not much we can do about this.
+            e.printStackTrace(System.out);
+        }
+        
+        super.finalize();
+    }
+    
+    /**
+     * Cleanup backing files.
+     * 
+     * Call when completely done w/ recorder.  Removes any backing files that
+     * may have been dropped.
+     */
+    public void cleanup()
+    {
+        this.close();
+        this.delete(this.backingFileBasename + RECORDING_OUTPUT_STREAM_SUFFIX);
+        this.delete(this.backingFileBasename + RECORDING_INPUT_STREAM_SUFFIX);   
+    }
+
+    /**
+     * Delete file if exists.
+     * 
+     * @param name Filename to delete.
+     */
+    private void delete(String name)
+    {
+        File f = new File(name);
+        if (f.exists())
+        {
+            f.delete();
+        }
+    }
 }
