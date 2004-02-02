5ba5abb85472f63d61b3bb987b13be7bce526e39
kristinn_sig <kristinn_sig@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2004-02-02 18:15:12 +0000

    * Moved 'delete dir' method to FileUtils class

--- heritrix/src/java/org/archive/crawler/admin/CrawlJobHandler.java
+++ heritrix/src/java/org/archive/crawler/admin/CrawlJobHandler.java
@@ -41,6 +41,7 @@ import org.archive.crawler.event.CrawlStatusListener;
 import org.archive.crawler.framework.CrawlController;
 import org.archive.crawler.framework.exceptions.FatalConfigurationException;
 import org.archive.util.ArchiveUtils;
+import org.archive.util.FileUtils;
 
 /**
  * This class manages CrawlJobs. Submitted crawl jobs are queued up and run
@@ -596,27 +597,8 @@ public class CrawlJobHandler implements CrawlStatusListener {
      * written to disk.
      */
     public void discardNewJob(){
-        deleteDir(new File(newJob.getDirectory()));
+        FileUtils.deleteDir(new File(newJob.getDirectory()));
     }
-    
-    /** Deletes all files and subdirectories under dir.
-     *  @return true if all deletions were successful. If a deletion fails, the 
-     *          method stops attempting to delete and returns false.
-     */
-    private static boolean deleteDir(File dir) {
-        if (dir.isDirectory()) {
-            String[] children = dir.list();
-            for (int i=0; i<children.length; i++) {
-                boolean success = deleteDir(new File(dir, children[i]));
-                if (!success) {
-                    return false;
-                }
-            }
-        }
-        // The directory is now empty so delete it
-        return dir.delete();
-    }
-
     
     /**
      * Get the handler's 'new job'
