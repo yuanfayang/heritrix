39553e2b5c85b427011a1131155a256682c3f064
kristinn_sig <kristinn_sig@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2004-02-24 23:15:43 +0000

    * Added a method for converting status codes to human readable strings. Implemented to handle all postive codes, needs additional work to handle Heritrix's internal (negative) codes.

--- trunk/heritrix/src/java/org/archive/crawler/datamodel/CrawlURI.java
+++ trunk/heritrix/src/java/org/archive/crawler/datamodel/CrawlURI.java
@@ -133,8 +133,63 @@ public class CrawlURI extends CandidateURI
         return false;
     }
 
-
-
+    /**
+     * Takes a status code and converts it into a human readable string.
+     * @param code the status code
+     * @return a human readable string declaring what the status code is.
+     */
+    public static String fetchStatusCodesToString(int code){
+        switch(code){
+            // DNS
+            case 1    : return "DNS-1-OK"; 
+            // HTTP Informational 1xx
+            case 100  : return "HTTP-100-Info-Continue";
+            case 101  : return "HTTP-101-Info-Switching Protocols";
+            // HTTP Successful 2xx
+            case 200  : return "HTTP-200-Success-OK";
+            case 201  : return "HTTP-200-Success-Created";
+            case 202  : return "HTTP-200-Success-Accepted";
+            case 203  : return "HTTP-200-Success-Non-Authoritative";
+            case 204  : return "HTTP-200-Success-No Content ";
+            case 205  : return "HTTP-200-Success-Reset Content";
+            case 206  : return "HTTP-200-Success-Partial Content";
+            // HTTP Redirection 3xx
+            case 300  : return "HTTP-300-Redirect-Multiple Choices";
+            case 301  : return "HTTP-301-Redirect-Moved Permanently";
+            case 302  : return "HTTP-302-Redirect-Found";
+            case 303  : return "HTTP-303-Redirect-See Other";
+            case 304  : return "HTTP-304-Redirect-Not Modified";
+            case 305  : return "HTTP-305-Redirect-Use Proxy";
+            case 307  : return "HTTP-307-Redirect-Temporary Redirect";
+            // HTTP Client Error 4xx
+            case 400  : return "HTTP-400-ClientErr-Bad Request";
+            case 401  : return "HTTP-401-ClientErr-Unauthorized";
+            case 402  : return "HTTP-402-ClientErr-Payment Required";
+            case 403  : return "HTTP-403-ClientErr-Forbidden";
+            case 404  : return "HTTP-404-ClientErr-Not Found";
+            case 405  : return "HTTP-405-ClientErr-Method Not Allowed";
+            case 407  : return "HTTP-406-ClientErr-Not Acceptable";
+            case 408  : return "HTTP-407-ClientErr-Proxy Authentication Required";
+            case 409  : return "HTTP-408-ClientErr-Request Timeout";
+            case 410  : return "HTTP-409-ClientErr-Conflict";
+            case 406  : return "HTTP-410-ClientErr-Gone";
+            case 411  : return "HTTP-411-ClientErr-Length Required";
+            case 412  : return "HTTP-412-ClientErr-Precondition Failed";
+            case 413  : return "HTTP-413-ClientErr-Request Entity Too Large";
+            case 414  : return "HTTP-414-ClientErr-Request-URI Too Long";
+            case 415  : return "HTTP-415-ClientErr-Unsupported Media Type";
+            case 416  : return "HTTP-416-ClientErr-Requested Range Not Satisfiable";
+            case 417  : return "HTTP-417-ClientErr-Expectation Failed";
+            // HTTP Server Error 5xx
+            case 500  : return "HTTP-500-ServerErr-Internal Server Error";
+            case 501  : return "HTTP-501-ServerErr-Not Implemented";
+            case 502  : return "HTTP-502-ServerErr-Bad Gateway";
+            case 503  : return "HTTP-503-ServerErr-Service Unavailable";
+            case 504  : return "HTTP-504-ServerErr-Gateway Timeout";
+            case 505  : return "HTTP-505-ServerErr-HTTP Version Not Supported";
+            default : return Integer.toString(code);
+        }
+    }
 
 
     public int getFetchStatus(){
