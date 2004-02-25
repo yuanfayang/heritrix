69b75ac420d4b15fd6e97dd65dc7f1bc94d94af4
kristinn_sig <kristinn_sig@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2004-02-25 18:22:27 +0000

    * RFE [ 895341 ] Prioritize UI threads   - Have upped the priority of UI threads by 1.

--- trunk/heritrix/src/java/org/archive/crawler/SimpleHttpServer.java
+++ trunk/heritrix/src/java/org/archive/crawler/SimpleHttpServer.java
@@ -68,6 +68,8 @@ public class SimpleHttpServer
         this.port = port;
         SocketListener listener = new SocketListener();
         listener.setPort(port);
+        // Bumpt UI thread priority up 1 above the normal
+        listener.setThreadsPriority(Thread.NORM_PRIORITY);
         this.server.addListener(listener);
         this.server.setRootWebApp("root");
         this.contexts = server.addWebApplications(null, getWARSPath(), true);
