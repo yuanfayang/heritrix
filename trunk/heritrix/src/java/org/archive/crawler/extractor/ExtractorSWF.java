44a791167dd705dfd237ee70f95af2614df0b6e6
ia_igor <ia_igor@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2004-03-25 01:12:33 +0000

    Bugs item #877873 Flash link extractor causes OutOfMemory exceptions.
    Rewrote ExtractorSWF so that it does not use excessive amount of memory while parsing SWF files.
    The old version of swf extractor was using writer�s tags implementation that was then used by the reader to parse URIs. The writer it self keeps local buffer of an entire swf file that is at the end flushed to an output stream (in this case null.) So while parsing large swf files, extractor was causing OutOfMemomry exceptions especially on the systems with limited amount of memory available.
    The new version simple overwrites concrete implementation of tags found within the API that is than passed to the reader. This way minimum amount of memory is used while parsing one tag at the time.

--- trunk/heritrix/src/java/org/archive/crawler/extractor/ExtractorSWF.java
+++ trunk/heritrix/src/java/org/archive/crawler/extractor/ExtractorSWF.java
@@ -1,4 +1,11 @@
-/* Copyright (C) 2003 Internet Archive.
+/*
+ * Heritrix
+ *
+ * $Id$
+ *
+ * Created on March 19, 2004
+ *
+ * Copyright (C) 2003 Internet Archive.
  *
  * This file is part of the Heritrix web crawler (crawler.archive.org).
  *
@@ -15,37 +22,31 @@
  * You should have received a copy of the GNU Lesser Public License
  * along with Heritrix; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- * Created on Jul 9, 2003
- *
  */
+
 package org.archive.crawler.extractor;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.Writer;
-import java.util.ArrayList;
 import java.util.logging.Logger;
 
 import org.archive.crawler.datamodel.CoreAttributeConstants;
 import org.archive.crawler.datamodel.CrawlURI;
 import org.archive.crawler.framework.Processor;
-import org.archive.io.NullOutputStream;
 
 import com.anotherbigidea.flash.readers.SWFReader;
 import com.anotherbigidea.flash.readers.TagParser;
-import com.anotherbigidea.flash.writers.SWFWriter;
-import com.anotherbigidea.io.InStream;
 
 /**
- * Extract URIs from SWF (flash/shockwave) files.
+ * Extracts URIs from SWF (flash/shockwave) files.
  *
- * @author Parker Thompson
+ * @author Igor Ranitovic
  *
  */
 public class ExtractorSWF extends Processor implements CoreAttributeConstants {
 
-    private static Logger logger = Logger.getLogger("org.archive.crawler.extractor.ExtractorSWF");
+    private static Logger logger = 
+        Logger.getLogger("org.archive.crawler.extractor.ExtractorSWF");
 
     protected long numberOfCURIsHandled = 0;
     protected long numberOfLinksExtracted = 0;
@@ -54,8 +55,8 @@ public class ExtractorSWF extends Processor implements CoreAttributeConstants {
      * @param name
      */
     public ExtractorSWF(String name) {
-        super(name, "Flash extractor. Extract URIs from SWF (flash/shockwave)" +
-                " files.");
+        super(name, "Flash extractor. Extracts URIs from SWF " +
+            "(flash/shockwave) files.");
     }
 
     /* (non-Javadoc)
@@ -63,85 +64,53 @@ public class ExtractorSWF extends Processor implements CoreAttributeConstants {
      */
     protected void innerProcess(CrawlURI curi) {
 
-        ArrayList links  = new ArrayList();
-        InputStream documentStream = null;
-        Writer out = null;
-
-        // assumes swfs will be coming in through http
-        //TODO make htis more general (currently we're only fetching via http so it doesn't matter)
-        if (!curi.isHttpTransaction())
-        {
+        if (!curi.isHttpTransaction()) {
             return;
         }
 
         String contentType = curi.getContentType();
-        if ((contentType == null) ||
-            (!contentType.startsWith("application/x-shockwave-flash")))
-        {
-            // nothing to extract for other types here
+        if ((contentType == null) || (!contentType.toLowerCase().
+                startsWith("application/x-shockwave-flash"))) {
             return;
         }
 
         numberOfCURIsHandled++;
 
-        // get the swf as a File
-        try
-        {
-            documentStream = curi.getHttpRecorder().getRecordedInput().
-                getContentReplayInputStream();
-        }catch(IOException e){
-            e.printStackTrace();
-        }
-
-        if (documentStream==null) {
-            // TODO: note problem
-            return;
-        }
-
-        InStream in = new InStream(documentStream);
-
-        // the lib wants something to write to.  Since we don't want output we'll just
-        // pass it a good 'ol NullOutputStream object.  In the long-term it may be better
-        // to create our own classes for javaswf that more effeciently (and generally)
-        // parse files, but this will work for now and should hopefully not prove to be
-        // a performance bottleneck anyway.
-        NullOutputStream nos = new NullOutputStream();
-        SWFWriter tags = new SWFWriter(nos);
-
-        IATagParser iatp = new IATagParser(tags);
-        TagParser parser = new TagParser( iatp );
-
-        SWFReader reader = new SWFReader( parser, in );
-
+        InputStream documentStream = null;
+        // Get the SWF file's content stream.
         try {
+            documentStream = curi.getHttpRecorder()
+                .getRecordedInput().getContentReplayInputStream();
+
+            if (documentStream == null) {
+                return;
+            }
+
+            // Create SWF action taht will add discoved URIs to CrawlURI 
+            // alist(s).             
+            CrawlUriSWFAction curiAction = new CrawlUriSWFAction(curi);
+            // Overwirte parsing of specific tags that might have URIs.
+            CustomSWFTags customTags = new CustomSWFTags(curiAction);
+            TagParser linkParser = new TagParser(customTags);
+            // Parse the file.
+            SWFReader reader = new SWFReader(linkParser, documentStream);
             reader.readFile();
+            
+            numberOfLinksExtracted += curiAction.getLinkCount();
+            
         } catch (IOException e) {
-            // this direct writing to the crawlErrors log
-            // couldn't possibly work: the crawlError log
-            // currently expects a runtime error instance inside
-            // the problem crawlURI
-//            Object array[] = { curi };
-//            controller.crawlErrors.log(
-//                Level.INFO,
-//                curi.getUURI().getUri().toString(),
-//                array);
-            curi.addLocalizedError(getName(),e,null);
+            curi.addLocalizedError(getName(), e, null);
         } finally {
             try {
                 documentStream.close();
-            } catch (IOException ignored) {
-
+            } catch (IOException e) {
+                // TODO: Report the problem.
             }
         }
-
-        links = iatp.getLinks();
-        if(links.size() > 0){
-            numberOfLinksExtracted += links.size();
-            curi.getAList().putObject(A_HTML_LINKS, links);
-        }
-
-        curi.linkExtractorFinished(); // Set flag to indicate that link extraction is completed.
-        logger.fine(curi + " has " + links.size() + " links.");
+        
+        // Set flag to indicate that link extraction is completed.
+        curi.linkExtractorFinished(); 
+        logger.fine(curi + " has " + numberOfLinksExtracted + " links.");
     }
 
     /* (non-Javadoc)
@@ -150,11 +119,11 @@ public class ExtractorSWF extends Processor implements CoreAttributeConstants {
     public String report() {
         StringBuffer ret = new StringBuffer();
         ret.append("Processor: org.archive.crawler.extractor.ExtractorSWF\n");
-        ret.append("  Function:          Link extraction on Shockwave Flash documents (.swf)\n");
+        ret.append("  Function:          Link extraction on Shockwave Flash " +
+            "documents (.swf)\n");
+
         ret.append("  CrawlURIs handled: " + numberOfCURIsHandled + "\n");
         ret.append("  Links extracted:   " + numberOfLinksExtracted + "\n\n");
-
         return ret.toString();
     }
-
 }
