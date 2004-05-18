30c409175749054ff5e629c75b531fc84e969950
gojomo <gojomo@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2004-05-18 01:25:43 +0000

    * DiskQueueTest.java     Corrected faulty testIterator() test --     which was merely throwing an exception     and reporting false success

--- heritrix/src/java/org/archive/util/DiskQueueTest.java
+++ heritrix/src/java/org/archive/util/DiskQueueTest.java
@@ -118,28 +118,29 @@ public class DiskQueueTest extends QueueTestBase {
         }
     }
     
-    public void testIterator(){
-        try {
-            DiskQueue queue = new DiskQueue(new File("/foo"), "bar");
-            queue.enqueue("Item 1");
-            queue.enqueue("Item 2");
-            queue.enqueue("Item 3");
-            Iterator it = queue.getIterator(false);
-            assertTrue("disk queue iterator has first item (no dequeues have been made)",it.hasNext());
-            assertEquals("disk queue iterator checking item 1 (no dequeues have been made)","Item 1",(String)it.next());
-            assertEquals("dequeue from disk","Item 1",(String)queue.dequeue());
-            queue.enqueue("Item 4");
-            queue.enqueue("Item 5");
-            assertEquals("dequeue from disk","Item 2",(String)queue.dequeue());
-            it = queue.getIterator(false);
-            assertTrue("disk queue iterator has first item",it.hasNext());
-            assertEquals("disk queue iterator checking item 3","Item 3",(String)it.next());
-            assertEquals("disk queue iterator checking item 4","Item 4",(String)it.next());
-            assertEquals("disk queue iterator checking item 4","Item 5",(String)it.next());
-            assertFalse("disk queue iterator ends correctly",it.hasNext());
-        } catch (IOException e) {
-            return;
-        }
+    /**
+     * Tests custom iterator over disk-stored contents
+     * 
+     * @throws IOException
+     */
+    public void testIterator() throws IOException {
+        DiskQueue queue = new DiskQueue(getTmpDir(), "bar");
+        queue.enqueue("Item 1");
+        queue.enqueue("Item 2");
+        queue.enqueue("Item 3");
+        Iterator it = queue.getIterator(false);
+        assertTrue("disk queue iterator has first item (no dequeues have been made)",it.hasNext());
+        assertEquals("disk queue iterator checking item 1 (no dequeues have been made)","Item 1",(String)it.next());
+        assertEquals("dequeue from disk","Item 1",(String)queue.dequeue());
+        queue.enqueue("Item 4");
+        queue.enqueue("Item 5");
+        assertEquals("dequeue from disk","Item 2",(String)queue.dequeue());
+        it = queue.getIterator(false);
+        assertTrue("disk queue iterator has first item",it.hasNext());
+        assertEquals("disk queue iterator checking item 3","Item 3",(String)it.next());
+        assertEquals("disk queue iterator checking item 4","Item 4",(String)it.next());
+        assertEquals("disk queue iterator checking item 4","Item 5",(String)it.next());
+        assertFalse("disk queue iterator ends correctly",it.hasNext());
     }
 }
 
