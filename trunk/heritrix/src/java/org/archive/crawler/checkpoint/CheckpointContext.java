b313ecb60889992cb13414a25e52b8486dad4bdc
gojomo <gojomo@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2004-05-20 23:31:09 +0000

    simplified checkpoint numbering scheme

--- trunk/heritrix/src/java/org/archive/crawler/checkpoint/CheckpointContext.java
+++ trunk/heritrix/src/java/org/archive/crawler/checkpoint/CheckpointContext.java
@@ -28,6 +28,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.Serializable;
+import java.text.DecimalFormat;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -37,37 +38,18 @@ import org.archive.util.ArchiveUtils;
  * Captures the checkpoint history and upcoming checkpoint name
  * of a crawl. 
  * 
- * Checkpoints are named according to the pseudo-pattern
- * "([plus-sign][checkpoint-number])+". A resume from a checkpoint
- * starts a new acsending number appended to its origin. So for
- * example, a crawl which checkpoints 5 times while running to 
- * completion would have checkpoints:
- * 
- *   +1
- *   +2
- *   +3
- *   +4
- *   +5
- * 
- * A resume from checkpoint +3 which then ran to completion, 
- * with two new checkpoints, would consist of the above
- * checkpoints '+1','+2','+3' and then new checkpoints:
- * 
- *   +3+1
- *   +3+2
- * 
- * In this way, checkpoint names within a single crawl need
- * never conflict. 
+ * A checkpoint name is a series number, starting from 1, 
+ * typically zero-padded to 5 places.
  * 
  * @author gojomo
  *
  */
 public class CheckpointContext implements Serializable {
-    /** String to prefix any new checkpoint names (essentially, name
-     * of checkpoint that was resumed to create current context) */
+    /** String to prefix any new checkpoint names */
     protected String checkpointPrefix = "";
-    /** Next checkpoint series number */
+    /** Next  overall series checkpoint number */
     protected int nextCheckpoint = 1;
+
     /** All checkpoint names in chain prior to now. May not all still 
      * exist on disk */
     protected List predecessorCheckpoints = new LinkedList();
@@ -101,17 +83,17 @@ public class CheckpointContext implements Serializable {
      * 
      */
     public void begin() {
-        checkpointInProgress = new File(checkpointDirectory, nextName());
+        checkpointInProgress = new File(checkpointDirectory, nextCheckpointDirectoryName());
         checkpointErrors = false;
     }
 
     /**
-     * @return
+     * @return next checkpoint, as a zero-padding string
      */
-    private String nextName() {
-        return checkpointPrefix + "+" + nextCheckpoint;
+    private String nextCheckpointDirectoryName() {
+        return (new DecimalFormat("00000")).format(nextCheckpoint);
     }
-
+    
     /**
      * 
      */
@@ -158,7 +140,7 @@ public class CheckpointContext implements Serializable {
      * @return
      */
     public boolean isAtBeginning() {
-        return checkpointPrefix.length() == 0;
+        return nextCheckpoint == 1;
     }
 
     /**
@@ -166,8 +148,8 @@ public class CheckpointContext implements Serializable {
      */
     public void noteResumed() {
         // extend name
-        checkpointPrefix = nextName();
-        // reset counter
-        nextCheckpoint = 1;
+        // checkpointPrefix = nextHistoryName();
+        // update counters
+        nextCheckpoint += 1;
     }
 }
