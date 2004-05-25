7af66581a75d8bbb4e98765ccf46bdf7b837bdb2
nobody <nobody@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2004-05-25 03:11:34 +0000

    This commit was manufactured by cvs2svn to create tag 'Root_kris-compressed_queues'.

---
+++ tags/Root_kris-compressed_queues/heritrix/src/java/org/archive/crawler/frontier/KeyedQueue.java
@@ -0,0 +1,506 @@
+/* KeyedQueue
+ * 
+ * $Id$
+ * 
+ * Created on May 29, 2003
+ * 
+ * Copyright (C) 2003 Internet Archive.
+ *
+ * This file is part of the Heritrix web crawler (crawler.archive.org).
+ *
+ * Heritrix is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * any later version.
+ *
+ * Heritrix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with Heritrix; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.archive.crawler.frontier;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.commons.collections.Predicate;
+import org.archive.crawler.datamodel.CrawlURI;
+import org.archive.util.ArchiveUtils;
+import org.archive.util.CompositeIterator;
+import org.archive.util.DiskBackedQueue;
+import org.archive.util.Queue;
+
+/**
+ * Ordered collection of work items with the same "classKey". 
+ * 
+ * The collection itself has a state, which may reflect where it
+ * is stored or what can be done with the contained items.
+ * 
+ * <p>For easy access to several locations in the main collection,
+ * it is held between 2 data structures: a top stack and a 
+ * bottom queue. (These in turn may be disk-backed.)
+ * 
+ * <p>Also maintains a collection 'off to the side' of 'frozen'
+ * items. 
+ *
+ * <p>About KeyedQueue states:
+ * 
+ * <p>All KeyedQueues begin INACTIVE. A call to activate() will 
+ * render them READY (if not empty of eligible URIs) or EMPTY
+ * otherwise. 
+ * 
+ * <p>A noteInProcess() puts the KeyedQueue into IN_PROCESS state. 
+ * A matching noteProcessDone() puts the KeyedQueue bank into 
+ * READY or EMPTY. 
+ * 
+ * <p>A freeze() may be issued to any READY or EMPTY queue to 
+ * put it into FROZEN state. Only an unfreeze() will move 
+ * the queue to INACTIVE state. 
+ * 
+ * <p>A deactivate() may be issued to any READY or EMPTY queue
+ * to put it into INACTIVE state. 
+ * 
+ * <p>A snooze() may be issued to any READY or EMPTY queue to 
+ * put it into SNOOZED state.
+ * 
+ * <p>A discard() may be issued to any EMPTY queue to put it into
+ * the DISCARDED state. A queue never leaves the discarded state;
+ * if a queue of its hostname is needed again, a new one is created.
+ * 
+ * @author gojomo
+ * @version $Date$ $Revision$
+ */
+public class KeyedQueue implements Serializable  {
+    // be robust against trivial implementation changes
+    private static final long serialVersionUID = ArchiveUtils.classnameBasedUID(KeyedQueue.class,1);
+    
+    // states
+    /** INACTIVE: not considered as URI source until activated by policy */
+    public static final Object INACTIVE = "INACTIVE".intern();
+    /** READY: eligible and able to supply a new work URI on demand */
+    public static final Object READY = "READY".intern();
+    /** FROZEN: not considered as URI source until operator intervention */
+    public static final Object FROZEN = "FROZEN".intern();
+    /** IN_PROCESS: on hold until one or more URIs in progress are finished */
+    public static final Object BUSY = "BUSY".intern();
+    /** SNOOZED: on hold until a specific time interval has passed */
+    public static final Object SNOOZED = "SNOOZED".intern();
+    /** EMPTY: eligible to supply URIs, but without any to supply */
+    public static final Object EMPTY = "EMPTY".intern();
+    /** FINISHED: discarded because empty (not irreversible) */
+    public static final Object DISCARDED = "FINISHED".intern();
+
+    /** ms time to wake, if snoozed */
+    long wakeTime;
+    /** common string 'key' of included items (typically hostname)  */
+    String classKey;
+    /** current state; see above values */
+    Object state;
+    /** maximum simultaneous plain URIs to allow in-process at a time */
+    int valence = 1;
+
+    /** items in progress */
+    LinkedList inProcessItems = new LinkedList();
+    int inProcessLoad = 0;
+    
+    LinkedList innerStack; // topmost eligible items
+    Queue innerQ; // rest of eligible items
+    
+    LinkedList unqueued; // held batch of items to be queued
+    
+    /**
+     * Put-to-side items; not returned from normal accessors.
+     */
+    Queue frozenQ = null;
+
+    /**
+     * @param key A unique identifier used to distingush files related to this
+     *           objects disk based data structures (will be a part of their
+     *           file name, must therefor be a legal filename).
+     * @param scratchDir Directory where disk based data structures will be
+     *           created.
+     * @param headMax Maximum number of items to keep in memory (excluding
+     *           those that have been enqueuedMedium or enqueuedHigh).
+     * @throws IOException When it fails to create disk based data structures.
+     */
+    public KeyedQueue(String key, File scratchDir, int headMax)
+            throws IOException {
+        super();
+        this.classKey = key;
+        String tmpName = key;
+        this.innerStack = new LinkedList();
+        this.innerQ = new DiskBackedQueue(scratchDir,tmpName,false,headMax);    
+        this.unqueued = new LinkedList();
+        // TODO: Currently unimplemented.  Commenting out for now because its
+        // presence means extra two file descriptors per processed URI.
+        // See https://sourceforge.net/tracker/?func=detail&aid=943768&group_id=73833&atid=539099
+        // this.frozenQ =
+        //  new DiskBackedQueue(scratchDir,tmpName+".frozen",headMax);
+        this.state = INACTIVE;
+    }
+
+    /**
+     * The 'classKey' identifier common to items in this queue
+     * @return Object
+     */
+    public String getClassKey() {
+        return this.classKey;
+    }
+
+    /** 
+     * @return The state of this queue.
+     */
+    public Object getState() {
+        return this.state;
+    }
+
+//
+// STATE TRANSITIONS
+//
+    /**
+     * Move queue from INACTIVE to ACTIVE state
+     */
+    public void activate() {
+        assert this.state == INACTIVE;
+        this.state = isEmpty() ? EMPTY : READY;
+    }
+    /**
+     * Move queue from READY or EMPTY state to INACTIVE
+     */
+    public void deactivate() {
+        assert this.state == READY || this.state == EMPTY;
+        this.state = INACTIVE;
+    }
+    /**
+     * Move queue from READY or EMPTY state to FROZEN
+     */
+    public void freeze() {
+        assert this.state == READY || this.state == EMPTY;
+        this.state = FROZEN;
+    }
+    /**
+     * Move queue from FROZEN state to INACTIVE
+     */
+    public void unfreeze() {
+        assert this.state == FROZEN;
+        this.state = INACTIVE;
+    }
+    /**
+     * Move queue from READY or EMPTY state to SNOOZED
+     */
+    public void snooze() {
+        assert this.state == READY || this.state == EMPTY;
+        this.state = SNOOZED;
+    }
+    /**
+     * Move queue from SNOOZED state to READY or EMPTY
+     */
+    public void wake() {
+        assert this.state == SNOOZED;
+        this.state = isEmpty() ? EMPTY : READY;
+    }
+    /**
+     * Move queue from READY or EMPTY to DISCARDED
+     */
+    public void discard() {
+        assert this.state == READY || this.state == EMPTY;
+        this.state = DISCARDED;
+    }
+    /**
+     * Note that the given item is 'in process';
+     * move queue from READY or EMPTY to IN_PROCESS
+     * and remember in-process item.
+     * 
+     * @param o
+     */
+    public void noteInProcess(CrawlURI o) {
+        assert this.state == READY || this.state == EMPTY;
+        //assert this.inProcessItem == null;
+        inProcessItems.add(o);
+        inProcessLoad += loadFor(o);
+        if(inProcessLoad>=valence) {
+            this.state = BUSY;
+        }
+    }
+    
+    /**
+     * Note that the given item's processing
+     * has completed; forget the in-process item
+     * and move queue from BUSY to READY or 
+     * EMPTY state
+     * 
+     * @param o
+     */
+    public void noteProcessDone(CrawlURI o) {
+        // assert this.state == BUSY;
+        assert inProcessItems.contains(o);
+        inProcessItems.remove(o);
+        inProcessLoad -= loadFor(o);
+        if(inProcessLoad<valence) {
+            this.state = isEmpty() ? EMPTY : READY;
+        }
+    }
+    
+    /**
+     * @param o
+     * @return
+     */
+    private int loadFor(CrawlURI o) {
+        if (o.needsImmediateScheduling()) {
+            // treat anything high-prio as
+            // blocking all others
+            return valence;
+        } else {
+            // otherwise, it's just normal 
+            return 1;
+        }
+    }
+
+    /**
+     * Update READY/EMPTY state after preceding
+     * queue edit operations.
+     * 
+     * @return true if state changed, false otherwise
+     */
+    public boolean checkEmpty() {
+        // update READY|EMPTY state after recent relevant changes
+        if (! (this.state == READY || this.state == EMPTY) ) {
+            // only relevant for active states
+            return false;
+        }
+        Object previous = this.state;
+        this.state = isEmpty() ? EMPTY : READY;
+        return this.state != previous;
+    }
+    
+//
+// SCHEDULING SUPPORT
+//
+    /** 
+     * @return Time to wake, when snoozed
+     */
+    public long getWakeTime() {
+        return this.wakeTime;
+    }
+
+    /**
+     * @param w time to wake, when snoozed
+     */
+    public void setWakeTime(long w) {
+        this.wakeTime = w;
+    }
+
+    /** 
+     * To ensure total and consistent ordering when 
+     * in scheduled order, a fallback sort criterion
+     * @return Fallback sort.
+     */
+    public String getSortFallback() {
+        return this.classKey.toString();
+    }
+
+    /**
+     * The only equals() that matters for KeyedQueues is
+     * object equivalence.
+     *
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    public boolean equals(Object o) {
+        return this == o;
+    }
+
+    /** 
+     * Add an item in the default manner
+     * 
+     * @param curi
+     * @see org.archive.util.Queue#enqueue(java.lang.Object)
+     */
+    public void enqueue(CrawlURI curi) {
+        if(curi.needsImmediateScheduling()) {
+            enqueueHigh(curi);
+        } else if (curi.needsSoonScheduling()) {
+            enqueueMedium(curi);
+        } else {
+            this.innerQ.enqueue(curi);
+        }
+    }
+
+    /**
+     * enqueue at a middle location (ahead of 'most'
+     * items, but behind any recent 'enqueueHigh's
+     * 
+     * @param curi
+     */
+    private void enqueueMedium(CrawlURI curi) {
+        this.innerStack.addLast(curi);
+    }
+    /**
+     * enqueue ahead of everything else
+     * 
+     * @param curi
+     */
+    private void enqueueHigh(CrawlURI curi) {
+        this.innerStack.addFirst(curi);
+    }
+   
+    /** 
+     * Is this KeyedQueue empty of ready-to-try URIs. (NOTE: may
+     * still have 'frozen' off-to-side URIs.)
+     * 
+     * @see org.archive.util.Queue#isEmpty()
+     * @return
+     */
+    public boolean isEmpty() {
+        return this.innerStack.isEmpty() && this.innerQ.isEmpty();
+        // return innerStack.isEmpty() && innerQ.isEmpty() && frozenQ.isEmpty();
+    }
+
+    /** 
+     * Remove an item in the default manner
+     * 
+     * @see org.archive.util.Queue#dequeue()
+     * @return
+     */
+    public Object dequeue() {
+        if (!this.innerStack.isEmpty()) {
+            return this.innerStack.removeFirst();
+        }
+        return this.innerQ.dequeue();
+    }
+
+    /** 
+     * Total number of available items. (Does not include
+     * any 'frozen' items.)
+     * 
+     * @see org.archive.util.Queue#length()
+     * @return
+     */
+    public long length() {
+        return this.innerQ.length() + this.innerStack.size();
+    }
+
+    /** 
+     * @return Total number of 'frozen' items. 
+     */
+    public long frozenLength() {
+        return this.innerQ.length() + this.innerStack.size();
+    }
+    
+    /**
+     * Release any external resources (eg open files) which
+     * may be held.
+     */
+    public void release() {
+        this.innerQ.release();
+        if (this.frozenQ != null) {
+            this.frozenQ.release();
+        }
+    }
+
+    /** 
+     * Iterate over all available (non-frozen) items. 
+     * 
+     * @param inCacheOnly
+     * @see org.archive.util.Queue#getIterator(boolean)
+     * @return
+     */
+    public Iterator getIterator(boolean inCacheOnly) {
+        return new CompositeIterator(this.innerStack.iterator(),
+            this.innerQ.getIterator(inCacheOnly));
+    }
+
+    /** 
+     * Delete items matching the supplied criterion. 
+     * 
+     * @param matcher
+     * @see org.archive.util.Queue#deleteMatchedItems(org.apache.commons.collections.Predicate)
+     * @return
+     */
+    public long deleteMatchedItems(Predicate matcher) {
+        // Delete from inner queue
+        long numberOfDeletes = this.innerQ.deleteMatchedItems(matcher);
+        // Then delete from inner stack
+        Iterator it = this.innerStack.iterator();
+        while(it.hasNext()){
+            if(matcher.evaluate(it.next())){
+                it.remove();
+                numberOfDeletes++;
+            }
+        }
+        // return total deleted
+        return numberOfDeletes;
+    }
+
+    /**
+     * @return The remembered item in process (set with noteInProgress()).
+     */
+    public List getInProcessItems() {
+       return inProcessItems;
+    }
+
+    /**
+     * enqueue to the 'frozen' set-aside queue,
+     * which holds items indefinitely (only 
+     * operator action returns them to availability)
+     * 
+     * @param curi
+     */
+    public void enqueueFrozen(CrawlURI curi) {
+        if (this.frozenQ != null) {
+            this.frozenQ.enqueue(curi);
+        }
+    }
+    
+    /**
+     * Return, without removing, the top available item.
+     * 
+     * @return The top available item.
+     */
+    public Object peek() {
+        if(!this.innerStack.isEmpty()) {
+            return this.innerStack.getFirst();
+        }
+        if(!this.innerQ.isEmpty()) {
+            return this.innerQ.peek();
+        }
+        return null;
+    }
+
+    /**
+     * May this KeyedQueue be completely discarded. 
+     *
+     * It may be discarded only if empty of available and frozen items, and
+     * not SNOOZED or FROZEN (which implies state info which would be lost if
+     * discarded).
+     * 
+     * @return True if discardable.
+     */
+    public boolean isDiscardable() {
+        return isEmpty() &&
+            ((this.frozenQ != null)? this.frozenQ.isEmpty(): true) &&
+                this.state == EMPTY;
+    }
+    
+    // custom serialization
+    private void readObject(ObjectInputStream stream) throws IOException, ClassNotFoundException {
+        stream.defaultReadObject();
+        // ensure object identities of state value match
+        this.state = ((String) this.state).intern();
+    }
+
+    /**
+     * @param v
+     */
+    public void setValence(int v) {
+        valence = v;
+    }
+}
---
+++ tags/Root_kris-compressed_queues/heritrix/src/java/org/archive/util/DiskBackedDequeTest.java
@@ -0,0 +1,216 @@
+/* DiskBackedQueueTest
+ *
+ * $Id$
+ *
+ * Created Tue Jan 20 14:17:59 PST 2004
+ *
+ * Copyright (C) 2004 Internet Archive.
+ *
+ * This file is part of the Heritrix web crawler (crawler.archive.org).
+ *
+ * Heritrix is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * any later version.
+ *
+ * Heritrix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with Heritrix; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package org.archive.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.archive.crawler.checkpoint.ObjectPlusFilesInputStream;
+import org.archive.crawler.checkpoint.ObjectPlusFilesOutputStream;
+
+/**
+ * JUnit test suite for DiskBackedQueue
+ *
+ * @author <a href="mailto:me@jamesc.net">James Casey</a>
+ * @version $Id$
+ */
+public class DiskBackedDequeTest extends DiskBackedQueueTest {
+    /**
+     * Create a new DiskBackedQueueTest object
+     *
+     * @param testName the name of the test
+     */
+    public DiskBackedDequeTest(final String testName) {
+        super(testName);
+    }
+
+    /**
+     * run all the tests for DiskBackedQueueTest
+     *
+     * @param argv the command line arguments
+     */
+    public static void main(String argv[]) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    /**
+     * return the suite of tests for DiskBackedQueueTest
+     *
+     * @return the suite of test
+     */
+    public static Test suite() {
+        return new TestSuite(DiskBackedDequeTest.class);
+    }
+
+
+    protected Queue makeQueue() {
+        try {
+            return new DiskBackedDeque(getTmpDir(),"foo",false,5);
+        } catch (final IOException e) {
+            fail("Caught IO Exception on creation of deque : " + e.getMessage());
+            // never gets here
+            return null;
+        }
+    }
+
+    // TODO - implement test methods in DiskBackedDequeTest
+
+    /**
+     * Test which will overflow into disk backing,
+     * trigger flip of write and read files.
+     */
+    public synchronized void testIntoBothDiskBacking() {
+        fillAllSubsegments();        
+        emptyAllSubsegments();    
+    }
+
+    /**
+     * Test which will overflow into disk backing,
+     * trigger flip of write and read files.
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
+    public synchronized void testSerializationBoth() throws IOException, ClassNotFoundException {
+        fillAllSubsegments(); 
+        // serialize/deserialize
+        File tmpDir = getTmpDir();
+        File serFile = new File(tmpDir,"foo.ser");
+        File storeDir = new File(tmpDir,"serialization");
+        ObjectPlusFilesOutputStream out = null;
+        try {
+            out = new ObjectPlusFilesOutputStream(
+                    new FileOutputStream(serFile),storeDir);
+            out.writeObject(queue);
+        } finally {
+            out.close();
+        }
+        
+        ((DiskBackedQueue)queue).release();
+        queue = null;
+        
+        ObjectPlusFilesInputStream in = null;
+        try {
+            in = new ObjectPlusFilesInputStream(
+                        new FileInputStream(serFile),
+                        storeDir);
+            queue = (Queue) in.readObject();
+        } finally {
+            in.close();
+        }
+        emptyAllSubsegments();    
+    }
+
+    private void fillAllSubsegments() {
+        DiskBackedDeque queue = (DiskBackedDeque)this.queue;
+        
+        queue.enqueue("foo1");
+        queue.enqueue("foo2");
+        queue.enqueue("foo3");
+        queue.enqueue("foo4");
+        queue.enqueue("foo5");
+        // these next six go to disk backing (first file)
+        queue.enqueue("foo6");
+        queue.enqueue("foo7");
+        queue.enqueue("foo8");
+        queue.enqueue("foo9");
+        queue.enqueue("foo10");
+        queue.enqueue("foo11");
+        // these next 6 force items into the disk stack
+        queue.push("foo0");
+        queue.push("foo-1");
+        queue.push("foo-2");
+        queue.push("foo-3");
+        queue.push("foo-4");
+        queue.push("foo-5");
+        // now dequeue
+        assertEquals("foo-5 dequeued","foo-5",queue.dequeue());
+        assertEquals("foo-4 dequeued","foo-4",queue.dequeue());
+        assertEquals("foo-3 dequeued","foo-3",queue.dequeue());
+        assertEquals("foo-2 dequeued","foo-2",queue.dequeue());
+        assertEquals("foo-1 dequeued","foo-1",queue.dequeue());
+        // this next forces read of several into memory from disk stack
+        assertEquals("foo0 dequeued","foo0",queue.dequeue());
+       
+        assertEquals("deque properly sized",queue.length(),11);
+        // at this point, a couple should be in memory, a few in 
+        // the stack, and 6 still in the queue. 
+    }
+
+    
+    private void emptyAllSubsegments() {
+        DiskBackedDeque queue = (DiskBackedDeque)this.queue;
+        // now empty stack
+        assertEquals("foo1 dequeued","foo1",queue.dequeue());
+        assertEquals("foo2 dequeued","foo2",queue.dequeue());
+        assertEquals("foo3 dequeued","foo3",queue.dequeue());
+        assertEquals("foo4 dequeued","foo4",queue.dequeue());
+        assertEquals("foo5 dequeued","foo5",queue.dequeue());
+        // and force several itesm from queue in, flipping files
+        assertEquals("foo6 dequeued","foo6",queue.dequeue());
+
+        // these next 4 write to new file
+        queue.enqueue("foo12");
+        queue.enqueue("foo13");
+        queue.enqueue("foo14");
+        queue.enqueue("foo15");
+
+        // dequeue rest
+        assertEquals("foo7 dequeued","foo7",queue.dequeue());
+        assertEquals("foo8 dequeued","foo8",queue.dequeue());
+        assertEquals("foo9 dequeued","foo9",queue.dequeue());
+        assertEquals("foo10 dequeued","foo10",queue.dequeue());
+        assertEquals("foo11 dequeued","foo11",queue.dequeue());
+        assertEquals("foo12 dequeued","foo12",queue.dequeue());
+        assertEquals("foo13 dequeued","foo13",queue.dequeue());
+        assertEquals("foo14 dequeued","foo14",queue.dequeue());
+        // Only one left. (less then or equal to a quarter of availible in
+        // memory cache. Files are deleted.
+        assertFalse("files released",queue.tailQ.isInitialized());
+        queue.enqueue("foo16");
+        queue.enqueue("foo17");
+        queue.enqueue("foo18");
+        queue.enqueue("foo19");
+        // Next two will be written to file again.
+        queue.enqueue("foo20");
+        queue.enqueue("foo21");
+        assertEquals("foo15 dequeued","foo15",queue.dequeue());
+        assertEquals("foo16 dequeued","foo16",queue.dequeue());
+        assertEquals("foo17 dequeued","foo17",queue.dequeue());
+        assertEquals("foo18 dequeued","foo18",queue.dequeue());
+        assertEquals("foo19 dequeued","foo19",queue.dequeue());
+        assertEquals("foo20 dequeued","foo20",queue.dequeue());
+        assertEquals("foo21 dequeued","foo21",queue.dequeue());
+        assertTrue("queue is empty", queue.isEmpty());
+    }
+
+
+}
+
