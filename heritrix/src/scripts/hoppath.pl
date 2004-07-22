725afa152feef7511b1151f9f1f4a25c8b478973
stack-sf <stack-sf@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2004-07-22 21:51:58 +0000

    Finish for "[ 983001 ] crawl.log entries all on one line." * maven.xml     Copy over new hoppath.pl to binary bin dir. * src/articles/user_manual.xml     Update doc on hoppath.pl. * src/scripts/hoppath.pl     Made script work without av tool dependence.  Made it go against new     crawl.log layout.  Some cleanup so easier on public.

--- heritrix/src/scripts/hoppath.pl
+++ heritrix/src/scripts/hoppath.pl
@@ -1,130 +1,124 @@
-#!/usr/local/bin/perl -w
+#!/usr/bin/perl -w
 
-# usage: heri_hoppath crawl.log urlprefix
-# urlprefix has to start with 'http(s)://' or 'dns:'
-# Keep in mind that this is very raw perl code that works in the IA environment only.
-# To run it outside the IA environment replace sort ($sort) and bin_search ($bin_search) commands.
-# Please notify igor at archive dot org of all problems.
-
+# Script that parses crawl.log and prints out path to passed in URI.
+# This script works by first sorting the crawl.log to a file named
+# flattened.crawl.log which it writes to current directory.  It then runs
+# its queries against this file (If the crawl.log is changed, you'll need
+# to remove the cache flattened_crawl.log file).
+#
+# ASSUMPTIONS:
+# + This script was tested with perl 5.8 on debian.
+# + Script expects unix 'sort' in path, and probably a linux sort at that
+# (Takes a '-k' flag).  Adjust the '$SORT' variable below if your sort acts
+# differently or is in an alternate location.
+#
+# $Id$
+#
 use strict;
 
-my $crawllog = shift;
-my $urlprefix = shift;
-my $flatcrawllog = "flat.crawl.log";
-my $sort = "av_sort -k5,5 -S 50%";
-my $bin_search = "/alexa/bin/bin_search";
-my @stack;
-my $cmd;
+# External dependency.  Change this variable to point at your local 'sort'
+# install.
+my $SORT = "sort -k4,4";
 
-# make flat and sorted crawl.log if needed.
-# todo: add some smarts to it.
-if ( ! -f $flatcrawllog ) {
-    open (FH, "< $crawllog")
-        or die "Coudn't open file $crawllog: ($!)\n";
-    open (FLATLOG, "| $sort > $flatcrawllog")
-        or die "Couldn't open filehandle to $flatcrawllog: ($!)\n";
-    print STDOUT "Sorting crawl log file... please wait. This may take a several minutes and it is done only once!\n"; 
-    my $line = "";
-    while (<FH>) {
+# Usage message.
+my $USAGE = <<END;
+Usage: hoppath.pl crawl.log URI_PREFIX
+  crawl.log    Full-path to Heritrix crawl.log instance.
+  URI_PREFIX   URI we're querying about. Must begin 'http(s)://' or 'dns:'.
+               Wrap this parameter in quotes to avoid shell interpretation
+               of any '&' present in URI_PREFIX.
+END
+
+# Make sure of the command-line arguments.
+my $CRAWLLOG = shift;
+my $URLPREFIX = shift;
+die "$USAGE" unless $CRAWLLOG and $URLPREFIX;
+die "Error: $CRAWLLOG does not exist.\n${USAGE}" unless (-e $CRAWLLOG);
+die "Error: $CRAWLLOG is not readable.\n${USAGE}" unless (-r $CRAWLLOG);
+die "Error: $URLPREFIX does not have http(s) or dns prefix.\n${USAGE}" 
+    unless $URLPREFIX =~ m|^(?:(?:https?://)\|(?:dns:))|;
+
+
+# Go to work.
+my $sortedLogFile = checkCache();
+search($sortedLogFile, $URLPREFIX);
+
+
+# Make flattened and sorted crawl.log unless one already exists.
+# TODO: add some smarts to it (e.g. the crawl.log may have changed out from
+# under the flattened representation).
+sub checkCache {
+    # Name of sorted file.
+    my $sortedLogFile = "flattened_crawl.log";
+    # Does sorted file exist?  If not, make it.
+    if (! -f $sortedLogFile) {
+        open (FH, "< $CRAWLLOG")
+            or die "Couldn't open file $CRAWLLOG: $!.\n";
+        open (FLATLOG, "| $SORT > $sortedLogFile")
+            or die "Couldn't open filehandle to $sortedLogFile: $!.\n";
+        print STDOUT "Sorting crawl log file and saving to $sortedLogFile.\n" .
+            "May take a few minutes (This is only done once!).\n";
+        my $line;
+        while (<FH>) {
+            # Collapse field spaces.
+            tr/ //s;
+            print FLATLOG;
+        }
+        close(FH);
+        close(FLATLOG);
+    }
+    return $sortedLogFile;
+}
+
+# Search in cached flattened log file for requested url prefix
+sub search {
+    my ($sortedLogFile, $query) = @_;
+    my @stack;
+    my $exactMatch = 0;
+    open (FLATLOG, $sortedLogFile) or die "Failed open of $sortedLogFile: $!\n";
+    LINES: while (<FLATLOG>) {
+        # Split the line in to constituent parts.
         chomp;
-        if (/^200[34]/) {
-            if ($line) {
-                $line =~ tr/ //s;
-                print FLATLOG "$line\n";
+        my @parts = split(/ /, $_);
+        my $uri = $parts[3];
+        if ($exactMatch? $uri eq $query: $uri =~ m|$query|) {
+            # Get last path character
+            my $path = $parts[4];
+            my $pathchar = $path? (split(//, $path))[-1]: ''; 
+            # Push url onto stack.
+            push @stack, "$parts[0] $pathchar $uri\n";
+            # Rewind and search for this url's referrer.
+            $query = $parts[5];
+            if (not $query) {
+                # If no referrer, we're done.
+                last LINES;
             }
-            $line = "";
-            $line = $_;
-        } else {
-            if (/^  /) {
-                $line .= $_;
-            } else {
-                $line .= "\n";
-            }
+            $exactMatch = 1;
+            seek(FLATLOG, 0, 0) or die "Failed rewind: $!.\n";
         }
     }
-    close(FH);
-    close(FLATLOG);
 
+    # Print out results.
+    if (not @stack) {
+        print STDOUT "URL prefix not found: $URLPREFIX.\n";
+        exit 0;
+    } else {
+        my $spaces = '';
+        while (@stack) {
+            my @parts = split(/ /, pop(@stack));
+            my $line = formatDate($parts[0]) . $spaces;
+            for (my $i = 1; $i < scalar @parts; $i++) {
+                $line .= ' ' . $parts[$i];
+            }
+            print STDOUT "$line";
+            $spaces .= " ";
+        }
+   }
 }
 
-# search for requested url prefix
-$cmd = "$bin_search -f 5 -d ' ' '$urlprefix' $flatcrawllog 2>/dev/null";
-my $res = `$cmd`;
-if (! $res) {
-    print "Url prefix not found: $urlprefix\n";
-    exit;
-}
-chomp($res);
-my @parts = split(/ /, $res);
-
-# Adjust for crawl.log that has extra field due (multi part mime type)
-if (scalar @parts == 10) {
-    splice(@parts, 7, 1);
-}
-
-# Check if seed.
-if (scalar @parts == 7) {
-    print "Found seed for requested prefix!\n$parts[0]: Seed: $parts[4]\n";
-    exit;
-}
-
-# Get last path character
-my @chars = split(//, $parts[7]); 
-my $pathchar = $chars[-1];
-
-# push first url in path to stack
-my $line = "$parts[0]: $pathchar: $parts[4]\n";
-push @stack, $line;
-
-# search for this url next
-my $urlinpath = $parts[8];
-
-# follow the path
-my $pathlength = length($parts[7]);
-my $i = 1;
-while ( $i < $pathlength ){
-    # search for the url in path
-    $cmd = "$bin_search -f 5 -d ' ' '$urlinpath' $flatcrawllog";
-    $res = `$cmd 2>/dev/null`;
-    chomp($res);
-    @parts = split(/ /, $res);
-
-    # Adjust for crawl.log that has extra field due (multi part mime type)
-    if (scalar @parts == 10) {
-        splice(@parts, 7, 1);
-    }
-
-    # get next path character
-    @chars = split(//, $parts[7]);
-    $pathchar = $chars[-1];
-    
-    # push url in path to stack
-    $line = "$parts[0]: $pathchar: $urlinpath\n";
-    push @stack, $line;
-    
-    # next url in path
-    $urlinpath = $parts[8];
-    $i++;
-}
-
-$line = "$parts[0]: $urlinpath\n";
-push @stack, $line;
-my $spaces = " ";
-while (@stack){
-    @parts = split(/ /, pop(@stack));
-    $line = formatDate($parts[0]) . ":" . $spaces;
-    $i=1;
-    while ( $i < scalar @parts) {
-        $line .= " " . $parts[$i];
-        $i++;
-    }
-    print "$line";
-    $spaces .= " ";
-}
-
-
+# Date formatting.
 sub formatDate {
-
     my @p = split(//, shift);
-    return "$p[0]$p[1]$p[2]$p[3]-$p[4]$p[5]-$p[6]$p[7]-$p[8]$p[9]-$p[10]$p[11]-$p[12]$p[14]";
+    return "$p[0]$p[1]$p[2]$p[3]-$p[4]$p[5]-$p[6]$p[7]-$p[8]$p[9]-" .
+        "$p[10]$p[11]-$p[12]$p[14]";
 }
