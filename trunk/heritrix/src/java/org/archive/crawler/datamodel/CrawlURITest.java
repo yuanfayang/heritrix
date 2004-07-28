efe2e40bbc818fdd434107be43e947c0a774fcdd
stack-sf <stack-sf@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2004-07-28 17:38:27 +0000

    * src/java/org/archive/crawler/datamodel/CrawlURITest.java     Made the serialize test serialize multiple times instead of once.     Added test that uses DiskStack.

--- trunk/heritrix/src/java/org/archive/crawler/datamodel/CrawlURITest.java
+++ trunk/heritrix/src/java/org/archive/crawler/datamodel/CrawlURITest.java
@@ -29,6 +29,9 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
+import org.apache.commons.httpclient.URIException;
+import org.archive.util.DiskStack;
+import org.archive.util.Stack;
 import org.archive.util.TmpDirTestCase;
 
 /**
@@ -36,6 +39,21 @@ import org.archive.util.TmpDirTestCase;
  * @version $Revision$, $Date$
  */
 public class CrawlURITest extends TmpDirTestCase {
+    
+    CrawlURI seed = null;
+    
+    protected void setUp() throws Exception {
+        super.setUp();
+        final String url = "http://www.dh.gov.uk/Home/fs/en";
+        this.seed = new CrawlURI(UURIFactory.getInstance(url));
+        this.seed.setSchedulingDirective(CrawlURI.MEDIUM);
+        this.seed.setIsSeed(true);
+        // Force calc. of class key.
+        this.seed.getClassKey();
+        // Force caching of string.
+        this.seed.toString();
+        this.seed.setVia(url);
+    }
 
     /**
      * Test serialization/deserialization works.
@@ -45,34 +63,78 @@ public class CrawlURITest extends TmpDirTestCase {
      */
     final public void testSerialization()
     		throws IOException, ClassNotFoundException {
-        UURI uuri = UURIFactory.getInstance("http://www.dh.gov.uk/Home/fs/en");
-        CrawlURI curi = new CrawlURI(uuri);
-        curi.setSchedulingDirective(CrawlURI.MEDIUM);
-        curi.setIsSeed(true);
-        // Force calc. of class key.
-        curi.getClassKey();
-        // Force caching of string.
-        curi.toString();
-        curi.setVia(uuri);
-        // Write out the object.
         File serialize = new File(getTmpDir(), 
-            this.getClass().getName() + "serialize");
+            this.getClass().getName() + ".serialize");
         try {
             FileOutputStream fos = new FileOutputStream(serialize);
             ObjectOutputStream oos = new ObjectOutputStream(fos);
-            oos.writeObject(curi);
+            oos.writeObject(this.seed);
+            oos.reset();
+            oos.writeObject(this.seed);
+            oos.reset();
+            oos.writeObject(this.seed);
             oos.close();
             // Read in the object.
             FileInputStream fis = new FileInputStream(serialize);
             ObjectInputStream ois = new ObjectInputStream(fis);
             CrawlURI deserializedCuri = (CrawlURI)ois.readObject();
+            deserializedCuri = (CrawlURI)ois.readObject();
+            deserializedCuri = (CrawlURI)ois.readObject();
             assertTrue("Deserialized not equal to original",
-                curi.toString().equals(deserializedCuri.toString()));
-            String host = curi.getUURI().getHost();
+                this.seed.toString().equals(deserializedCuri.toString()));
+            String host = this.seed.getUURI().getHost();
             assertTrue("Deserialized host not null",
                 host != null && host.length() >= 0);
         } finally {
             serialize.delete();
         }
+    }
+    
+    /** 
+     * Make a new DiskStack, using <code>/tmp</code> and <code>foo</code>
+     * as the <code>scratchDir</code> and <code>prefix</code> respectively.
+     * @return the Queue
+     */
+    protected Stack makeStack() {
+        try {
+            File storeFile = new File(getTmpDir(),
+                this.getClass().getName() + ".stack");
+            storeFile.delete();
+            return new DiskStack(storeFile);
+        } catch (IOException e) {
+            fail("IOException: " + e.getMessage());
+            // never gets here
+            return null;
+        }
+    }
+
+    public void testStack() throws URIException {
+        Stack stack = makeStack();
+        try {
+            pushPop(stack, this.seed);
+            pushPop(stack, this.seed);
+            pushPop(stack, this.seed);
+            pushPop(stack, this.seed);
+            stack.push(this.seed);
+            stack.push(this.seed);
+            stack.push(this.seed);
+            stack.push(this.seed);
+            stack.push(this.seed);
+            stack.push(this.seed);
+            stack.push(this.seed);
+            pushPop(stack, this.seed);
+            pushPop(stack, this.seed);
+            pushPop(stack, this.seed);
+        } finally {
+            stack.release();
+        }
+    }
+    
+    private void pushPop(Stack stack, CrawlURI curi) throws URIException {
+        stack.push(curi);
+        CrawlURI poppedCuri = (CrawlURI)stack.pop();
+        String host = poppedCuri.getUURI().getHost();
+        assertTrue("Host is empty",
+            host != null && host.length() > 0);
     }
 }
