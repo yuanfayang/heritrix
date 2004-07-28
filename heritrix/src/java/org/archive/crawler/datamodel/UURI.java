dce38aaf83c1c8b4fff71bd84b0b28beaac6a68c
stack-sf <stack-sf@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2004-07-28 20:06:57 +0000

    More cleanup for '[ 982813 ] CCE deserializing diskqueue     [Was: IllegalArgumentExcepti...]' * src/java/org/archive/crawler/datamodel/UURI.java     Undo workaround put in place yesterday pre-fix that had all data members     transient.

--- heritrix/src/java/org/archive/crawler/datamodel/UURI.java
+++ heritrix/src/java/org/archive/crawler/datamodel/UURI.java
@@ -48,11 +48,6 @@ import org.archive.util.TextUtils;
  * <p>This class tries to cache calculated strings such as the extracted host
  * and this class as a string rather than have the parent class rerun its
  * calculation everytime.
- * 
- * <p>Do not allow any of data members be serializable until
- * <a href="https://sourceforge.net/tracker/index.php?func=detail&aid=982813&group_id=73833&atid=539099">[ 982813 ]
- * CCE deserializing diskqueue [Was: IllegalArgumentExcepti...]</a> is properly
- * addressed.  For now all data members are marked transient.
  *
  * @author gojomo
  * @author stack
@@ -69,7 +64,7 @@ public class UURI extends URI implements Serializable {
      * Super class calculates on every call.  Profiling shows us spend 30% of
      * total elapsed time in URI class.
      */
-    transient private String cachedHost = null;
+    private String cachedHost = null;
 
     /**
      * Cache of this uuri escaped as a string.
@@ -77,7 +72,7 @@ public class UURI extends URI implements Serializable {
      * Super class calculates on every call.  Profiling shows us spend 30% of
      * total elapsed time in URI class.
      */
-    transient private String cachedEscapedURI = null;
+    private String cachedEscapedURI = null;
 
     /**
      * Cache of this uuri escaped as a string.
@@ -85,12 +80,12 @@ public class UURI extends URI implements Serializable {
      * Super class calculates on every call.  Profiling shows us spend 30% of
      * total elapsed time in URI class.
      */
-    transient private String cachedString = null;
+    private String cachedString = null;
 
     /**
      * Cache of this uuri in SURT format
      */
-    transient private String surtForm = null;
+    private String surtForm = null;
 
 
     /**
