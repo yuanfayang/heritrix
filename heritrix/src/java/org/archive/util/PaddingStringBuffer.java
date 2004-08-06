025414a4b5e5d5e983b7a15e28ac83aaaecd22a2
stack-sf <stack-sf@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2004-08-06 21:05:54 +0000

    * src/java/org/archive/util/PaddingStringBuffer.java     Formatting.

--- heritrix/src/java/org/archive/util/PaddingStringBuffer.java
+++ heritrix/src/java/org/archive/util/PaddingStringBuffer.java
@@ -45,7 +45,8 @@ public final class PaddingStringBuffer {
     // Location in current line
     int linePos;
 
-    /** Create a new PaddingStringBuffer
+    /** 
+     * Create a new PaddingStringBuffer
      *
      */
     public PaddingStringBuffer() {
