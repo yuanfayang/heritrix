aff35815bce00e3b22ec17c8d1a09b5475dd9034
gojomo <gojomo@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2004-08-26 00:02:10 +0000

    imrove compliance with source formatting conventions

--- trunk/heritrix/src/java/org/archive/queue/TieredQueue.java
+++ trunk/heritrix/src/java/org/archive/queue/TieredQueue.java
@@ -32,113 +32,129 @@ import java.util.NoSuchElementException;
 import org.apache.commons.collections.Predicate;
 
 /**
- * A queue with multiple internal queues, numbered 0 to n. 
+ * A queue with multiple internal queues, numbered 0 to n.
  * 
  * Dequeues come from lower-numbered queues before higher-numbered queues.
  * 
  * The standard enqueue() places an item at the back of the highest-numbered
- * queue. Other variants of enqueue place items on specific queues. 
+ * queue. Other variants of enqueue place items on specific queues.
  * 
- * A peek() guarantees that the item returned by peek() will be returned
- * by the next dequeue(), even if other items are enqueued to lower-numbered
- * internal queues in the meantime. (The unpeek() may be used to release
- * the TieredQueue from this guarantee; the next peek()/dequeue() will then 
- * return the item available from the lowest-numbered queue.)
+ * A peek() guarantees that the item returned by peek() will be returned by the
+ * next dequeue(), even if other items are enqueued to lower-numbered internal
+ * queues in the meantime. (The unpeek() may be used to release the TieredQueue
+ * from this guarantee; the next peek()/dequeue() will then return the item
+ * available from the lowest-numbered queue.)
  * 
  * @author gojomo
  */
 public class TieredQueue implements Queue {
     Object headObject = null; // offered/set by peek()
-    int headSource = -1;      // index of queue which provided headObject
-    Queue[] innerQueues;      // internal tiers
-    int lastQueue = 0;        // index of highest innerQueue
-    long length = 0;          // count of items in all innerQueues
-    
+
+    int headSource = -1; // index of queue which provided headObject
+
+    Queue[] innerQueues; // internal tiers
+
+    int lastQueue = 0; // index of highest innerQueue
+
+    long length = 0; // count of items in all innerQueues
+
     /**
-     * 
+     * Create a TieredQueue with the given number of
+     * internal queue slots. 
      */
     public TieredQueue(int tiers) {
         super();
         innerQueues = new Queue[tiers];
-        lastQueue = tiers-1;
+        lastQueue = tiers - 1;
     }
-    
-    /* (non-Javadoc)
+
+    /*
+     * (non-Javadoc)
+     * 
      * @see org.archive.queue.Queue#enqueue(java.lang.Object)
      */
     public void enqueue(Object obj) {
-        enqueue(obj,lastQueue);
+        enqueue(obj, lastQueue);
     }
 
     /**
-     * Enqueue the object to the given tier. 
-     * 
+     * Enqueue the object to the given tier.
      */
     public void enqueue(Object obj, int tier) {
         innerQueues[tier].enqueue(obj);
         length++;
     }
-    
-    /* (non-Javadoc)
+
+    /*
+     * (non-Javadoc)
+     * 
      * @see org.archive.queue.Queue#isEmpty()
      */
     public boolean isEmpty() {
         return length == 0;
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see org.archive.queue.Queue#dequeue()
      */
     public Object dequeue() throws NoSuchElementException {
         Object o = peek();
-        assert (o == innerQueues[headSource].dequeue()) : "TieredQueue innerQueue[headSource] != headObject";
+        assert (o == innerQueues[headSource].dequeue()) : 
+            "TieredQueue innerQueue[headSource] != headObject";
         headObject = null;
         length--;
         return o;
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see org.archive.queue.Queue#length()
      */
     public long length() {
         return length;
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see org.archive.queue.Queue#release()
      */
     public void release() {
-        for(int i = 0; i <= lastQueue; i++) {
+        for (int i = 0; i <= lastQueue; i++) {
             innerQueues[i].release();
         }
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see org.archive.queue.Queue#peek()
      */
     public Object peek() {
         if (isEmpty()) {
             throw new NoSuchElementException();
         }
-        if(headObject==null) {
+        if (headObject == null) {
             loadHead();
         }
         return headObject;
     }
 
-    /**
-     * 
-     */
     private void loadHead() {
         assert headObject == null : "TieredQueue redundant loadHead called";
         headSource = 0;
-        while(innerQueues[headSource].isEmpty()) {
+        while (innerQueues[headSource].isEmpty()) {
             headSource++;
         }
         headObject = innerQueues[headSource].peek();
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see org.archive.queue.Queue#unpeek()
      */
     public void unpeek() {
@@ -146,22 +162,29 @@ public class TieredQueue implements Queue {
         headSource = -1;
     }
 
-    public void setQueue(int tier, Queue q) {
-        innerQueues[tier]=q;
-        recalculateLength();
-    }
     
     /**
-     * 
+     * Set the internal queue tier to be the 
+     * supplied Queue instance. 
+     *  
+     * @param tier
+     * @param q
      */
+    public void setQueue(int tier, Queue q) {
+        innerQueues[tier] = q;
+        recalculateLength();
+    }
+
     private void recalculateLength() {
         length = 0;
-        for(int i = 0; i <= lastQueue; i++) {
+        for (int i = 0; i <= lastQueue; i++) {
             length += innerQueues[i].length();
         }
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see org.archive.queue.Queue#getIterator(boolean)
      */
     public Iterator getIterator(boolean inCacheOnly) {
@@ -169,7 +192,9 @@ public class TieredQueue implements Queue {
         return null;
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see org.archive.queue.Queue#deleteMatchedItems(org.apache.commons.collections.Predicate)
      */
     public long deleteMatchedItems(Predicate matcher) {
@@ -178,26 +203,31 @@ public class TieredQueue implements Queue {
     }
 
     /**
-     * Initialize all innerQueues to be DiskBackedQueues in the given 
-     * scratch directory, using the timpName prefix. 
+     * Initialize all innerQueues to be DiskBackedQueues in the given scratch
+     * directory, using the tmpName prefix.
      * 
      * @param scratchDir
      * @param tmpName
      * @throws IOException
      */
-    public void initializeDiskBackedQueues(File scratchDir, String tmpName, int inMemCap) throws IOException {
-        for(int i = 0; i <= lastQueue; i++) {
-            innerQueues[i] = new DiskBackedQueue(scratchDir, tmpName+i, false, inMemCap/(lastQueue+1));
+    public void initializeDiskBackedQueues(File scratchDir, String tmpName,
+            int inMemCap) throws IOException {
+        for (int i = 0; i <= lastQueue; i++) {
+            innerQueues[i] = new DiskBackedQueue(scratchDir, tmpName + i,
+                    false, inMemCap / (lastQueue + 1));
         }
     }
 
     /**
-     * @param i
+     * Set the total number of in-memory items, assuming the underlying
+     * subqueues are DiskBackedQueue instances.
+     * 
+     * @param inMemCap
      */
     public void setMemoryResidentQueueCap(int inMemCap) {
-        // assumes subqueues are DiskBackedQueues
-        for(int i = 0; i <= lastQueue; i++) {
-            ((DiskBackedQueue)innerQueues[i]).setHeadMax(inMemCap/(lastQueue+1));
+        for (int i = 0; i <= lastQueue; i++) {
+            ((DiskBackedQueue) innerQueues[i]).setHeadMax(inMemCap
+                    / (lastQueue + 1));
         }
     }
 
