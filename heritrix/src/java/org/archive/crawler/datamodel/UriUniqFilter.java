b2253760445330c29a6ff40ab047a9d031368782
stack-sf <stack-sf@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2004-09-15 23:23:46 +0000

    * src/java/org/archive/crawler/datamodel/UriUniqFilter.java     Javadoc.

--- heritrix/src/java/org/archive/crawler/datamodel/UriUniqFilter.java
+++ heritrix/src/java/org/archive/crawler/datamodel/UriUniqFilter.java
@@ -1,4 +1,8 @@
-/* Copyright (C) 2003 Internet Archive.
+/* UriUniqFilter
+ * 
+ * Created on Apr 17, 2003
+ * 
+ * Copyright (C) 2003 Internet Archive.
  *
  * This file is part of the Heritrix web crawler (crawler.archive.org).
  *
@@ -15,47 +19,106 @@
  * You should have received a copy of the GNU Lesser Public License
  * along with Heritrix; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- * UURISet.java
- * Created on Apr 17, 2003
- *
- * $Header$
  */
 package org.archive.crawler.datamodel;
 
 
 /**
- * A UriUniqFilter passes add()ed HasUri objects to its 
- * destination, unless the corresponding URI was previously 
- * seen, in which case the HasUri is dropped. 
+ * A UriUniqFilter passes add()ed URI (HasUri) objects to a destination
+ * (receiver) if the passed URI has not been previously seen.
+ * 
+ * If already seen, the passed URI is dropped.
  *
- * For efficiency in comparison against a large history of
+ * <p>For efficiency in comparison against a large history of
  * seen URIs, HasUris may not be passed immediately, unless 
  * the addNow() is used or a flush() is forced.
  * 
  * @author gojomo
+ * @version $Date$, $Revision$
  */
 public interface UriUniqFilter {
-    public long count();   // total known URIs
-    public long pending(); // items added not yet evaluated
-    public void setDestination(HasUriReceiver r); // inner receiver of uniq URIs
-    public void add(HasUri hu); // add given item, if not already present
-    public void addNow(HasUri hu); // add given item, lagless 
-    public void addForce(HasUri hu); // add given item, even if already present
-    public void note(HasUri hu); // note item as seen, without passing through
-    public void forget(HasUri hu); // forget that item was seen
-    public long flush(); // force pending items to be added/dropped, return #added
     
     /**
+     * @return Count of already seen URIs.
+     */
+    public long count();
+    
+    /**
+     * @return Count of items added not yet evaluated (TODO: Whats this mean?).
+     */
+    public long pending();
+
+    /**
+     * Receiver of uniq URIs.
+     * Items that have not been seen before are pass through to this object.
+     * @param receiver Object that will be passed items. Must implement
+     * HasUriReceiver interface.
+     */
+    public void setDestination(HasUriReceiver receiver);
+    
+    /**
+     * Add given item, if not already present.
+     * @param item Item to add.
+     */
+    public void add(HasUri item);
+    
+    /**
+     * Immediately add item.
+     * @param item Item to add immediately.
+     */
+    public void addNow(HasUri item);
+    
+    /**
+     * Add given item, even if already present
+     * 
+     * TODO: What is this for?
+     * 
+     * @param item Item to force add.
+     */
+    public void addForce(HasUri hu);
+    
+    /**
+     * Note item as seen, without passing through to receiver.
+     * @param item Item to note as seen.
+     */
+    public void note(HasUri item);
+    
+    /**
+     * Forget item was seen
+     * @param item Item to forget.
+     */
+    public void forget(HasUri item);
+    
+    /**
+     * Force pending items to be added/dropped.
+     * @return Number added.
+     */
+    public long flush();
+    
+    /**
+     * Objects that have a URI implement this interface.
+     * 
+     * Implementers of this interface would include UURI.
+     * 
      * @author gojomo
      */
     public interface HasUri {
+        /**
+         * @return Return contained URI as a String.
+         */
         public String getUri();
     }
+    
     /**
+     * URIs that have not been seen before 'visit' this 'Visitor'.
+     * 
+     * Usually implementations of Frontier implement this interface.
      * @author gojomo
      */
     public interface HasUriReceiver {
-        public void receive(HasUri h);
+        /**
+         * @param item Item that is 'visiting'.
+         */
+        public void receive(HasUri item);
     }
 }
