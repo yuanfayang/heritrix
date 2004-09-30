14ba05a0e83ef42c078a3a7a41d2889840390fc0
gojomo <gojomo@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2004-09-30 02:18:19 +0000

    shorten the 'getLine()' string display, as now used by BdbFrontier's frontier-browse console

--- heritrix/src/java/org/archive/crawler/datamodel/CandidateURI.java
+++ heritrix/src/java/org/archive/crawler/datamodel/CandidateURI.java
@@ -195,7 +195,9 @@ public class CandidateURI implements Serializable, Lineable, HasUri {
      * @see org.archive.util.Lineable#getLine()
      */
     public String getLine() {
-        return this.getClass().getName()
+        String className = this.getClass().getName();
+        className = className.substring(className.lastIndexOf(".")+1);
+        return className
                 +" "+getUURI().toString()
                 +" "+pathFromSeed
                 +" "+flattenVia();
