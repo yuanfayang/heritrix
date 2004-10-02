82f20ac02165ec9d188595f66259c471aff6d31d
gojomo <gojomo@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2004-10-02 03:11:42 +0000

    Correct typo in ATTR_SURTS_DUMP_FILE.

--- heritrix/src/java/org/archive/crawler/scope/SurtPrefixScope.java
+++ heritrix/src/java/org/archive/crawler/scope/SurtPrefixScope.java
@@ -50,7 +50,7 @@ import org.archive.util.SurtPrefixSet;
 public class SurtPrefixScope extends RefinedScope {
     public static final String ATTR_SURTS_SOURCE_FILE = "surts-source-file";
     public static final String ATTR_SEEDS_AS_SURT_PREFIXES = "seeds-as-surt-prefixes";
-    public static final String ATTR_SURTS_DUMP_FILE = "surts-dump=file";
+    public static final String ATTR_SURTS_DUMP_FILE = "surts-dump-file";
     
     private static final Boolean DEFAULT_SEEDS_AS_SURT_PREFIXES = new Boolean(true);
 
