9a49d46bd6c2ef30339c723af99190a29de8520b
stack-sf <stack-sf@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2004-10-08 21:37:01 +0000

    * src/java/org/archive/crawler/datamodel/UURIFactoryTest.java     Clean out test with no asserts.

--- heritrix/src/java/org/archive/crawler/datamodel/UURIFactoryTest.java
+++ heritrix/src/java/org/archive/crawler/datamodel/UURIFactoryTest.java
@@ -119,20 +119,6 @@ public class UURIFactoryTest extends TestCase {
                 uuri.toString().equals(tgtUri));
     }
     
-    public final void testBadScheme() throws URIException {
-        final String uriStr = "file:///home/stack/index.html";
-        boolean exception = false;
-        try {
-            UURI uuri = UURIFactory.getInstance(uriStr);
-        } catch (URIException e) {
-            String message = e.getMessage();
-        	    exception = true;
-        }
-        // Comment in when unit tests have a list of disallowed schemes loaded.
-        // Normally they don't.
-        // assertTrue("Didn't get exception", exception);
-    }
-    
 	public final void testFailedGetPath() throws URIException {
 		final String path = "/RealMedia/ads/" +
 		"click_lx.ads/%%PAGE%%/%%RAND%%/%%POS%%/%%CAMP%%/empty";
