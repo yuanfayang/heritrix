c228b24a33b7766a34c1878c6a654b8384bb9338
stack-sf <stack-sf@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2004-12-01 18:06:12 +0000

    * src/java/org/archive/util/TextUtils.java     Cap Matcher creations and fix an NPE ([ 1002760 ] NPE in     TextUtils.getMatcher).  Formatting.

--- heritrix/src/java/org/archive/util/TextUtils.java
+++ heritrix/src/java/org/archive/util/TextUtils.java
@@ -26,20 +26,31 @@ import java.util.regex.Pattern;
 import EDU.oswego.cs.dl.util.concurrent.ConcurrentReaderHashMap;
 
 public class TextUtils {
+    /**
+     * Upper-bound on Matcher Stacks.
+     * Profiling has the size of these Stacks tending upward over
+     * the life of a crawl.
+     */
+    private final static int MAXIMUM_STACK_SIZE = 10;
 
-    /** Reusable precompiled Pattern objects indexed on pattern string. */
-    final static ConcurrentReaderHashMap patterns =
+    /** Reusable precompiled Pattern objects indexed on pattern string.
+     * Profiling has this Map growing to ~18 elements total circa
+     * 1.3.0 Heritrix.
+     */
+    private final static ConcurrentReaderHashMap patterns =
         new ConcurrentReaderHashMap(50);
 
     /** Resuable match objects indexed on pattern string. Each element is a
      * stack of Matcher objects that can be reused.
      */
-    final static ConcurrentReaderHashMap patternMatchers =
+    private final static ConcurrentReaderHashMap patternMatchers =
         new ConcurrentReaderHashMap(50);
 
     /**
      * Get a matcher object for a precompiled regex pattern.
      * This method tries to reuse Matcher objects for efficiency.
+     * 
+     * This method is a hotspot frequently accessed.
      *
      * @param pattern the precompiled Pattern
      * @param input the character sequence the matcher should be using
@@ -48,8 +59,8 @@ public class TextUtils {
     public static Matcher getMatcher(String pattern, CharSequence input) {
         pattern = pattern == null ? "" : pattern;
         input = input == null ? "" : input;
-        Matcher matcher;
-        Pattern p = (Pattern) patterns.get(pattern);
+        Matcher matcher = null;
+        Pattern p = (Pattern)patterns.get(pattern);
         if (p == null) {
             p = Pattern.compile(pattern);
             patterns.put(pattern, p);
@@ -57,10 +68,17 @@ public class TextUtils {
             matcher = p.matcher(input);
         } else {
             try {
-                matcher = (Matcher) ((Stack)patternMatchers.get(pattern)).pop();
-                matcher.reset(input);
+                Stack s = (Stack)patternMatchers.get(pattern);
+                if (s != null) {
+                    matcher = (Matcher)s.pop();
+                    matcher.reset(input);
+                }
             } catch(EmptyStackException e) {
-                matcher = ((Pattern)patterns.get(pattern)).matcher(input);
+                // The finally clause will 'recover' from this exception.
+            } finally {
+                if (matcher == null) {
+                    matcher = ((Pattern)patterns.get(pattern)).matcher(input);
+                }
             }
         }
         return matcher;
@@ -77,8 +95,7 @@ public class TextUtils {
     public static void freeMatcher(Matcher m) {
         Stack matchers;
         if((matchers = (Stack) patternMatchers.get(m.pattern().pattern()))
-            == null) {
-
+                == null == matchers.size() > MAXIMUM_STACK_SIZE) {
             // This matcher wasn't created by any pattern in the map, throw it
             // away
             return;
