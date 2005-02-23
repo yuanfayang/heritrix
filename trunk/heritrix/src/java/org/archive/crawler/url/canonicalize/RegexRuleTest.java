c5cda811086e4e4dc888479f9a4129207ddd1372
stack-sf <stack-sf@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2005-02-23 05:24:10 +0000

    * src/java/org/archive/crawler/url/canonicalize/RegexRuleTest.java     Added test of non-default regex and squash null attribute complaints     by adding a SettingsHandler context.

--- trunk/heritrix/src/java/org/archive/crawler/url/canonicalize/RegexRuleTest.java
+++ trunk/heritrix/src/java/org/archive/crawler/url/canonicalize/RegexRuleTest.java
@@ -22,25 +22,63 @@
  */
 package org.archive.crawler.url.canonicalize;
 
-import org.apache.commons.httpclient.URIException;
-import org.archive.crawler.datamodel.UURIFactory;
+import java.io.File;
 
-import junit.framework.TestCase;
+import javax.management.InvalidAttributeValueException;
+
+import org.apache.commons.httpclient.URIException;
+import org.archive.crawler.datamodel.CrawlOrder;
+import org.archive.crawler.datamodel.UURIFactory;
+import org.archive.crawler.settings.CrawlerSettings;
+import org.archive.crawler.settings.MapType;
+import org.archive.crawler.settings.XMLSettingsHandler;
+import org.archive.util.TmpDirTestCase;
+
 
 /**
  * Test the regex rule.
  * @author stack
  * @version $Date$, $Revision$
  */
-public class RegexRuleTest extends TestCase {
-
-    public void testCanonicalize() throws URIException {
+public class RegexRuleTest extends TmpDirTestCase {
+    private File orderFile;
+    private CrawlerSettings globalSettings;
+    protected XMLSettingsHandler settingsHandler;
+    private MapType rules = null;
+    
+    protected void setUp() throws Exception {
+        super.setUp();
+        this.orderFile = new File(getTmpDir(), this.getClass().getName() +
+            ".order.xml");
+        this.settingsHandler = new XMLSettingsHandler(orderFile);
+        this.settingsHandler.initialize();
+        this.rules = (MapType)(settingsHandler.getSettingsObject(null)).
+            getModule(CrawlOrder.ATTR_NAME).
+               getAttribute(CrawlOrder.ATTR_RULES);
+    }
+    
+    public void testCanonicalize()
+    throws URIException, InvalidAttributeValueException {
         final String url = "http://www.aRchive.Org/index.html";
         final String urlMinusWWW = "http://aRchive.Org/index.html";
-        
-        assertTrue("Default doesn't work.",
-            url.equals((new RegexRule("test")).
-                canonicalize(url, UURIFactory.getInstance(url))));
+        RegexRule rr = new RegexRule("Test " + this.getClass().getName());
+        this.rules.addElement(null, rr);
+        rr.canonicalize(url, UURIFactory.getInstance(url));
+        String product = rr.canonicalize(url, null);
+        assertTrue("Default doesn't work.",  url.equals(product));
     }
 
+    public void testSessionid()
+    throws URIException, InvalidAttributeValueException {
+        final String urlBase = "http://joann.com/catalog.jhtml";
+        final String urlMinusSessionid = urlBase + "?CATID=96029";
+        final String url = urlBase +
+		    ";$sessionid$JKOFFNYAAKUTIP4SY5NBHOR50LD3OEPO?CATID=96029";
+        RegexRule rr = new RegexRule("Test",
+            "^(.+)(?:;\\$sessionid\\$[A-Z0-9]{32})(\\?.*)+$",
+        	"$1$2");
+        this.rules.addElement(null, rr);
+        String product = rr.canonicalize(url, null);
+        assertTrue("Failed " + url, urlMinusSessionid.equals(product));
+    }
 }
