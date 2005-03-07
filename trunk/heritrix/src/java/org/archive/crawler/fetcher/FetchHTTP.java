97660b2ba1df228416394da0d8ef748e9e0fb7e5
stack-sf <stack-sf@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2005-03-07 22:31:39 +0000

    * src/java/org/archive/crawler/fetcher/FetchHTTP.java     Remove import of non-exitent class (Import broke the build).

--- trunk/heritrix/src/java/org/archive/crawler/fetcher/FetchHTTP.java
+++ trunk/heritrix/src/java/org/archive/crawler/fetcher/FetchHTTP.java
@@ -90,7 +90,6 @@ import org.archive.httpclient.ConfigurableX509TrustManager;
 import org.archive.httpclient.HttpRecorderGetMethod;
 import org.archive.httpclient.HttpRecorderMethod;
 import org.archive.httpclient.HttpRecorderPostMethod;
-import org.archive.httpclient.SingleHttpConnectionManager;
 import org.archive.httpclient.ThreadLocalHttpConnectionManager;
 import org.archive.io.RecorderLengthExceededException;
 import org.archive.io.RecorderTimeoutException;
