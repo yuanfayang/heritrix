084b921a1158b8e253587ce89a09fb196d692ce9
stack-sf <stack-sf@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2005-04-01 02:55:00 +0000

    Fix for '[ 1156363 ] Flash SWF Extractor Unexpected end of input' * src/java/org/archive/crawler/extractor/ExtractorSWF.java     (parseDefineFont2): Dumb removal of 'int reservedFlags = in.readUI8();'  -- because     reservedFalgs is unused -- threw off the parse.

--- trunk/heritrix/src/java/org/archive/crawler/extractor/ExtractorSWF.java
+++ trunk/heritrix/src/java/org/archive/crawler/extractor/ExtractorSWF.java
@@ -159,108 +159,112 @@ implements CoreAttributeConstants {
              * Override just so I can comment out a System.out.println
              * that was left in parent TagParser#parseDefineFont2.
              */
-            protected void parseDefineFont2(InStream in)
-            throws IOException {
+            protected void parseDefineFont2(InStream in) throws IOException {
                 int id = in.readUI16();
                 int flags = in.readUI8();
+                int reservedFlags = in.readUI8();
+
                 int nameLength = in.readUI8();
-                String name = new String( in.read( nameLength ) );
+                String name = new String(in.read(nameLength));
+
                 int glyphCount = in.readUI16();
                 Vector glyphs = new Vector();
-                int[] offsets = new int[ glyphCount + 1 ];
-                boolean is32 = ( flags & FONT2_32OFFSETS ) != 0;
-                for( int i = 0; i <= glyphCount; i++ ){
-                    offsets[i] = is32 ? (int)in.readUI32() : in.readUI16();
+
+                int [] offsets = new int[glyphCount + 1];
+                boolean is32 = (flags & FONT2_32OFFSETS) != 0;
+                for (int i = 0; i <= glyphCount; i++) {
+                    offsets[i] = is32? (int)in.readUI32(): in.readUI16();
                 }
-                for( int i = 1; i <= glyphCount; i++ ) {
-                    int glyphSize = offsets[i] - offsets[i-1];
+
+                for (int i = 1; i <= glyphCount; i++) {
+                    int glyphSize = offsets[i] - offsets[i - 1];
                     // Here is an SWF which returns massive size to read
                     // causing OOME:
                     // http://pya.cc/pyaimg/img3/2004080708.swf
                     if (glyphSize > MAX_READ_SIZE) {
-                        throw new IOException("Glyphsize to read is too big " +
-                            glyphSize + " (Max is " + MAX_READ_SIZE + ").");
+                        throw new IOException("Glyphsize to read is too big "
+                                + glyphSize + " (Max is " + MAX_READ_SIZE
+                                + ").");
                     }
-                    byte[] glyphBytes = in.read( glyphSize );
-                    glyphs.addElement( glyphBytes );
+                    byte [] glyphBytes = in.read(glyphSize);
+                    glyphs.addElement(glyphBytes);
                 }
-                
-                boolean isWide = (( flags & FONT2_WIDECHARS ) != 0 ) ||
-                ( glyphCount > 256 );
-                
-                int[] codes = new int[ glyphCount ];
-                for( int i = 0; i < glyphCount; i++ ) {
-                    codes[i] = isWide ? in.readUI16() : in.readUI8();
+
+                boolean isWide = ((flags & FONT2_WIDECHARS) != 0)
+                        || (glyphCount > 256);
+
+                int [] codes = new int[glyphCount];
+                for (int i = 0; i < glyphCount; i++) {
+                    codes[i] = isWide? in.readUI16(): in.readUI8();
                 }
-                
-                // Commented out.  Was leaving dross in heritrix_out.log.
+
+                // Commented out. Was leaving dross in heritrix_out.log.
                 // System.out.println( "glyphCount=" + glyphCount +
                 // " flags=" + Integer.toBinaryString( flags ) );
-                
+
                 int ascent = 0;
                 int descent = 0;
                 int leading = 0;
-                int[]  advances = new int[0];
-                Rect[] bounds   = new Rect[0];
-                int[]  kerningCodes1      = new int[0];
-                int[]  kerningCodes2      = new int[0];
-                int[]  kerningAdjustments = new int[0];
-                if( ( flags & FONT2_HAS_LAYOUT ) != 0 ) {
-                    ascent  = in.readSI16();
+                int [] advances = new int[0];
+                Rect [] bounds = new Rect[0];
+                int [] kerningCodes1 = new int[0];
+                int [] kerningCodes2 = new int[0];
+                int [] kerningAdjustments = new int[0];
+
+                if ((flags & FONT2_HAS_LAYOUT) != 0) {
+                    ascent = in.readSI16();
                     descent = in.readSI16();
                     leading = in.readSI16();
-                    advances = new int[ glyphCount ];
-                    
-                    for( int i = 0; i < glyphCount; i++ ) {
+
+                    advances = new int[glyphCount];
+
+                    for (int i = 0; i < glyphCount; i++) {
                         advances[i] = in.readSI16();
                     }
-                    bounds = new Rect[ glyphCount ];
-                    
-                    for( int i = 0; i < glyphCount; i++ ) {
-                        bounds[i] = new Rect( in );
+
+                    bounds = new Rect[glyphCount];
+
+                    for (int i = 0; i < glyphCount; i++) {
+                        bounds[i] = new Rect(in);
                     }
-                    
+
                     int kerningCount = in.readUI16();
-                    kerningCodes1      = new int[ kerningCount ];
-                    kerningCodes2      = new int[ kerningCount ];
-                    kerningAdjustments = new int[ kerningCount ];
-                    
-                    for( int i = 0; i < kerningCount; i++ ) {
-                        kerningCodes1     [i] = isWide ?
-                                in.readUI16() : in.readUI8();
-                                kerningCodes2     [i] = isWide ?
-                                        in.readUI16() : in.readUI8();
-                                        kerningAdjustments[i] = in.readSI16();
+
+                    kerningCodes1 = new int[kerningCount];
+                    kerningCodes2 = new int[kerningCount];
+                    kerningAdjustments = new int[kerningCount];
+
+                    for (int i = 0; i < kerningCount; i++) {
+                        kerningCodes1[i] = isWide? in.readUI16(): in.readUI8();
+                        kerningCodes2[i] = isWide? in.readUI16(): in.readUI8();
+                        kerningAdjustments[i] = in.readSI16();
                     }
                 }
-                
-                SWFVectors vectors = mTagtypes.tagDefineFont2( id, flags,
-                        name, glyphCount,
-                        ascent, descent, leading,
-                        codes, advances, bounds,
-                        kerningCodes1,
-                        kerningCodes2,
-                        kerningAdjustments );
-                
-                if( vectors == null ) return;
-                
-                if( glyphs.isEmpty() ) {
+
+                SWFVectors vectors = mTagtypes.tagDefineFont2(id, flags, name,
+                        glyphCount, ascent, descent, leading, codes, advances,
+                        bounds, kerningCodes1, kerningCodes2,
+                        kerningAdjustments);
+
+                if (vectors == null)
+                    return;
+
+                if (glyphs.isEmpty()) {
                     vectors.done();
                 } else {
-                    for( Enumeration e = glyphs.elements();
-                    e.hasMoreElements(); ) {
-                        byte[] glyphBytes = (byte[])e.nextElement();
-                        
-                        InStream glyphIn = new InStream( glyphBytes );
-                        
-                        parseShape( glyphIn, vectors, false, false );
+                    for (Enumeration enum = glyphs.elements(); enum
+                            .hasMoreElements();) {
+                        byte [] glyphBytes = (byte [])enum.nextElement();
+
+                        InStream glyphIn = new InStream(glyphBytes);
+
+                        parseShape(glyphIn, vectors, false, false);
                     }
                 }
             }
-            
         };
     }
-
+    
     public String report() {
         StringBuffer ret = new StringBuffer();
         ret.append("Processor: org.archive.crawler.extractor.ExtractorSWF\n");
