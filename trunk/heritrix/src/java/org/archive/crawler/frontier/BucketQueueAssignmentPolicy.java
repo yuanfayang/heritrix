59576f2c3945491151dc6a6669dbc9627361080c
stack-sf <stack-sf@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2005-06-13 20:59:38 +0000

    Fix for '[ 1219715 ] [patch] Signature change broke BucketQueueAssignmentPolicy' Contributed by Christian Kohlschuetter.  Here is comment from Christian:
        " The change of QueueAssignmentPolicy.getClassKey's
        method signature from CrawlURI to CandidateURI has
        broken BucketQueueAssignmentPolicy, making it behave
        like HostnameQueueAssignmentPolicy (it inherited from
        that class)"
    
    * src/java/org/archive/crawler/frontier/BucketQueueAssignmentPolicy.java
        (getClassKey): Get host from ServerCache.

--- trunk/heritrix/src/java/org/archive/crawler/frontier/BucketQueueAssignmentPolicy.java
+++ trunk/heritrix/src/java/org/archive/crawler/frontier/BucketQueueAssignmentPolicy.java
@@ -1,7 +1,7 @@
 /* BucketQueueAssignmentPolicy
  * 
  * $Header$
-* 
+ * 
  * Created on May 06, 2005
  *
  *  Copyright (C) 2005 Christian Kohlschuetter
@@ -25,27 +25,39 @@
  */
 package org.archive.crawler.frontier;
 
+import org.apache.commons.httpclient.URIException;
+import org.archive.crawler.datamodel.CandidateURI;
 import org.archive.crawler.datamodel.CrawlHost;
-import org.archive.crawler.datamodel.CrawlURI;
 import org.archive.crawler.framework.CrawlController;
 
 /**
-* Uses the target IPs as basis for queue-assignment,
-* distributing them over a fixed number of sub-queues.
-* 
-* @author Christian Kohlschuetter
-*/
-public class BucketQueueAssignmentPolicy
-extends HostnameQueueAssignmentPolicy {
+ * Uses the target IPs as basis for queue-assignment,
+ * distributing them over a fixed number of sub-queues.
+ * 
+ * @author Christian Kohlschuetter
+ */
+public class BucketQueueAssignmentPolicy extends QueueAssignmentPolicy {
     private static final int DEFAULT_NOIP_BITMASK = 1023;
     private static final int DEFAULT_QUEUES_HOSTS_MODULO = 1021;
-    public String getClassKey(CrawlController controller, CrawlURI curi) {
-        CrawlHost host = controller.getServerCache().getHostFor(curi);
+
+    public String getClassKey(final CrawlController controller,
+        final CandidateURI curi) {
+        
+        CrawlHost host;
+        try {
+            host = controller.getServerCache().getHostFor(
+                curi.getUURI().getReferencedHost());
+        } catch (URIException e) {
+            // FIXME error handling
+            e.printStackTrace();
+            host = null;
+        }
         if(host == null) {
             return "NO-HOST";
         } else if(host.getIP() == null) {
             return "NO-IP-".concat(Integer.toString(Math.abs(host.getHostName()
-                .hashCode()) & DEFAULT_NOIP_BITMASK));
+                .hashCode())
+                & DEFAULT_NOIP_BITMASK));
         } else {
             return Integer.toString(Math.abs(host.getIP().hashCode())
                 % DEFAULT_QUEUES_HOSTS_MODULO);
