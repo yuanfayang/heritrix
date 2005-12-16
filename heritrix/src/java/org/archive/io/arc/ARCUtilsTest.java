010f8787e446b8a01abf980f6256c80fa368d784
stack-sf <stack-sf@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2005-12-16 02:46:46 +0000

    * src/java/org/archive/io/GzipHeader.java     If no gzip magic, throw particular exception (A new one added with this     commit). * src/java/org/archive/io/RandomAccessInputStream.java     Javadoc. * src/java/org/archive/io/arc/ARCUtils.java     Add utils for testing streams are compressed.     Fix double open of file to test.     (testCompressedARCStream, testCompressedRepositionalStream,     testCompressedStream): Added. * src/java/org/archive/io/arc/ARCUtilsTest.java     Add tests for new methods. * src/java/org/archive/io/NoGzipMagicException.java     Added.

--- heritrix/src/java/org/archive/io/arc/ARCUtilsTest.java
+++ heritrix/src/java/org/archive/io/arc/ARCUtilsTest.java
@@ -22,7 +22,14 @@
  */
 package org.archive.io.arc;
 
+import it.unimi.dsi.mg4j.io.RepositionableStream;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.net.URISyntaxException;
+
+import org.archive.io.GzippedInputStream;
+import org.archive.io.RandomAccessInputStream;
 
 import junit.framework.TestCase;
 
@@ -37,5 +44,38 @@ public class ARCUtilsTest extends TestCase {
         assertEquals(filename,
                 ARCUtils.parseArcFilename("rsync://archive.org/tmp/one.two/" +
                     filename)); 
+    }
+    
+    public void testCompressedStream() throws IOException {
+        byte [] bytes = "test".getBytes();
+        ByteArrayInputStream baos = new ByteArrayInputStream(bytes);
+        assertFalse(ARCUtils.testCompressedStream(baos));
+        
+        byte [] gzippedMetaData = GzippedInputStream.gzip(bytes);
+        baos = new ByteArrayInputStream(gzippedMetaData);
+        assertTrue(ARCUtils.testCompressedStream(baos));
+        
+        gzippedMetaData = GzippedInputStream.gzip(bytes);
+        final RepositionableByteArrayInputStream rbaos =
+            new RepositionableByteArrayInputStream(gzippedMetaData);
+        final int totalBytes = gzippedMetaData.length;
+        assertTrue(ARCUtils.testCompressedRepositionalStream(rbaos));
+        long available = rbaos.available();
+        assertEquals(available, totalBytes);
+        assertEquals(rbaos.position(), 0);
+    }
+    
+    private class RepositionableByteArrayInputStream
+    extends ByteArrayInputStream implements RepositionableStream {
+        public RepositionableByteArrayInputStream(final byte [] bytes) {
+            super(bytes);
+        }
+        
+        public void position(long p) {
+            this.pos = (int)p;
+        }
+        public long position() {
+            return this.pos;
+        }
     }
 }
