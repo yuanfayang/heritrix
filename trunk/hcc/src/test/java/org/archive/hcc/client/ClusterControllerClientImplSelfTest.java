4f0b1ddfacf647253904079a1c81688390097d21
dbernstein <dbernstein@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2005-12-21 01:03:53 +0000

    added test to make sure that rapidly firing up multiple clients (20) and shutting them as quickly produces the expected notifications.

--- trunk/hcc/src/test/java/org/archive/hcc/client/ClusterControllerClientImplSelfTest.java
+++ trunk/hcc/src/test/java/org/archive/hcc/client/ClusterControllerClientImplSelfTest.java
@@ -1,6 +1,9 @@
 package org.archive.hcc.client;
 
 import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 public class ClusterControllerClientImplSelfTest
@@ -58,5 +61,73 @@ public class ClusterControllerClientImplSelfTest
             cc.removeCrawlerLifecycleListener(l);
         }
     }
+    
+    public void testCreateMultipleCrawlersInRapidSuccession() {
+        final List<Crawler> crawlers = new LinkedList<Crawler>();
+        
+        final CountDownLatch createdLatch = new CountDownLatch(10);
+        final CountDownLatch destroyedLatch = new CountDownLatch((int)createdLatch.getCount());
+        
+        class MyListener implements CrawlerLifecycleListener{
+            
+            /* (non-Javadoc)
+             * @see org.archive.hcc.client.CrawlerLifecycleListener#crawlerCreated(org.archive.hcc.client.Crawler)
+             */
+            public void crawlerCreated(Crawler c) {
+                crawlers.add(c);
+                createdLatch.countDown();
+                
+            }
+            
+            /* (non-Javadoc)
+             * @see org.archive.hcc.client.CrawlerLifecycleListener#crawlerDestroyed(org.archive.hcc.client.Crawler)
+             */
+            public void crawlerDestroyed(Crawler c) {
+                crawlers.remove(c);
+                destroyedLatch.countDown();
+            }
+        };
+        
+        MyListener l = new MyListener();
+        try {
+            cc.addCrawlerLifecycleListener(l);
+
+            int count = (int)createdLatch.getCount();
+            for(int i = 0; i < count; i++){
+                Crawler c = cc.createCrawler();
+            }
+
+            try {
+                assertTrue(createdLatch.await(
+                        30 * 1000,
+                        TimeUnit.MILLISECONDS));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            
+            assertEquals(count, crawlers.size());
+
+            for(Crawler c : new LinkedList<Crawler>(crawlers)){
+                c.destroy();
+            }
+
+            try {
+                assertTrue(destroyedLatch.await(
+                        30 * 1000,
+                        TimeUnit.MILLISECONDS));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            
+
+            
+        } catch (ClusterException e) {
+            e.printStackTrace();
+            assertFalse(true);
+        } finally {
+            cc.removeCrawlerLifecycleListener(l);
+        }
+    }
+
 
 }
