f36f46d0b6959da4a7b9d9df0f765d5e009236ba
dbernstein <dbernstein@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2006-01-06 01:33:12 +0000

    added "totalProcessedBytes" to CompositeData key array.

--- trunk/hcc/src/main/java/org/archive/hcc/client/ClusterControllerClientImpl.java
+++ trunk/hcc/src/main/java/org/archive/hcc/client/ClusterControllerClientImpl.java
@@ -302,8 +302,9 @@ class ClusterControllerClientImpl implements ClusterControllerClient{
                     "queuedUriCount", "docsPerSecond", "freeMemory",
                     "downloadFailures", "totalKBPerSec", "totalMemory",
                     "currentKBPerSec", "currentDocsPerSecond", "busyThreads",
-                    "averageDepth", "discoveredUriCount", "congestionRatio" };
+                    "averageDepth", "discoveredUriCount", "congestionRatio","totalProcessedBytes" };
             Map map = new HashMap();
+            
             Object[] values = cd.getAll(keys);
             for (int i = 0; i < keys.length; i++) {
                 map.put(keys[i], values[i]);
