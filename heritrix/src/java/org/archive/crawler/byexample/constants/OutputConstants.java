bcacdb3afb0034883a03f01c92534c0328f5bdb4
bemike <bemike@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2006-07-07 10:12:29 +0000

    Updates in File Paths constants

--- heritrix/src/java/org/archive/crawler/byexample/constants/OutputConstants.java
+++ heritrix/src/java/org/archive/crawler/byexample/constants/OutputConstants.java
@@ -1,19 +1,22 @@
 package org.archive.crawler.byexample.constants;
 
+import java.io.File;
+
 public class OutputConstants {
     
     // Print-out Constants
     
     public static final String KEY_SEPARATOR="~";
     public static final String ENTRY_SEPARATOR=":";
+    public static final String PATH_SEPARATOR=File.separator;
     
     // Folder structure constants
-    public static final String BYEXAMPLE_HOME="byexample/";
-    public static final String CONFIG_HOME=BYEXAMPLE_HOME+"conf/";
-    public static final String JOBS_HOME=BYEXAMPLE_HOME+"jobs/";
-    public static final String PREPROCESS_FILES_HOME="/preprocess/";    
-    public static final String CLUSTERING_FILES_HOME="/clustering/";
-    public static final String CLASSIFICATION_FILES_HOME="/classification/";
+    public static final String BYEXAMPLE_HOME="byexample"+PATH_SEPARATOR;
+    public static final String CONFIG_HOME=BYEXAMPLE_HOME+"conf"+PATH_SEPARATOR;
+    public static final String JOBS_HOME=BYEXAMPLE_HOME+"jobs"+PATH_SEPARATOR;
+    public static final String PREPROCESS_FILES_HOME="preprocess"+PATH_SEPARATOR;    
+    public static final String CLUSTERING_FILES_HOME="clustering"+PATH_SEPARATOR;
+    public static final String CLASSIFICATION_FILES_HOME="classification"+PATH_SEPARATOR;
     
     // Output file-names constants
     public static final String JOB_NAME_PREFIX="byexamplejob-";
@@ -24,5 +27,26 @@ public class OutputConstants {
     public static final String DOCUMENTS_CLUSTERING_LISTING_FILENAME="clusteringDocs.txt";
     public static final String PREPROCESS_XML_FILENAME="preprocess-results.xml";
     public static final String CLUSTERING_XML_FILENAME="clustering-results.xml";
+    
+    
+    /**
+     * Returns path under JOBS_HOME for a job of given id
+     * @param id
+     * @return job path
+     */
+    public static String getJobPath(String id){
+        return JOBS_HOME+id+PATH_SEPARATOR;
+    }
+    
+    public static String getPreprocessPath(String id){
+        return getJobPath(id)+PREPROCESS_FILES_HOME;
+    }
+    
+    public static String getClusteringPath(String id){
+        return getJobPath(id)+CLUSTERING_FILES_HOME;
+    }
 
+    public static String getClassificationPath(String id){
+        return getJobPath(id)+CLASSIFICATION_FILES_HOME;
+    }
 }
