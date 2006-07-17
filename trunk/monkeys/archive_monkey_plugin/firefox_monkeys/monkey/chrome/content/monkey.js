2158ba1ce6d341bc449e2cb384c097c787cd9049
evahlis <evahlis@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2006-07-17 19:19:20 +0000

    The monkey browser now retrieves a task, goes to the given url and extracts all the links. cool.

--- trunk/monkeys/archive_monkey_plugin/firefox_monkeys/monkey/chrome/content/monkey.js
+++ trunk/monkeys/archive_monkey_plugin/firefox_monkeys/monkey/chrome/content/monkey.js
@@ -6,41 +6,56 @@ function old_magic_button() {
 function magic_button() {
     //window.addEventListener("load", linksGetter, true);
     //getBrowser().contentDocument.location = "http://www.google.com";
-	
-	http_request = new XMLHttpRequest();
-	http_request.onreadystatechange = showResponse;
-	http_request.open('GET', 'http://localhost:8081/monkey?method=getTask&mid=MonkeyOne', true);
-	http_request.send(null);
+    
+    http_request = new XMLHttpRequest();
+    http_request.onreadystatechange = processControllerResponse;
+    //http_request.onreadystatechange = showResponse;
+    http_request.open('GET', 'http://localhost:8081/monkey?method=getTask&mid=MonkeyOne', true);
+    http_request.send(null);
 }
 
 /* Sends a ping request to the harness servlet */
 function pinger() {
-	//getBrowser().contentDocument.write("pinging...<br/>");
-	http_request = new XMLHttpRequest();
-	//http_request.onreadystatechange = showResponse;
-	http_request.open('GET', 'http://0.0.0.0:8082/harness?method=heartBeat', true);
-	http_request.send(null);
+    //getBrowser().contentDocument.write("pinging...<br/>");
+    http_request = new XMLHttpRequest();
+    //http_request.onreadystatechange = showResponse;
+    http_request.open('GET', 'http://0.0.0.0:8082/harness?method=heartBeat', true);
+    http_request.send(null);
+}
+function processControllerResponse() {
+    if (http_request.readyState == 4) {
+	if (http_request.status == 400) {
+	    // handle failed task request
+	    alert(http_request.responseText);
+	} else {
+	    var taskData = http_request.responseText.parseJSON();
+	    getBrowser().contentDocument.write("["+taskData.URL+"]");
+	    window.addEventListener("load", linksGetter, true);
+	    getBrowser().contentDocument.location = taskData.URL;
+	}
+    }
 }
 
 function showResponse() {
-	if (http_request.readyState == 4) {
-		getBrowser().contentDocument.write("["+http_request.responseText+"]");
-	}
+    if (http_request.readyState == 4) {
+	getBrowser().contentDocument.write("["+http_request.status + "," + http_request.responseText+"]");
+    }
 }
 
 /* Initializes the monkey plugin. */
 function monkey_load() {
-	try {
-		netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
-		var jsLoader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"].
-					   getService(Components.interfaces.mozIJSSubScriptLoader);
-		jsLoader.loadSubScript("chrome://monkey/content/prototype.js");
-		jsLoader.loadSubScript("chrome://monkey/content/link_getter.js");
-	} catch (e) {
-		alert(e);
-	}
+    try {
+	netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
+	var jsLoader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"].
+	    getService(Components.interfaces.mozIJSSubScriptLoader);
+	jsLoader.loadSubScript("chrome://monkey/content/prototype.js");
+	jsLoader.loadSubScript("chrome://monkey/content/link_getter.js");
+	jsLoader.loadSubScript("chrome://monkey/content/json.js");
+    } catch (e) {
+	alert(e);
+    }
     var prefs = Components.classes["@mozilla.org/preferences-service;1"].
-                    getService(Components.interfaces.nsIPrefBranch);
+	getService(Components.interfaces.nsIPrefBranch);
     prefs.setCharPref("browser.startup.homepage", "about:blank");
     window.setInterval("pinger()", 10000);
 }
