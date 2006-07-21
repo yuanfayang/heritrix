5ad3d678cbad2c2690a245346a13844bd4dd2c2c
bemike <bemike@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2006-07-21 15:03:19 +0000

    Added javadoc

--- heritrix/src/java/org/archive/crawler/byexample/constants/AlgorithmConstants.java
+++ heritrix/src/java/org/archive/crawler/byexample/constants/AlgorithmConstants.java
@@ -3,24 +3,68 @@ package org.archive.crawler.byexample.constants;
 import java.io.FileInputStream;
 import java.util.Properties;
 
+/**
+ * This class contains all parameters used by clustering and classification algorithms
+ * To change these parameters values, user should edit the properties file found under BY_EXAMPLE_HOME/CONF folder 
+ * 
+ * @author Michael Bendersky
+ *
+ */
 public class AlgorithmConstants {
     
     // Apriori algorithm constants
     
+    /**
+     * Only terms that appear in more than <i>MIN_GLOBAL_SUPPORT</i> per cent of the documents in the collection
+     * will be counted as 1-frequent itemsets
+     */
     public static int MIN_GLOBAL_SUPPORT;
+    
+    /**
+     * <i>MAX_DEPTH</i> determines the depth of clustering hierarchy. Itemsets in the deepest hierarchy level
+     * can contain at most <i>MAX_DEPTH</i> items.
+     */    
     public static int MAX_DEPTH;
+    
+    /**
+     * Only <i>MAX_1_FREQUENT_TERMS></i> with top global support will be considereed in building the clustering hierarchy.
+     * Please note that setting this value to too high value, may cause significant decrease in algorithm performance, as 
+     * total number of possible k-frequent itemsets will increase exponentially. 
+     */
     public static int MAX_1_FREQUENT_TERMS;
     
     // Clustering algorithm constants 
     
-    public static int MIN_CLUSTER_SUPPORT; 
+    /**
+     * Terms that appear in more than <i>MIN_CLUSTER_SUPPORT</i> per cent of the documents in the cluster will be 
+     * considered as terms associated with the cluster
+     */
+    public static int MIN_CLUSTER_SUPPORT;
+    
+    /**
+     * Clusters that contain less than <i>MIN_SIZE_TO_PRUNE</i> per cent of the documents in the collection will 
+     * be merged with larger most similar cluster and pruned.
+     */
     public static int MIN_SIZE_TO_PRUNE;
+    
+    /**
+     * Label of special cluster that contains all the documents that couldn't be assigned to any cluster
+     * by clustering/classification algorithm
+     */
     public static String UNCLASSIFIED_LABEL;
     
     // Classification algorithm constants
     
+    /**
+     * During classification stage each document will be assigned to at most <i>TOP_CLASSIFICATIONS</i> clusters 
+     */
     public static int TOP_CLASSIFICATIONS;
     
+    /**
+     * Reads algorithm parameters from properties file
+     * @param path properties file path
+     * @throws Exception
+     */
     public static void readPropeties(String path) throws Exception{
         // Read properties file.
         Properties properties = new Properties();
