3ac2a560558f82208f311fc46ce55557a61181cf
stack-sf <stack-sf@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2006-07-27 01:53:31 +0000

    Make a start on ANVL processing (Experimental format for WARC named fields). * src/java/org/archive/util/anvl/Comment.java * src/java/org/archive/util/anvl/Element.java * src/java/org/archive/util/anvl/Label.java * src/java/org/archive/util/anvl/Record.java * src/java/org/archive/util/anvl/RecordTest.java * src/java/org/archive/util/anvl/SubElement.java * src/java/org/archive/util/anvl/Value.java     Added.

---
+++ trunk/heritrix/src/java/org/archive/util/anvl/SubElement.java
@@ -0,0 +1,68 @@
+/* SubElement
+*
+* $Id$
+*
+* Created on July 26, 2006.
+*
+* Copyright (C) 2006 Internet Archive.
+*
+* This file is part of the Heritrix web crawler (crawler.archive.org).
+*
+* Heritrix is free software; you can redistribute it and/or modify
+* it under the terms of the GNU Lesser Public License as published by
+* the Free Software Foundation; either version 2.1 of the License, or
+* any later version.
+*
+* Heritrix is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Lesser Public License for more details.
+*
+* You should have received a copy of the GNU Lesser Public License
+* along with Heritrix; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+package org.archive.util.anvl;
+
+/**
+ * Abstract ANVL 'data element' part.
+ * @author stack
+ * @see http://www.cdlib.org/inside/diglib/ark/anvlspec.pdf
+ */
+abstract class SubElement {
+    private final String e;
+
+    protected SubElement() {
+        this(null);
+    }
+
+    public SubElement(final String s) {
+        this.e = baseCheck(s);
+    }
+
+    protected String baseCheck(final String s) {
+        // Check for null.
+        if (s == null || s.length() <= 0) {
+            throw new IllegalArgumentException("Can't be null or empty");
+        }
+        // Check for CRLF.
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isISOControl(c) && !Character.isWhitespace(c)) {
+                throw new IllegalArgumentException(s +
+                    " contains a control character(s): 0x" +
+                    Integer.toHexString(i));
+            } else if (s.charAt(i) == '\r' && (i + 1 < s.length())
+                    && s.charAt(i + 1) == '\n') {
+                throw new IllegalArgumentException(s + " contains CRLF: " +
+                    "http://en.wikipedia.org/wiki/CRLF");
+            }
+        }
+        return s;
+    }
+    
+    @Override
+    public String toString() {
+        return e;
+    }
+}
