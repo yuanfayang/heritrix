61119c433be451b8babbaba04180e6d7c490cb8a
evahlis <evahlis@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2006-08-14 15:16:49 +0000

    fixed some synchronization bugs

--- monkeys/archive_monkey_plugin/firefox_monkeys/monkey/chrome/content/link_getter.js
+++ monkeys/archive_monkey_plugin/firefox_monkeys/monkey/chrome/content/link_getter.js
@@ -1,32 +1,33 @@
 function linksGetter() {
-  window.removeEventListener("load", linksGetter, true);
-  var links = getBrowser().contentDocument.links;
-  var res = "";
+    window.removeEventListener("load", linksGetter, true);
+    var links = getBrowser().contentDocument.links;
+    var res = "";
 
-  $A(links).each(function(l) {
-      var linkHref = getBrowser().contentDocument.createTextNode(l.href);
-      var lineBreak = getBrowser().contentDocument.createElement("br");
-      res += linkHref.data + "\r\n";
-//      alert(linkHref.data);
-//      getBrowser().contentDocument.body.appendChild(linkHref);
-//      getBrowser().contentDocument.body.appendChild(lineBreak);
-      });
-  linksSubmitOperation(res);
+    $A(links).each(function(l) {
+            var linkHref = getBrowser().contentDocument.createTextNode(l.href);
+            var lineBreak = getBrowser().contentDocument.createElement("br");
+            res += linkHref.data + "\r\n";
+            });
+    linksSubmitOperation(res);
 }
 
 function linksSubmitOperation(links) {
-  http_request = new XMLHttpRequest();
-  http_request.onreadystatechange = linksSubmitSuccess;
-  http_request.open('POST', CONTROLLER_URL + '?method=urls', true);
-  http_request.send(links);
+    http_request = new XMLHttpRequest();
+    http_request.onreadystatechange = linksSubmitSuccess;
+    http_request.open('POST', LINKS_SUBMIT_URL, true);
+    http_request.send(links);
 }
 
 function linksSubmitSuccess() {
-  if (http_request.readyState == 4) {
-    if (http_request.status == 400) {
-      notifyControllerOfFailure();
-    } else {
-      notifyControllerOfSuccess(taskData.id);
+    try {
+        if (http_request.readyState == 4) {
+            if (http_request.status == 400) {
+                notifyControllerOfFailure();
+            } else {
+                notifyControllerOfSuccess(taskData.id);
+            }
+        }
+    } catch (e) {
+        startCycle();
     }
-  }
 }
