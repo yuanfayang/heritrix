de86630e2b7e1c08f92635f6f5982f297039e3df
evahlis <evahlis@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2006-08-14 13:48:47 +0000

    many updates to monkey plugin. it's almost ready.

--- trunk/monkeys/archive_monkey_plugin/firefox_monkeys/monkey/chrome/content/link_getter.js
+++ trunk/monkeys/archive_monkey_plugin/firefox_monkeys/monkey/chrome/content/link_getter.js
@@ -1,17 +1,32 @@
 function linksGetter() {
-	window.removeEventListener("load", linksGetter, true);
-	var links = getBrowser().contentDocument.links;
-/*	for(var i = 0; i < links.length; i++) {
-	  var linkHref = getBrowser().contentDocument.createTextNode(links[i].href);
-	  var lineBreak = getBrowser().contentDocument.createElement("br");
-	  getBrowser().contentDocument.body.appendChild(linkHref);
-	  getBrowser().contentDocument.body.appendChild(lineBreak);
-	}*/
-	
-	$A(links).each(function(l) {
-	  var linkHref = getBrowser().contentDocument.createTextNode(l.href);
-	  var lineBreak = getBrowser().contentDocument.createElement("br");
-	  getBrowser().contentDocument.body.appendChild(linkHref);
-	  getBrowser().contentDocument.body.appendChild(lineBreak);
-	});
+  window.removeEventListener("load", linksGetter, true);
+  var links = getBrowser().contentDocument.links;
+  var res = "";
+
+  $A(links).each(function(l) {
+      var linkHref = getBrowser().contentDocument.createTextNode(l.href);
+      var lineBreak = getBrowser().contentDocument.createElement("br");
+      res += linkHref.data + "\r\n";
+//      alert(linkHref.data);
+//      getBrowser().contentDocument.body.appendChild(linkHref);
+//      getBrowser().contentDocument.body.appendChild(lineBreak);
+      });
+  linksSubmitOperation(res);
+}
+
+function linksSubmitOperation(links) {
+  http_request = new XMLHttpRequest();
+  http_request.onreadystatechange = linksSubmitSuccess;
+  http_request.open('POST', CONTROLLER_URL + '?method=urls', true);
+  http_request.send(links);
+}
+
+function linksSubmitSuccess() {
+  if (http_request.readyState == 4) {
+    if (http_request.status == 400) {
+      notifyControllerOfFailure();
+    } else {
+      notifyControllerOfSuccess(taskData.id);
+    }
+  }
 }
