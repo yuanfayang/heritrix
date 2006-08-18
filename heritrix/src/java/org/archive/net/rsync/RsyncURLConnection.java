71d08ec9c361a7758ad9d872d0587dee8d0d5353
stack-sf <stack-sf@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2006-08-18 16:13:32 +0000

    Move duplicated code up into an abstract URLConnection.  Also, remove options on the md5 handler.  No need.  Can just call a wrapper script to provide any needed options. * src/java/org/archive/net/md5/Handler.java     Javadoc.  Point at org.archive.net to find scheme handlers. * src/java/org/archive/net/md5/Md5URLConnection.java * src/java/org/archive/net/rsync/RsyncURLConnection.java     Refactoring. Move bulk to new DownloadURLConnection superclass. * src/java/org/archive/net/DownloadURLConnection.java     Added.

--- heritrix/src/java/org/archive/net/rsync/RsyncURLConnection.java
+++ heritrix/src/java/org/archive/net/rsync/RsyncURLConnection.java
@@ -24,102 +24,34 @@
  */
 package org.archive.net.rsync;
 
-import java.io.BufferedInputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.net.URL;
-import java.net.URLConnection;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-import org.archive.util.ProcessUtils;
+import org.archive.net.DownloadURLConnection;
 
 /**
  * Rsync URL connection.
  * @author stack
  * @version $Date$, $Revision$
  */
-public class RsyncURLConnection extends URLConnection {
-    private static final String CLASSNAME = RsyncURLConnection.class.getName();
-    private final Logger LOGGER = Logger.getLogger(CLASSNAME);
-    private static final File TMPDIR =
-        new File(System.getProperty("java.io.tmpdir", "/tmp"));
-    private static final String RSYNC =
-        System.getProperty(CLASSNAME + ".path", "rsync");
-    private static final String RSYNC_TIMEOUT =
-        System.getProperty(CLASSNAME + ".timeout", "300");
-    private File downloadFile = null;
+public class RsyncURLConnection extends DownloadURLConnection {
+    private final String RSYNC_TIMEOUT =
+    	System.getProperty(RsyncURLConnection.class.getName() + ".timeout",
+    		"300");
 
     protected RsyncURLConnection(URL u) {
         super(u);
     }
-
-    /**
-     * Do rsync copy to local file.
-     * File is available via {@link #getFile()}.
-     * @throws IOException 
-     */
-    public void connect() throws IOException {
-        if (this.connected) {
-            return;
-        }
-        
-        this.downloadFile = File.createTempFile(CLASSNAME, null, TMPDIR);
-        try {
-            String [] cmd = new String[] {RSYNC, "--timeout=" + RSYNC_TIMEOUT,
-                this.url.toExternalForm(), this.downloadFile.getAbsolutePath()};
-            if (LOGGER.isLoggable(Level.FINE)) {
-                StringBuffer buffer = new StringBuffer();
-                for (int i = 0; i < cmd.length; i++) {
-                    if (i > 0) {
-                        buffer.append(" ");
-                    }
-                    buffer.append(cmd[i]);
-                }
-                LOGGER.fine("Rsyc command: " + buffer.toString());
-            }
-            ProcessUtils.exec(cmd);
-            // Assume download went smoothly.
-            this.connected = true;
-        } catch (IOException ioe) {
-            // Clean up my tmp file.
-            this.downloadFile.delete();
-            this.downloadFile = null;
-            // Rethrow.
-            throw ioe;
-        }
+    
+    protected String getScript() {
+    	return System.getProperty(this.getClass().getName() + ".path",
+    		"rsync");
     }
     
-    public File getFile() {
-        return this.downloadFile;
-    }
-    
-    protected void setFile(final File f) {
-        this.downloadFile = f;
-    }
-
-    public InputStream getInputStream() throws IOException {
-        if (!this.connected) {
-            connect();
-        }
-        
-        // Return BufferedInputStream so 'delegation' is done for me, so
-        // I don't have to implement all IS methods and pass to my
-        // 'delegate' instance.
-        final RsyncURLConnection connection = this;
-        return new BufferedInputStream(new FileInputStream(this.downloadFile)) {
-            private RsyncURLConnection ruc = connection;
-
-            public void close() throws IOException {
-                super.close();
-                if (this.ruc != null && this.ruc.getFile()!= null &&
-                    this.ruc.getFile().exists()) {
-                    this.ruc.getFile().delete();
-                    this.ruc.setFile(null);
-                }
-            }
-        };
+    @Override
+    protected String[] getCommand(final URL thisUrl,
+    		final File downloadFile) {
+    	return new String[] {getScript(), "--timeout=" + RSYNC_TIMEOUT,
+    		this.url.getPath(), downloadFile.getAbsolutePath()};  
     }
 }
