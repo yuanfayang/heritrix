1f1fe080e82dd51c47919823634574ec0a0ca386
paul_jack <paul_jack@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2006-09-14 00:48:49 +0000

    Initial commit for bug #1477371, which is not 100% fixed yet, but I want to save code before I get hit by a bus. This code does not support very large ( > 6 meg) or very small ( < 4K) .doc files. Those features and documentation to follow.
    * /src/java/org/archive/io/Endian.java
    New class for reading little or big endian values from streams.
    
    * /src/java/org/archive/io/SeekInputStream.java
    New abstract base class for repositionable input stream (the name 
    RepositionableInputStream was already taken).
    
    * /src/java/org/archive/io/RandomAccessInputStream.java
    Now derives from SeekInputStream.
    
    * /src/java/org/archive/util/IoUtils.java
    Added readFully(InputStream, byte[]) method.
    
    * /src/java/org/archive/util/ms/*
    Classes for memory-bounded reading of .doc file text, plus simple unit test
    that compares results against Apache POI.

---
+++ trunk/heritrix/src/java/org/archive/util/ms/DefaultBlockFileSystem.java
@@ -0,0 +1,95 @@
+/* DefaultBlockFileSystem
+*
+* Created on September 12, 2006
+*
+* Copyright (C) 2006 Internet Archive.
+*
+* This file is part of the Heritrix web crawler (crawler.archive.org).
+*
+* Heritrix is free software; you can redistribute it and/or modify
+* it under the terms of the GNU Lesser Public License as published by
+* the Free Software Foundation; either version 2.1 of the License, or
+* any later version.
+*
+* Heritrix is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Lesser Public License for more details.
+*
+* You should have received a copy of the GNU Lesser Public License
+* along with Heritrix; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+package org.archive.util.ms;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+import org.apache.poi.util.LittleEndian;
+import org.archive.io.SeekInputStream;
+import org.archive.util.IoUtils;
+
+
+
+public class DefaultBlockFileSystem implements BlockFileSystem {
+
+
+    private SeekInputStream input;
+    private HeaderBlock header;
+    
+    
+    public DefaultBlockFileSystem(SeekInputStream input) throws IOException {
+        this.input = input;
+        byte[] temp = new byte[512];
+        IoUtils.readFully(input, temp);
+        this.header = new HeaderBlock(ByteBuffer.wrap(temp));
+    }
+
+
+    public HeaderBlock getHeaderBlock() {
+        return header;
+    }
+
+
+    public Entry getRoot() throws IOException {
+        int block = header.getPropertiesStart();
+        input.position((block + 1) * 512);
+        return new DefaultEntry(this, input, 0);
+    }
+
+
+    public Entry getEntry(int propertyNumber) throws IOException {
+        if (propertyNumber < 0) {
+            return null;
+        }
+        int blockCount = propertyNumber / 4;
+        int remainder = propertyNumber % 4;
+        
+        // FIXME: Support XBAT and SBAT
+        int block = header.getPropertiesStart();
+        for (int i = 0; i < blockCount; i++) {
+            block = getNextBlock(block);
+        }
+
+        int filePos = (block + 1) * 512 + remainder * 128;
+        input.position(filePos);
+        
+        return new DefaultEntry(this, input, propertyNumber);
+    }
+
+
+    public int getNextBlock(int block) throws IOException {
+        // FIXME: Support for XBAT 
+        int headerBATIndex = block / 128;
+        int batBlockIndex = block % 128;
+        int batBlock = header.getBlockOffset(headerBATIndex);
+        input.position((batBlock + 1) * 512 + batBlockIndex * 4);
+        return LittleEndian.readInt(input);
+    }
+
+
+    
+    public SeekInputStream getRawInput() {
+        return input;
+    }
+}
---
+++ trunk/heritrix/src/java/org/archive/util/ms/Piece.java
@@ -0,0 +1,74 @@
+/* Piece
+*
+* Created on September 12, 2006
+*
+* Copyright (C) 2006 Internet Archive.
+*
+* This file is part of the Heritrix web crawler (crawler.archive.org).
+*
+* Heritrix is free software; you can redistribute it and/or modify
+* it under the terms of the GNU Lesser Public License as published by
+* the Free Software Foundation; either version 2.1 of the License, or
+* any later version.
+*
+* Heritrix is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Lesser Public License for more details.
+*
+* You should have received a copy of the GNU Lesser Public License
+* along with Heritrix; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+package org.archive.util.ms;
+
+public class Piece {
+
+    private boolean unicode;
+    private int size;
+    private int filePos;
+
+    
+    public Piece(int filePos, int size, boolean unicode) {
+        this.filePos = filePos;
+        this.size = size;
+        this.unicode = unicode;
+    }
+
+
+    /**
+     * 
+     * @return
+     */
+    public int getFilePos() {
+        return filePos;
+    }
+
+
+    /**
+     * 
+     * @return
+     */
+    public int getSize() {
+        return size;
+    }
+
+
+    /**
+     * 
+     * @return
+     */
+    public boolean isUnicode() {
+        return unicode;
+    }
+
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Piece{filePos=").append(filePos);
+        sb.append(" size=").append(size);
+        sb.append(" unicode=").append(unicode);
+        sb.append("}");
+        return sb.toString();
+    }
+}
