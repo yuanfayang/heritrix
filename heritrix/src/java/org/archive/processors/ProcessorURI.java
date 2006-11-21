2fa0f00fab5beb512c947f36eed50fdd498d04e3
paul_jack <paul_jack@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2006-11-21 18:06:58 +0000

    Added method descriptors (not documented yet) for every CrawlURI operation required by FetchHTTP.  Most of these were copied directly from CrawlURI, so I'm not going to mention those.
    However, a few things were changed to address existing TODO items and to try to keep the API simple.  Many methods in CrawlURI exposed operations on internal collections; now I'm just returning a mutable collection.  Eg, instead of an addAnnotation method and a getAnnotations method, there's just getAnnotations and if you want to add one you can use getAnnotations().add().
    
    Some CrawlURI methods are algorithms that would be the same regardless of implementation details.  Eg, the isSimple() method just checks a bunch of other state.  These kinds of things were moved into static utility methods in the Processor class.
    
    Similarly for addLocalizedError and getLocalizedError, except there I renamed the collection to getNonFatalFailures().  Also that is now a collection of mere Throwables; the LocalizedError class was just a wrapper around a single Throwable anyway.
    
    New methods were added to eliminate the "attribute" API usage (where we used the map of key/value pairs to store state).  The CrawlURI can still use the internal map to implement these methods.
    
    New methods were also added to lift some complicated details out of FetchHTTP.  These new methods are described below.  I have comments describing what the CrawlURI implementation should look like, but these still need proper documentation.
    
    CrawlURI will eventually have to implement the changed interface, and the new methods.  Getting there...
    
    * /src/java/org/archive/processors/ProcessorURI.java
    (attachRfc2617Credential)
    (detachRfc2617Credential)
    (populateCredentials)
    (promoteCredentials)
    These methods now exist to prevent FetchHTTP from needing a CrawlController and a CredentialStore.  The CrawlURI implementations can use those complicated structures but a simpler implementation could just use internal collections.
    
    (passedDNS)
    (getResolvedName)
    Similarly, these methods exist to prevent FetchHTTP from needing a ServerCache, CrawlHosts or CrawlServers.  The CrawlURI implementation will use all those things, but a simpler implementation can just use Java's APIs.
    
    (skipToPostProcessing)
    Used to be skipToProcessingChain, but that requires processors to know about ToeThreads and so on. Plus post processing was the only one ever skipped to.

--- heritrix/src/java/org/archive/processors/ProcessorURI.java
+++ heritrix/src/java/org/archive/processors/ProcessorURI.java
@@ -24,8 +24,15 @@
 package org.archive.processors;
 
 
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.commons.httpclient.HttpMethod;
 import org.archive.net.UURI;
+import org.archive.processors.credential.CredentialAvatar;
 import org.archive.state.StateProvider;
+import org.archive.util.Recorder;
 
 
 /**
@@ -36,6 +43,8 @@ import org.archive.state.StateProvider;
 public interface ProcessorURI extends StateProvider {
 
     
+    public static enum FetchType { HTTP_GET, HTTP_POST, UNKNOWN };
+    
     /**
      * Returns the URI being processed.
      * 
@@ -43,4 +52,117 @@ public interface ProcessorURI extends StateProvider {
      */
     UURI getUURI();
 
+    
+    UURI getVia();
+    
+    boolean isPrerequisite();
+    void setPrerequisite(boolean prereq);
+    
+    // Used to be a map attribute.
+    void setFetchBeginTime(long time);
+    void setFetchCompletedTime(long time);
+    
+    
+    FetchType getFetchType();
+    
+    Recorder getRecorder();
+
+    Map<String,Object> getData();
+    
+    String getUserAgent();
+    String getFrom();
+
+    void setContentSize(long size);
+    void setContentDigest(String algorithm, byte[] digest);
+
+    String getContentType();
+    void setContentType(String mimeType);
+
+    Collection<String> getAnnotations();
+    Collection<Throwable> getNonFatalFailures();
+    
+    // Give a better name?
+    int getFetchStatus();
+    void setFetchStatus(int status);
+    
+    // Used to be a map attribute. May still want to be one.
+    void setHttpMethod(HttpMethod method);
+    
+
+    /* OK, the Heritrix implementation of this will need to merge 
+     * CredentialAvatars from the server with CredentialAvatars from
+     * this instance itself.  CredentialAvatars from this instnace itself
+     * must be promoted to the proper CrawlServer.
+     * 
+     * However, the non-Heritrix implementation might not do any of that.
+     * In particular I'm trying to free FetchHTTP from needing to know about
+     * CrawlServers.
+     * 
+     * See the old versions of FetchHTTP.populateCredentials and
+     * FetchHTTP.promoteCredentials for the Heritrix implementation of this
+     * method.
+     * 
+     * @param method
+     */
+    boolean populateCredentials(HttpMethod method);
+    void promoteCredentials();
+    
+    Set<CredentialAvatar> getCredentialAvatars();
+
+    /**
+     * Detaches an Rfc2617Credential if one exists.
+     * 
+     * Heritrix implementation lives in FetchHTTP.handle401
+     * 
+     * @return  true if a Rfc2617Credential existed and was detached
+     */
+    boolean detachRfc2617Credential(String realm);
+    
+    
+    /**
+     * Search this curi's environment for an RFC 2617 credential that is 
+     * appropriate for the realm.
+     * 
+     * Heritrix implementation lives in FetchHTTP.handle401
+     * 
+     * @param realm
+     * @return
+     */
+    boolean attachRfc2617Credential(String realm);
+
+
+    /*
+     * True if the DNS lookup occurred and worked.
+     * 
+     * Heritrix implementation should be:
+     * 
+     *          CrawlHost host = getController().getServerCache().getHostFor(curi);
+         // make sure the dns lookup succeeded
+         if (host.getIP() == null && host.hasBeenLookedUp()) {
+             curi.setFetchStatus(S_DOMAIN_PREREQUISITE_FAILURE);
+             return false;
+         }
+         return true;
+
+     * 
+     * 
+     */
+    boolean passedDNS();
+
+    
+    // Eliminate CrawlURI.hasCredentialAvatars
+
+
+    /*
+     * Host name as determined by DNS? Maybe? Needed by Credential.rootUriMatch
+     * 
+     * Heritrix implementation is:
+     * 
+     *   String serverName = controller.getServerCache().getServerFor(curi).
+     *       getName();
+     */
+    String getResolvedName();
+
+    
+    void skipToPostProcessing();
 }
