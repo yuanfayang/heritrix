f28bd888eac5c451e5704106cf1b0dd9f1e17cfb
paul_jack <paul_jack@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2007-02-23 19:21:14 +0000

    Got the credential selftest working with new system.
    Surprisingly enough, credentials actually worked.
    
    * /src/java/org/archive/crawler/AuthSelfTest.java
    Now extends SelfTestBase; sets up Jetty to do basic authentication
    and a login servlet.
    
    * /src/java/org/archive/crawler/AuthServlet.java
    Dummy authentication servlet for testing.
    
    * /testdata/selftest/AuthSelfTest
    New test data for auth self test.

--- heritrix/src/java/org/archive/crawler/selftest/AuthSelfTest.java
+++ heritrix/src/java/org/archive/crawler/selftest/AuthSelfTest.java
@@ -22,9 +22,24 @@
  */
 package org.archive.crawler.selftest;
 
-import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.mortbay.jetty.Handler;
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.bio.SocketConnector;
+import org.mortbay.jetty.handler.DefaultHandler;
+import org.mortbay.jetty.handler.HandlerList;
+import org.mortbay.jetty.handler.ResourceHandler;
+import org.mortbay.jetty.security.Constraint;
+import org.mortbay.jetty.security.ConstraintMapping;
+import org.mortbay.jetty.security.HashUserRealm;
+import org.mortbay.jetty.security.SecurityHandler;
+import org.mortbay.jetty.servlet.ServletHandler;
+import org.mortbay.jetty.servlet.ServletHolder;
 
 
 /**
@@ -34,33 +49,77 @@ import java.util.List;
  * @version $Id$
  */
 public class AuthSelfTest
-    extends SelfTestCase
+    extends SelfTestBase
 {
-    private static final File BASIC = new File("basic");
-    private static final File FORM = new File("form");
-    private static final File GET = new File(FORM, "get");
-    private static final File POST = new File(FORM, "post");
+
 
     /**
      * Files to find as a list.
      */
-    private static final List<File> FILES_TO_FIND =
-        Arrays.asList(new File[] {
-                BASIC,
-                new File(BASIC, "basic-loggedin.html"),
-                FORM,
-                new File(POST, "success.jsp"),
-                new File(POST, "post-loggedin.html"),
-                new File(GET, "success.jsp"),
-                new File(GET, "get-loggedin.html")
-        });
-
+    final private static Set<String> EXPECTED = Collections.unmodifiableSet(
+            new HashSet<String>(Arrays.asList(new String[] {
+            "index.html", "link1.html", "link2.html", "link3.html", 
+            "basic/index.html", "basic/link1.html", "basic/link2.html", "basic/link3.html", 
+            "login/login.html", "success.html",
+            "", "robots.txt"
+    })));
 
     /**
      * Test the max-link-hops setting is being respected.
      */
-    public void testAuth() {
-        testFilesInArc(FILES_TO_FIND);
+    public void testAuth() throws IOException {
+        Set<String> found = this.filesInArcs();
+        assertTrue(EXPECTED.equals(found));
     }
+
+
+    @Override
+    protected Server configureHttpServer() {
+        Server server = new Server();
+        
+        Constraint constraint = new Constraint();
+        constraint.setName(Constraint.__BASIC_AUTH);;
+        constraint.setRoles(new String[]{"user","admin","moderator"});
+        constraint.setAuthenticate(true);
+
+        ConstraintMapping cm = new ConstraintMapping();
+        cm.setConstraint(constraint);
+        cm.setPathSpec("/basic/*");
+        
+        HashUserRealm realm = new HashUserRealm();
+        realm.setName("Hyrule");
+        realm.put("Mr. Happy Pants", "xyzzy");
+        realm.addUserToRole("Mr. Happy Pants", "user");
+        
+        SecurityHandler securityHandler = new SecurityHandler();
+        securityHandler.setUserRealm(realm);
+        securityHandler.setConstraintMappings(new ConstraintMapping[]{cm});
+        
+        SocketConnector sc = new SocketConnector();
+        sc.setHost("localhost");
+        sc.setPort(7777);
+        server.addConnector(sc);
+        ResourceHandler rhandler = new ResourceHandler();
+        rhandler.setResourceBase(getSrcHtdocs().getAbsolutePath());
+        
+        ServletHandler servletHandler = new ServletHandler();        
+        
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[] { 
+                securityHandler, 
+                rhandler, 
+                servletHandler,
+                new DefaultHandler() });
+        server.setHandler(handlers);
+        
+        ServletHolder holder = new ServletHolder(new AuthServlet());
+        servletHandler.addServletWithMapping(holder, "/login/*");
+
+        return server;
+    }
+
+
+
+
 }
 
