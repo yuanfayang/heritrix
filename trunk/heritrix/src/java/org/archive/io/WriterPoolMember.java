e8209d6ca3b742f619adca189f28752eef1de0e2
gojomo <gojomo@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2007-03-09 07:45:34 +0000

    Work for [#HER-1083] Make extraction and writing dependent on duplicate analysis (missed commit) * WriterPoolMember.java     readToLimitFrom(), for partial writing of recorded content

--- trunk/heritrix/src/java/org/archive/io/WriterPoolMember.java
+++ trunk/heritrix/src/java/org/archive/io/WriterPoolMember.java
@@ -434,6 +434,22 @@ public abstract class WriterPoolMember implements ArchiveFileConstants {
         }
 	}
 	
+	protected void readToLimitFrom(final InputStream is, final long limit,
+			final byte [] b)
+	throws IOException {
+        int read = b.length;
+        int total = 0;
+        while((read = is.read(b,0,Math.min(b.length, (int)(limit-total)))) != -1
+        		&& total < limit) {
+        	total += read;
+            write(b, 0, read);
+        }
+        if (total != limit) {
+        	throw new IOException("Read " + total + " but expected " +
+        	    limit);
+        }
+	}
+	
     public void close() throws IOException {
         if (this.out == null) {
             return;
