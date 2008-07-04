973898ecd0518f6b260697ecd64c6a99db3308aa
alexoz <alexoz@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2008-07-04 02:06:12 +0000

    Applied build fixups patch from Noah Levitt. * JobResource.java, JobFileReader.java   String.isEmpty() doesn't exist in Java 1.5. * .classpath, pom.xml   Added restws module to parent project. * restws/pom.xml   Added Restlet maven repository.

--- restws/src/main/java/org/archive/crawler/client/JobFileReader.java
+++ restws/src/main/java/org/archive/crawler/client/JobFileReader.java
@@ -30,7 +30,7 @@ public class JobFileReader extends Reader {
 	public int read(char[] b, int off, int len) throws IOException {
 		String data = job.getCrawler().getEngine().readFile(job.getJobId(),
 		settingsPath, null, pos, len);
-		if (data.isEmpty()) return -1;
+		if (data.length() == 0) return -1;
 		
 		data.getChars(0, data.length(), b, off);
 		pos += data.length();
