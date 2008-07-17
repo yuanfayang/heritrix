6aa93d8e1e5af9f225470a38073ed7ea14463b46
gojomo <gojomo@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2008-07-17 20:04:48 +0000

    Fix for [HER-1496] ExceedsDocumentLengthThresholdDecideRule missing initializer * ExceedsDocumentLengthDecideRule.java     add missing initializer

--- heritrix2/modules/src/main/java/org/archive/modules/deciderules/ExceedsDocumentLengthThresholdDecideRule.java
+++ heritrix2/modules/src/main/java/org/archive/modules/deciderules/ExceedsDocumentLengthThresholdDecideRule.java
@@ -1,18 +1,19 @@
 package org.archive.modules.deciderules;
 
 import org.archive.modules.ProcessorURI;
+import org.archive.state.KeyManager;
 
 
 
 public class ExceedsDocumentLengthThresholdDecideRule 
 extends NotExceedsDocumentLengthTresholdDecideRule {
-
-
     private static final long serialVersionUID = 3L;
-
 
     boolean decision(ProcessorURI curi, int contentlength) {
         return contentlength > curi.get(this, CONTENT_LENGTH_THRESHOLD);        
     }
     
+    static {
+        KeyManager.addKeys(ExceedsDocumentLengthThresholdDecideRule.class);
+    }
 }
