a3de980182755043ec6d7d9754c606831525de23
dbernstein <dbernstein@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2008-10-13 23:09:42 +0000

    0.3.0 changes - removed dependency on jndi.

---
+++ trunk/hcc/src/main/java/org/archive/hcc/ConfigXmlConverter.java
@@ -0,0 +1,14 @@
+package org.archive.hcc;
+
+import java.io.File;
+
+import org.dom4j.Attribute;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+public class ConfigXmlConverter {
+	
+	
+}
