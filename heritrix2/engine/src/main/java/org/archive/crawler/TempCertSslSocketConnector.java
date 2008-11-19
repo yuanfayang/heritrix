3f9f7d461eac4c9eb795fb9cec51fd4f819def39
nlevitt <nlevitt@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2008-11-19 04:54:21 +0000

    * TempCertSslSocketConnector.java     add license header comment

--- heritrix2/engine/src/main/java/org/archive/crawler/TempCertSslSocketConnector.java
+++ heritrix2/engine/src/main/java/org/archive/crawler/TempCertSslSocketConnector.java
@@ -1,3 +1,28 @@
+/* TempCertSslSocketConnector
+ * 
+ * $Id$
+ * 
+ * Created on Nov 18, 2008
+ * 
+ * Copyright (C) 2008 Internet Archive.
+ *
+ * This file is part of the Heritrix web crawler (crawler.archive.org).
+ *
+ * Heritrix is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * any later version.
+ *
+ * Heritrix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with Heritrix; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 package org.archive.crawler;
 
 import java.lang.reflect.Constructor;
@@ -18,6 +43,9 @@ import javax.net.ssl.TrustManagerFactory;
 
 import org.mortbay.jetty.security.SslSocketConnector;
 
+/**
+ * @author nlevitt
+ */
 public class TempCertSslSocketConnector extends SslSocketConnector 
 {
     private static final Logger logger =
