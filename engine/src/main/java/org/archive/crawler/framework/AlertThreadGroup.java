aaea6a43b2547a94e2e1666eeddade76e13d8dc7
gojomo <gojomo@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2009-04-14 01:47:31 +0000

    Improved alert handling/display * AlertHandler.java, AlertThreadGroup.java     simplify, comment, generalize * CrawlerLoggerModule.java, CrawlJob.java     install alert.log and job log as recipient of Alert LogRecords * SinkHandlerLogThread.java     TODO comment for renaming * SinkHandler.java, SinkHandlerLogRecord.java     remove unused classes

--- engine/src/main/java/org/archive/crawler/framework/AlertThreadGroup.java
+++ engine/src/main/java/org/archive/crawler/framework/AlertThreadGroup.java
@@ -1,45 +1,57 @@
-/**
- * 
+/*
+ *  This file is part of the Heritrix web crawler (crawler.archive.org).
+ *
+ *  Licensed to the Internet Archive (IA) by one or more individual 
+ *  contributors. 
+ *
+ *  The IA licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
+
 package org.archive.crawler.framework;
 
-import org.archive.io.GenerationFileHandler;
+import java.util.LinkedList;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
+import org.archive.io.SinkHandlerLogThread;
 
 /**
- * @author pjack
- *
+ * Parent thread group which lets all child threads find the right 
+ * 'alert' error handler. Helpful for collecting all log events
+ * of a certain severity (WARNING, SEVERE) from a group of related
+ * threads. 
+ * 
+ * @contributor pjack
+ * @contributor gojomo
  */
 public class AlertThreadGroup extends ThreadGroup {
-
-    
-    private GenerationFileHandler handler;
-    private int count;
+    protected int count;
+    protected LinkedList<Logger> loggers = new LinkedList<Logger>(); 
     
     public AlertThreadGroup(String name) {
         super(name);
     }
-    
-    
-    public GenerationFileHandler getDelegate() {
-        return handler;
-    }
-    
-    
+
     public int getAlertCount() {
         return count;
     }
-    
-    public void incrementAlertCount() {
-        count++;
-    }
-    
-    
+
     public void resetAlertCount() {
         count = 0;
     }
     
-    public static void setCurrentHandler(GenerationFileHandler handler) {
-        current().handler = handler;
+    public void addLogger(Logger logger) {
+        loggers.add(logger);
     }
 
     public static AlertThreadGroup current() {
@@ -51,22 +63,48 @@ public class AlertThreadGroup extends ThreadGroup {
         return (AlertThreadGroup)th;
     }
 
-
-    public static GenerationFileHandler currentHandler() {
-        AlertThreadGroup current = current();
-        if (current == null) {
-            return null;
-        }
-        return current.handler;
-    }
-
-
-    public static void closeCurrent() {
-        AlertThreadGroup current = current();
-        if (current == null) {
+    public static void publishCurrent(LogRecord record) {
+        AlertThreadGroup atg = AlertThreadGroup.current();
+        if (atg == null) {
             return;
         }
-        current.handler.close();
-        current.handler = null;
+        atg.publish(record);
+    }
+
+    /**
+     * Pass a record to all loggers registered with the 
+     * AlertThreadGroup. Adds thread info to the message, 
+     * if available.
+     * 
+     * @param record
+     */
+    public void publish(LogRecord record) {
+        String orig = record.getMessage();
+        StringBuilder newMessage = new StringBuilder(256);
+        Thread current = Thread.currentThread();
+        newMessage.append(orig).append(" (in thread '");
+        newMessage.append(current.getName()).append("'");
+        if (current instanceof SinkHandlerLogThread) {
+            SinkHandlerLogThread tt = (SinkHandlerLogThread)current;
+            if(tt.getCurrentProcessorName().length()>0) {
+                newMessage.append("; in processor '");
+                newMessage.append(tt.getCurrentProcessorName());
+                newMessage.append("'");
+            }
+        }
+        newMessage.append(")");
+        record.setMessage(newMessage.toString());
+        count++;
+        for(Logger logger : loggers) {
+            // for the relay, suppress use of parent handlers
+            // (otherwise endless loop a risk if any target
+            // loggers relay through parents to topmost logger)
+            synchronized(logger) {
+                boolean usePar = logger.getUseParentHandlers();
+                logger.setUseParentHandlers(false);
+                logger.log(record);
+                logger.setUseParentHandlers(usePar); 
+            }
+        }
     }
 }
