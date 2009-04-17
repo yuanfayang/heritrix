bad901f36dd6ffc96d3816acaace5717e3bc4df4
gojomo <gojomo@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2009-04-17 21:38:41 +0000

    Remove unused classes * Accumulator.java, TypeSubstitution.java     (removed) no longer needed * CrawledBytesHistotable.java     remove reference

--- branches/gjm-h2-springy/engine/src/main/java/org/archive/crawler/util/CrawledBytesHistotable.java
+++ branches/gjm-h2-springy/engine/src/main/java/org/archive/crawler/util/CrawledBytesHistotable.java
@@ -1,15 +1,33 @@
+/*
+ *  This file is part of the Heritrix web crawler (crawler.archive.org).
+ *
+ *  Licensed to the Internet Archive (IA) by one or more individual 
+ *  contributors. 
+ *
+ *  The IA licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package org.archive.crawler.util;
 
 import org.apache.commons.httpclient.HttpStatus;
 import org.archive.crawler.datamodel.CoreAttributeConstants;
 import org.archive.crawler.datamodel.CrawlURI;
 import org.archive.modules.deciderules.recrawl.IdenticalDigestDecideRule;
-import org.archive.util.Accumulator;
 import org.archive.util.ArchiveUtils;
 import org.archive.util.Histotable;
 
 public class CrawledBytesHistotable extends Histotable<String> 
-implements Accumulator<CrawlURI>, CoreAttributeConstants {
+implements CoreAttributeConstants {
     private static final long serialVersionUID = 7923431123239026213L;
     
     public static final String NOTMODIFIED = "not-modified";
