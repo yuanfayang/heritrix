edc4a288970da1501a96f5bcce5fd203a369f26a
gojomo <gojomo@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2009-04-20 03:25:36 +0000

    Adapt self/unit tests to spring-based config (many)     fix problems with test harness dummy values

--- modules/src/main/java/org/archive/modules/extractor/Extractor.java
+++ modules/src/main/java/org/archive/modules/extractor/Extractor.java
@@ -1,26 +1,22 @@
-/* Copyright (C) 2006 Internet Archive.
+/*
+ *  This file is part of the Heritrix web crawler (crawler.archive.org).
  *
- * This file is part of the Heritrix web crawler (crawler.archive.org).
+ *  Licensed to the Internet Archive (IA) by one or more individual 
+ *  contributors. 
  *
- * Heritrix is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser Public License as published by
- * the Free Software Foundation; either version 2.1 of the License, or
- * any later version.
+ *  The IA licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
  *
- * Heritrix is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser Public License for more details.
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU Lesser Public License
- * along with Heritrix; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- * Extractor.java
- * Created on October 5, 2006
- *
- * $Header$
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
+ 
 package org.archive.modules.extractor;
 
 
@@ -50,7 +46,14 @@ public abstract class Extractor extends Processor {
     private static final Logger logger = 
         Logger.getLogger(Extractor.class.getName());
 
-    protected UriErrorLoggerModule loggerModule;
+    public static final ExtractorParameters DEFAULT_PARAMETERS = 
+        new ExtractorParameters() {
+            public int getMaxOutlinks() {
+                return 6000;
+            }
+        };
+
+    transient protected UriErrorLoggerModule loggerModule;
     public UriErrorLoggerModule getLoggerModule() {
         return this.loggerModule;
     }
@@ -59,7 +62,7 @@ public abstract class Extractor extends Processor {
         this.loggerModule = loggerModule;
     }
     
-    ExtractorParameters extractorParameters;
+    transient ExtractorParameters extractorParameters;
     public ExtractorParameters getExtractorParameters() {
         return extractorParameters;
     }
