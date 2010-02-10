e61a2486d4c8afb314090d26c281c5e431a7fa73
szznax <szznax@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2010-02-10 23:14:56 +0000

    test checkin

--- trunk/heritrix/src/java/org/archive/crawler/Heritrix.java
+++ trunk/heritrix/src/java/org/archive/crawler/Heritrix.java
@@ -122,7 +122,7 @@ import sun.net.www.protocol.file.FileURLConnection;
 
 /**
  * Main class for Heritrix crawler. 
- *
+ * 
  * Heritrix is usually launched by a shell script that backgrounds heritrix
  * that redirects all stdout and stderr emitted by heritrix to a log file.  So
  * that startup messages emitted subsequent to the redirection of stdout and
